905b904d740d84069dba9135ab76c3a8dd376796
angelov <angelov@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2008-11-26 13:26:50 +0000

    here
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@74 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/KasiskiTest/CollectionElement.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cryptool.KasiskiTest
+{
+    public class CollectionElement
+    {
+
+
+       
+        private int m_amount;
+        private int m_factor;
+
+
+
+        public CollectionElement(int factor, int amount)
+        {
+            m_amount = amount;
+            m_factor = factor;
+           
+        }
+
+                        
+        public int Factor
+        {
+            get { return m_factor; }
+            set
+            {
+                m_factor = value;
+            }
+        }
+        public int Amount
+        {
+            get { return m_amount; }
+            set
+            {
+                m_amount = value;
+            }
+        }
+
+    }
+}
---
+++ CrypPlugins/KasiskiTest/DataSource.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using Cryptool.KasiskiTest;
+
+
+namespace Cryptool.KasiskiTest
+{
+    public class DataSource
+    {
+        
+
+        private ObservableCollection<CollectionElement> valueCollection;
+
+        public ObservableCollection<CollectionElement> ValueCollection
+        {
+            get { return valueCollection; }
+            set { valueCollection = value; }
+        }
+
+       
+        public DataSource()
+        {
+            
+            valueCollection = new ObservableCollection<CollectionElement>();
+           
+        }
+    }
+}
