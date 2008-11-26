6379d16c6892c54c51d692a26095dbdba2496a61
angelov <angelov@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2008-11-26 12:28:35 +0000

    again
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@72 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/FrequencyTest/CollectionElement.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cryptool.FrequencyTest
+{
+    public class CollectionElement
+    {
+
+
+        private string m_caption;
+        private double m_percent;
+        private int m_amount;
+
+
+
+        public CollectionElement(int amount, double percent, string caption)
+        {
+            m_amount = amount;
+            m_caption = caption;
+            m_percent = percent;
+        }
+
+
+
+
+
+
+        public string Caption
+        {
+            get { return m_caption; }
+            set
+            {
+                m_caption = value;
+            }
+        }
+
+
+        public double Percent
+        {
+            get { return m_percent; }
+            set
+            {
+                m_percent = value;
+            }
+        }
+        public int Amount
+        {
+            get { return m_amount; }
+            set
+            {
+                m_amount = value;
+            }
+        }
+
+    }
+}
---
+++ CrypPlugins/FrequencyTest/DataSource.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace Cryptool.FrequencyTest
+{
+    public class DataSource
+    {
+
+        private ObservableCollection<CollectionElement> valueCollection;
+
+        public ObservableCollection<CollectionElement> ValueCollection
+        {
+            get { return valueCollection; }
+            set { valueCollection = value; }
+        }
+
+        //public void AddtoCollection(int i, double d, string s)
+        // {
+        //    CollectionElement z = new CollectionElement(i, d, s);
+        //    valueCollection.Add(z);
+        // }
+
+        public DataSource()
+        {
+            // CollectionElement z= new CollectionElement(30,30.5,"qqq");
+            valueCollection = new ObservableCollection<CollectionElement>();
+            // valueCollection.Add(z);
+            // CollectionElement y = new CollectionElement(30, 30.5, "qqq");
+            //CollectionElement s = new CollectionElement(30, 30.5, "qqq");
+            // valueCollection.Add(s);
+            // valueCollection.Add(y);
+            // CollectionElement q = new CollectionElement(30, 30.5, "qqq");
+            // valueCollection.Add(q);
+        }
+    }
+}
