b4557350a257b0439a6032d206d45326bdab1a5b
vazov <vazov@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2008-11-26 12:28:11 +0000

    Added a beta version Presentation.
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@71 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/KasiskiTest/KasiskiTestPresentation.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.Windows.Automation.Provider;
+using System.Windows.Automation.Peers;
+using Cryptool.KasiskiTest;
+using System.Windows.Threading;
+
+namespace KasiskiTest
+{
+    /// <summary>
+    /// Interaction logic for KasiskiTestPresentation.xaml
+    /// </summary>
+    public partial class KasiskiTestPresentation : UserControl
+    {
+        //public static DataSource source = KasiskiTest.Data ;
+
+
+        //private FrequencyTest freqT;
+        public KasiskiTestPresentation(KasiskiTest KasiskiTest)
+        {
+            InitializeComponent();
+        }
+
+        public void InitializeComponent()
+            {
+            Dispatcher.Invoke(DispatcherPriority.Normal, (SendOrPostCallback)delegate
+            {
+                // if (freqT.StringOutput != null)
+                // {
+                DataSource source = (DataSource)this.Resources["source"];
+                source.ValueCollection.Clear();
+                for (int i = 0; i < KasiskiTest.Data.ValueCollection.Count; i++)
+                {
+                    source.ValueCollection.Add(KasiskiTest.Data.ValueCollection[i]);
+                }
+                //}
+
+
+               }, null);
+            }
+    }
+}
