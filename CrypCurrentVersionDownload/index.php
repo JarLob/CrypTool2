56f9ae89e3dc154c7c2c15a895c98ca9b26ec5f1
rinne <rinne@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2008-11-28 14:38:34 +0000

    added php file for automatic download
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@83 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypCurrentVersionDownload/index.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+ <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+ <meta http-equiv="refresh" content="1; URL=http://cryptool2.vs.uni-due.de/downloads/program/curversion/Cryptool-Setup-v2.0.3236a(beta).msi">
+</head>
+<body>
+<?php
+$config_file = 'CurrentVersion.txt';
+$comment = "#";
+
+if (file_exists($config_file)) {
+	$fp = fopen($config_file, "r");
+
+    while (!feof($fp)) {
+      $line = trim(fgets($fp));
+      if ($line && !ereg("^$comment", $line)) {
+        $pieces = explode("=", $line);
+        $option = trim($pieces[0]);
+        $value = trim($pieces[1]);
+        $config_values[$option] = $value;
+      }
+    }
+    fclose($fp);
+} else echo "No such file: " . $config_file;
+
+?>
+If your download doesn't start now, click <a href="<?php echo 'http://cryptool2.vs.uni-due.de/downloads/program/curversion/Cryptool-Setup-v' . $config_values['version'] . '.msi';?>">here</a>.
+
+<script language="javascript" type="text/javascript">
+<!-- // JavaScript-Bereich fr ltere Browser auskommentieren
+    window.location.href = 'http://cryptool2.vs.uni-due.de/downloads/program/curversion/Cryptool-Setup-v<?php echo $config_values['version'];?>.msi';
+// -->
+</script>
+</body>
+</html>
