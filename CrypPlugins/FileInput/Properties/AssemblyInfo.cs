e6d609037e2de3b96dceb70ce17270f257358f46
schmid <schmid@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2008-12-01 21:56:22 +0000

    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@93 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/FileInput/Properties/AssemblyInfo.cs
+++ CrypPlugins/FileInput/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.3250.00002")]
+[assembly: AssemblyVersion("2.0.3257.00001")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: GuidAttribute("B110730C-0F8A-4653-BC6D-76DB24F95841")]
