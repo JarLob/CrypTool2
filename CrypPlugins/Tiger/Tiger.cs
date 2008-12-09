b8622c69fcc5778d057a4acef385fd619156902d
junker <junker@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2008-12-09 10:45:32 +0000

    Tiger: new empty hash plugin created
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@129 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/Tiger/Tiger.cs
@@ -0,0 +1,33 @@
+﻿//////////////////////////////////////////////////////////////////////////////////////////////////
+// CrypTool V2
+// © 2008 - Gerhard Junker
+// Apache License see http://www.apache.org/licenses/
+//
+// $HeadURL: https://www.cryptool.org/svn/CrypTool2/trunk/CrypPlugins/PKCS5/PKCS5.cs $
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// $Revision:: 121                                                                            $://
+// $Author:: junker                                                                           $://
+// $Date:: 2008-12-05 10:55:45 +0100 (Fr, 05 Dez 2008)                                        $://
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+using Cryptool.PluginBase;
+using Cryptool.PluginBase.Cryptography;
+using Cryptool.PluginBase.IO;
+using Cryptool.PluginBase.Miscellaneous;
+
+
+namespace Tiger
+{
+	[Author("Gerhard Junker", null, "private project member", "http://www.uni-siegen.de")]
+	[PluginInfo(false, "Tiger", "Tiger V1.0.2 Hash", "", "Tiger/Tiger.png")]
+	public class Tiger
+	{
+	}
+}
