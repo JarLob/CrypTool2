c7874cc8b943179f37181ca20fb5b6023bd71983
junker <junker@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2008-12-10 07:09:25 +0000

    ***** new Library to extend System.Security,Cryptography **** ---------------------------------------------------------
    Please collect here all inherited classed (from SSC)
    
    
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@134 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CCSext/SSCwhirl.cs
@@ -0,0 +1,42 @@
+﻿//////////////////////////////////////////////////////////////////////////////////////////////////
+// CrypTool V2
+// © 2008 - Gerhard Junker
+// Apache License see http://www.apache.org/licenses/
+//
+// $HeadURL$
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// $Revision::                                                                                $://
+// $Author::                                                                                  $://
+// $Date::                                                                                    $://
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Text;
+
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+
+#if DEBUG
+using System.Diagnostics;
+#endif
+
+namespace System.Security.Cryptography
+{
+	class Whirlpool: HashAlgorithm
+	{
+		protected override void HashCore(byte[] array, int ibStart, int cbSize)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override byte[] HashFinal()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Initialize()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
