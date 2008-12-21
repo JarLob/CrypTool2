c51039d71a64a55899b6c68a52a96333c24fc3e9
schmid <schmid@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2008-12-21 19:00:13 +0000

    - Initial SmartCard blueprint, NOT WORKING yet / no functions yet. just a check in save the sample code. A first working version will probably be available at the end of the year.
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@163 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/SmartCard/SmartCard.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cryptool.PluginBase.IO;
+using Cryptool.PluginBase;
+using System.ComponentModel;
+using Cryptool.PluginBase.Miscellaneous;
+using System.Windows.Controls;
+using Cryptool.PluginBase.Cryptography;
+
+namespace SmartCard
+{
+  [Author("Malte Gronau", null, "", "")]
+  [PluginInfo(false, "SmartCard", "SmartCard operations.", "", "SmartCard/Images/SmartCard.png")]
+  public class SmartCard : IThroughput
+  {
+    # region private variables
+    private SmartCardSettings settings = new SmartCardSettings();
+    private byte[] dataInput;
+    private byte[] response = null;
+    private byte[] statusWord = { 0x90, 0x00 };
+    # endregion
+
+    #region events
+    public event StatusChangedEventHandler OnPluginStatusChanged;
+
+    private void PluginStatusChanged(int imageNumber)
+    {
+      if (OnPluginStatusChanged != null)
+      {
+        OnPluginStatusChanged(this, new StatusEventArgs(imageNumber));
+      }
+    }
+
+    public event GuiLogNotificationEventHandler OnGuiLogNotificationOccured;
+    private void GuiLogMessage(string message, NotificationLevel logLevel)
+    {
+      EventsHelper.GuiLogMessage(OnGuiLogNotificationOccured, this, new GuiLogEventArgs(message, this, logLevel));
+    }
+
+    public event PluginProgressChangedEventHandler OnPluginProgressChanged;
+    # endregion events
+
+    # region constructor
+    public SmartCard()
+    {
+      settings.OnGuiLogNotificationOccured += settings_OnGuiLogNotificationOccured;
+    }
+
+    void settings_OnGuiLogNotificationOccured(IPlugin sender, GuiLogEventArgs args)
+    {
+      GuiLogMessage(args.Message, args.NotificationLevel);
+    }
+    # endregion
+
+    #region IO
+
+    [PropertyInfo(Direction.Input, "Data Input", "The input of the card reader.", "", true, false, DisplayLevel.Beginner, QuickWatchFormat.Hex, null)]
+    public byte[] DataInput
+    {
+      get { return dataInput; }
+      set
+      {
+        dataInput = value;
+        OnPropertyChanged("DataInput");
+      }
+    }
+
+    [PropertyInfo(Direction.Output, "Response", "The response of the card reader.", "", true, false, DisplayLevel.Beginner, QuickWatchFormat.Hex, null)]
+    public byte[] Response
+    {
+      get { return response; }
+      set { OnPropertyChanged("Response"); }
+    }
+
+    [PropertyInfo(Direction.Output, "StatusWord", "The response SW of the card reader.", "", true, false, DisplayLevel.Beginner, QuickWatchFormat.Hex, null)]
+    public byte[] StatusWord
+    {
+      get { return statusWord; }
+      set { OnPropertyChanged("StatusWord"); }
+    }
+    #endregion
+
+    # region IPlugin-Methods
+    public ISettings Settings
+    {
+      get { return settings; }
+    }
+
+    public UserControl Presentation
+    {
+      get { return null; }
+    }
+
+    public UserControl QuickWatchPresentation
+    {
+      get { return null; }
+    }
+
+    public void PreExecution()
+    {
+    }
+
+    public void Execute()
+    {
+      this.Response = null;
+      this.StatusWord = null;
+      GuiLogMessage("Executed.", NotificationLevel.Info);
+    }
+
+    public void PostExecution()
+    {
+    }
+
+    public void Pause()
+    {
+    }
+
+    public void Stop()
+    {
+    }
+
+    public void Initialize()
+    {
+
+    }
+
+    public void Dispose()
+    {
+    }
+
+    #endregion IPlugin-Methods
+
+    #region INotifyPropertyChanged Members
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged(string name)
+    {
+      EventsHelper.PropertyChanged(PropertyChanged, this, new PropertyChangedEventArgs(name));
+    }
+
+    #endregion
+  }
+}
---
+++ CrypPlugins/SmartCard/SmartCardSettings.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cryptool.PluginBase;
+using System.ComponentModel;
+using Cryptool.PluginBase.Miscellaneous;
+using System.Collections.ObjectModel;
+
+namespace SmartCard
+{
+  public class SmartCardSettings : ISettings
+  {
+    private ObservableCollection<string> collection = new ObservableCollection<string>();
+
+    #region ISettings Members
+
+    private bool hasChanges = false;
+    public bool HasChanges
+    {
+      get { return hasChanges; }
+      set { hasChanges = value; }
+    }
+
+    #endregion
+
+    public event GuiLogNotificationEventHandler OnGuiLogNotificationOccured;
+    private void GuiLogMessage(string message, NotificationLevel logLevel)
+    {
+      EventsHelper.GuiLogMessage(OnGuiLogNotificationOccured, null, new GuiLogEventArgs(message, null, logLevel));
+    }
+
+
+    private int cardReader = 0; // 0=default reader
+
+    public SmartCardSettings()
+    {
+      collection.Add("Virtual Cardreader");
+      collection.Add("Cardreader1");
+      Collection = collection;
+    }
+
+    [TaskPane("SmartCard reader", "Select your reader.", "", 0, false, DisplayLevel.Beginner, ControlType.DynamicComboBox, new string[] { "Collection" })]
+    public int CardReader
+    {
+      get { return this.cardReader; }
+      set
+      {
+        if (((int)value) != cardReader) hasChanges = true;
+        this.cardReader = (int)value;
+        OnPropertyChanged("CardReader");
+      }
+    }
+
+    [TaskPane("Search Card Readers", "Search for readers connected to system.", "", 1, false, DisplayLevel.Beginner, ControlType.Button)]
+    public void SearchCardReaders()
+    {
+      GuiLogMessage("found N new readers", NotificationLevel.Info);
+      collection.Clear();
+      collection.Add("Cardreader2");
+      Collection = collection;
+      CardReader = 0;
+    }
+
+    public ObservableCollection<string> Collection
+    {
+      get { return collection; }
+      set 
+      {
+        if (value != collection)
+        {
+          collection = value;
+        }
+        OnPropertyChanged("Collection");
+      }
+    }
+
+
+    #region INotifyPropertyChanged Members
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged(string name)
+    {
+      EventsHelper.PropertyChanged(PropertyChanged, this, new PropertyChangedEventArgs(name));
+    }
+
+    #endregion
+  }
+}
