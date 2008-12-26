515fa26d3036710a51cef137e5e161824fb8337e
schmid <schmid@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2008-12-26 13:11:09 +0000

    - added Plugin descriptions for Vernam, Substitution, Caesar
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@172 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/Substitution/Substitution.cs
+++ CrypPlugins/Substitution/Substitution.cs
@@ -216,7 +216,7 @@ using System.Windows.Controls;
 namespace Cryptool.Substitution
 {
     [Author("Sebastian Przybylski", "sebastian@przybylski.org", "Uni-Siegen", "http://www.uni-siegen.de")]
-    [PluginInfo(false, "Substitution", "Substitution -- classic character-based mono-alphabetic substitution", "Playfair/DetailedDescription/Description.xaml", "Substitution/icon.png")]
+    [PluginInfo(false, "Substitution", "Substitution -- classic character-based mono-alphabetic substitution", "Substitution/DetailedDescription/Description.xaml", "Substitution/icon.png")]
     [EncryptionType(EncryptionType.Classic)]
     public class Substitution : IEncryption
     {
