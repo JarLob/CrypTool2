858ccaf770a88b7911dadc1ef36f00ef402c01f1
enkler <enkler@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2009-03-16 20:50:10 +0000

    renamed SkyTale to ScyTale added Contextmenu support
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@256 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/ScyTale/ScyTaleSettings.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.ComponentModel;
+using Cryptool.PluginBase;
+
+namespace Cryptool.SkyTale
+{
+    internal enum Action
+    {
+        Encrypt,
+        Decrypt
+    }
+
+    class ScyTaleSettings : ISettings
+    {
+        private Action action = SkyTale.Action.Encrypt;
+        [ContextMenu("Action","Select the Algorithm action",0,DisplayLevel.Beginner,ContextMenuControlType.ComboBox,null, new [] { "Encrypt", "Decrypt" })]
+        [TaskPane("Action", "Select the Algorithm action", null, 0, false, DisplayLevel.Beginner, ControlType.ComboBox, new [] { "Encrypt", "Decrypt" })]
+        public int Action
+        {
+            get { return (int)action; }
+            set
+            {
+                try
+                {
+                    action = (Action)value;
+                    OnPropertyChanged("Action");
+                }
+                catch (Exception)
+                {
+                    action = SkyTale.Action.Encrypt;
+                }
+            }
+        }
+
+        private int stickSize = 1;
+        [TaskPane("Stick size", "This is the size of the used stick.", null, 1, false, DisplayLevel.Beginner, ControlType.NumericUpDown, ValidationType.RangeInteger, 2, 100)]
+        public int StickSize
+        {
+            get { return stickSize; }
+            set
+            {
+                stickSize = value;
+                OnPropertyChanged("StickSize");
+            }
+        }
+
+        private bool hasChanges;
+        public bool HasChanges
+        {
+            get { return hasChanges; }
+            set
+            {
+                if (value != hasChanges)
+                {
+                    hasChanges = value;
+                    OnPropertyChanged("HasChanges");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            if (name.ToLower() != "haschanges")
+                HasChanges = true;
+        }
+    }
+}
