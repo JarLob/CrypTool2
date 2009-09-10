0d407f49383a77e138de6ceba9c845606f713a56
nolte <nolte@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2009-09-10 12:15:02 +0000

    New Apache License added ("Team Cryptool Projects")
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@506 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/Substring/Substring.cs
+++ CrypPlugins/Substring/Substring.cs
@@ -1,5 +1,5 @@
 ﻿/*                              
-   Copyright 2009 Dennis Nolte, Uni Duisburg-Essen
+   Copyright 2009 Team Cryptool (Sven Rech,Dennis Nolte,Raoul Falk,Nils Kopal), Uni Duisburg-Essen
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
