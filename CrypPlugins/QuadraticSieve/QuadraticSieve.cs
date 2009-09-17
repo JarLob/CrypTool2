36fd04f6b5357ce8e18a9a60c0fae6fb17ab19ca
nolte <nolte@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2009-09-17 12:27:50 +0000

    QuadraticSieve Body (still not working)
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@529 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/QuadraticSieve/QuadraticSieve.cs
@@ -0,0 +1,121 @@
+﻿/*                              
+   Copyright 2009 Team CrypTool (Sven Rech,Dennis Nolte,Raoul Falk,Nils Kopal), Uni Duisburg-Essen
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cryptool.PluginBase;
+using Cryptool.PluginBase.IO;
+using QuadraticSieve;
+using Cryptool.PluginBase.Miscellaneous;
+using System.ComponentModel;
+
+namespace Cryptool.Plugins.QuadraticSieve
+{
+    [Author("Sven Rech", "rech@cryptool.org", "Uni Duisburg-Essen", "http://www.uni-due.de")]
+    [PluginInfo(false, "Quadratic Sieve", "Sieving Primes", "", "QuadraticSieve/iconqs.png")]
+    class QuadraticSieve : IThroughput
+    {
+        #region IPlugin Members
+
+        private QuadraticSieveSettings settings = new QuadraticSieveSettings();
+
+        public event StatusChangedEventHandler OnPluginStatusChanged;
+
+        public event GuiLogNotificationEventHandler OnGuiLogNotificationOccured;
+
+        public event PluginProgressChangedEventHandler OnPluginProgressChanged;
+
+        public Cryptool.PluginBase.ISettings Settings
+        {
+            get { return this.settings; }
+            set { this.settings = (QuadraticSieveSettings)value; } 
+        }
+
+        public System.Windows.Controls.UserControl Presentation
+        {
+            get { return null; }
+        }
+
+        public System.Windows.Controls.UserControl QuickWatchPresentation
+        {
+            get { return null; }
+        }
+
+        public void PreExecution()
+        {
+            
+        }
+
+        public void Execute()
+        {
+            
+        }
+
+        public void PostExecution()
+        {
+           
+        }
+
+        public void Pause()
+        {
+            
+        }
+
+        public void Stop()
+        {
+            
+        }
+
+        public void Initialize()
+        {
+            
+        }
+
+        public void Dispose()
+        {
+           
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged Members
+
+
+
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            EventsHelper.PropertyChanged(PropertyChanged, this, new PropertyChangedEventArgs(name));
+        }
+
+        public event PluginProgressChangedEventHandler OnPluginProcessChanged;
+
+        private void ProgressChanged(double value, double max)
+        {
+            EventsHelper.ProgressChanged(OnPluginProgressChanged, this, new PluginProgressEventArgs(value, max));
+        }
+
+        private void GuiLogMessage(string p, NotificationLevel notificationLevel)
+        {
+            EventsHelper.GuiLogMessage(OnGuiLogNotificationOccured, this, new GuiLogEventArgs(p, this, notificationLevel));
+        }
+
+        #endregion
+    }
+}
