8bd07095e045781fb9244627a99a9e950e348ec3
wander <wander@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2009-09-22 08:39:19 +0000

    further Scytale renamings
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@553 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/Scytale/Properties/AssemblyInfo.cs
+++ CrypPlugins/Scytale/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ScyTale")]
+[assembly: AssemblyTitle("Scytale")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ScyTale")]
+[assembly: AssemblyProduct("Scytale")]
 [assembly: AssemblyCopyright("Copyright ©  2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
