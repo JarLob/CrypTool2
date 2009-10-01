ff2de86530f943ac05708f7ea462c3d26f69a5c3
falk <falk@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2009-10-01 09:50:56 +0000

    converter plugin added (working on it)
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@618 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/Converter/Converter.cs
@@ -0,0 +1,187 @@
+﻿/*                              
+   Copyright 2009 Team CrypTool (Sven Rech,Dennis Nolte,Raoul Falk,Nils Kopal), Uni Duisburg-Essen
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cryptool.PluginBase;
+using Cryptool.PluginBase.IO;
+using System.ComponentModel;
+using Cryptool.PluginBase.Miscellaneous;
+using System.Runtime.CompilerServices;
+
+namespace Cryptool.Plugins.Comparators
+{
+    [Author("Raoul Falk, Dennis Nolte", "falk@cryptool.org", "Uni Duisburg-Essen", "http://www.uni-due.de")]
+    [PluginInfo(false, "Converter", "Converts input to another type", "", "Converter/icons/icon.png")]
+
+    class Converter : IThroughput
+    {
+        #region private variables
+
+        private ComparatorsSettings settings = new ConverterSettings();
+        private object inputOne;
+        private object output;
+
+        #endregion
+
+        #region public interfaces
+
+        public Converter()
+        {
+            this.settings = new ConverterSettings();
+            this.settings.OnPluginStatusChanged += settings_OnPluginStatusChanged;
+        }
+
+        public ISettings Settings
+        {
+            get { return this.settings; }
+            set { this.settings = (ConverterSettings)value; }
+        }
+
+        private void Comparators_LogMessage(string msg, NotificationLevel loglevel)
+        {
+            EventsHelper.GuiLogMessage(OnGuiLogNotificationOccured, this, new GuiLogEventArgs(msg, this, loglevel));
+        }
+
+        public System.Windows.Controls.UserControl Presentation
+        {
+            get { return null; }
+        }
+
+        public System.Windows.Controls.UserControl QuickWatchPresentation
+        {
+            get { return null; }
+        }
+
+        [PropertyInfo(Direction.InputData, "Input one", "Input one.", "", true, false, DisplayLevel.Beginner, QuickWatchFormat.Text, null)]
+        public object InputOne
+        {
+            [MethodImpl(MethodImplOptions.Synchronized)]
+            get { return inputOne; }
+            [MethodImpl(MethodImplOptions.Synchronized)]
+            set
+            {
+                if (value != inputOne)
+                {
+                    inputOne = value;
+                    OnPropertyChanged("InputOne");
+                }
+            }
+        }
+
+     
+
+        [PropertyInfo(Direction.OutputData, "Output", "Output.", "", true, false, DisplayLevel.Beginner, QuickWatchFormat.Text, null)]
+        public object Output
+        {
+            [MethodImpl(MethodImplOptions.Synchronized)]
+            get
+            {
+                return output;
+            }
+
+            [MethodImpl(MethodImplOptions.Synchronized)]
+            set
+            {
+                this.output = value;
+                OnPropertyChanged("Output");
+            }
+        }
+
+        #endregion
+
+        #region IPlugin members
+
+        public void Dispose()
+        {
+
+        }
+
+        public void Execute()
+        {
+           
+        }
+
+        public void Initialize()
+        {
+
+        }
+
+        public void Pause()
+        {
+
+        }
+
+        public void PostExecution()
+        {
+
+        }
+
+        public void PreExecution()
+        {
+
+        }
+
+        public void Stop()
+        {
+
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged Member
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            EventsHelper.PropertyChanged(PropertyChanged, this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+
+        #region event handling
+
+        private void Progress(double value, double max)
+        {
+            EventsHelper.ProgressChanged(OnPluginProgressChanged, this, new PluginProgressEventArgs(value, max));
+        }
+
+        public event StatusChangedEventHandler OnPluginStatusChanged;
+
+        void settings_OnPluginStatusChanged(IPlugin sender, StatusEventArgs args)
+        {
+            if (OnPluginStatusChanged != null) OnPluginStatusChanged(this, args);
+        }
+
+        public event PluginProgressChangedEventHandler OnPluginProgressChanged;
+
+        private void ProgressChanged(double value, double max)
+        {
+            EventsHelper.ProgressChanged(OnPluginProgressChanged, this, new PluginProgressEventArgs(value, max));
+        }
+
+        public event GuiLogNotificationEventHandler OnGuiLogNotificationOccured;
+
+        private void GuiLogMessage(string p, NotificationLevel notificationLevel)
+        {
+            EventsHelper.GuiLogMessage(OnGuiLogNotificationOccured, this, new GuiLogEventArgs(p, this, notificationLevel));
+        }
+
+        #endregion
+    }
+}
---
+++ CrypPlugins/Converter/ConverterSettings.cs
@@ -0,0 +1,81 @@
+﻿/*                              
+   Copyright 2009 Team CrypTool (Sven Rech,Dennis Nolte,Raoul Falk,Nils Kopal), Uni Duisburg-Essen
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+using Cryptool.PluginBase;
+
+namespace Cryptool.Plugins.Comparators
+{
+    public class ConverterSettings : ISettings
+    {
+        #region private variables
+        private int comparator = 0; // 0 =, 1 !=, 2 <, 3 >, 4 <=, 5 >=
+        private bool hasChanges;
+        #endregion
+
+        #region taskpane
+        [TaskPane("Converter", "Choose the output type", null, 1, false, DisplayLevel.Beginner, ControlType.ComboBox, new string[] { "int", "biginteger", "short", "byte", "double", "string" })]
+        public int Comparator
+        {
+            get { return this.comparator; }
+            set
+            {
+                if (value != this.comparator)
+                {
+                    this.comparator = value;
+                    OnPropertyChanged("Comparator");
+                    HasChanges = true;
+
+                    ChangePluginIcon(comparator);
+                }
+            }
+        }
+        #endregion
+
+        #region ISettings Member
+
+        public bool HasChanges
+        {
+            get { return hasChanges; }
+            set { hasChanges = value; }
+
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged Member
+
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+
+        }
+        public event StatusChangedEventHandler OnPluginStatusChanged;
+        private void ChangePluginIcon(int iconIndex)
+        {
+            if (OnPluginStatusChanged != null) OnPluginStatusChanged(null, new StatusEventArgs(StatusChangedMode.ImageUpdate, iconIndex));
+        }
+        #endregion
+    }
+}
