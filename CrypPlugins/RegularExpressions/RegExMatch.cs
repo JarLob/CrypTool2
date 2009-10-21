575b46dbd085ccad260b10412e5fd8849e312f49
kohnen <kohnen@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2009-10-21 18:37:03 +0000

    new Plugin: Regular Expressions - Match
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@764 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/RegularExpressions/RegExMatch.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.ComponentModel;
+
+using Cryptool.PluginBase;
+using Cryptool.PluginBase.Validation;
+using Cryptool;
+using Cryptool.PluginBase.IO;
+using Cryptool.PluginBase.Miscellaneous;
+
+using System.Text.RegularExpressions;
+
+namespace RegularExpressions
+{
+    [Author("Daniel Kohnen", "kohnen@cryptool.org", "Universität Duisburg Essen", "http://www.uni-due.de")]
+    [PluginInfo(false, "Regular Expression Match", "Matching Regular Expression", "", "RegularExpressions/icons/match-icon.png")]
+
+
+    public class RegExMatch : IThroughput
+    {
+        #region Private variables
+
+        private RegExMatchSettings settings;
+        private string input;
+        private string pattern;
+        private string outputText;
+        private Boolean outputBool = false;
+
+        # endregion
+
+        # region Public Methods
+
+        public RegExMatch()
+        {
+                this.settings = new RegExMatchSettings();
+        }
+
+        # endregion
+
+        # region Properties
+
+        [PropertyInfo(Direction.InputData, "Input", "Input a string to be processed by the RegEx Matcher", "", true, false, DisplayLevel.Beginner, QuickWatchFormat.Text, null)]
+        public string Input
+        {
+            get
+            {
+                return this.input;
+            }
+
+            set
+            {
+                this.input = value;
+                OnPropertyChange("Input");
+            }
+        }
+
+        [PropertyInfo(Direction.InputData, "Pattern", "Pattern for the RegEx", "", true, false, DisplayLevel.Beginner, QuickWatchFormat.Text, null)]
+        public string Pattern
+        {
+            get
+            {
+                return this.pattern;
+            }
+
+            set
+            {
+                this.pattern = value;
+                OnPropertyChange("Pattern");
+            }
+        }
+
+        [PropertyInfo(Direction.OutputData, "Output", "Output", "", DisplayLevel.Beginner)]
+        public string OutputText
+        {
+            get
+            {
+                    return this.outputText;
+            }
+            set
+            {
+                this.outputText = value;
+                OnPropertyChange("OutputText");
+            }
+        }
+
+        [PropertyInfo(Direction.OutputData, "Output Bool", "True: pattern matches. False: it does not.", "", DisplayLevel.Beginner)]
+        public Boolean OutputBool
+        {
+            get
+            {
+                return this.outputBool;       
+            }
+            set
+            {
+                this.outputBool = value;
+                OnPropertyChange("OutputBool");
+            }
+        }
+
+        # endregion
+
+        #region IPlugin Member
+
+        public void Dispose()
+        {
+          
+        }
+
+        public void Execute()
+        {
+            try
+            {
+                if (input != null && pattern != null)
+                {
+                    Match match = Regex.Match(input, pattern);
+                    if (match.Success)
+                    {
+                        OutputBool = true;
+                        OutputText = match.Value;
+
+                    }
+
+                    else
+                    {
+                        OutputBool = false;
+                        OutputText = "";
+
+                    }
+                }
+
+                ProgressChanged(1, 1);
+            }
+            catch (Exception e)
+            {
+                //GuiLogMessage("Regular Expression is not valid.", NotificationLevel.Warning);
+            }
+        }
+
+        public void Initialize()
+        {
+           
+        }
+
+        public event GuiLogNotificationEventHandler OnGuiLogNotificationOccured;
+
+        public void GuiLogMessage(string message, NotificationLevel logLevel)
+        {
+            EventsHelper.GuiLogMessage(OnGuiLogNotificationOccured, this, new GuiLogEventArgs(message, this, logLevel));
+        }
+        
+        public event PluginProgressChangedEventHandler OnPluginProgressChanged;
+        public event StatusChangedEventHandler OnPluginStatusChanged;
+        
+        private void OnPropertyChange(String propertyname)
+        {
+            EventsHelper.PropertyChanged(PropertyChanged, this, new PropertyChangedEventArgs(propertyname));
+        }
+
+        private void ProgressChanged(double value, double max)
+        {
+            EventsHelper.ProgressChanged(OnPluginProgressChanged, this, new PluginProgressEventArgs(value, max));
+        }
+
+        public void Pause()
+        {
+           
+        }
+
+        public void PostExecution()
+        {
+            
+        }
+
+        public void PreExecution()
+        {
+            
+        }
+
+        public System.Windows.Controls.UserControl Presentation
+        {
+            get { return null; }
+        }
+
+        public System.Windows.Controls.UserControl QuickWatchPresentation
+        {
+            get { return null; }
+        }
+
+        public ISettings Settings
+        {
+            get { return this.settings;}
+            set { this.settings = (RegExMatchSettings)value; }
+        }
+
+        public void Stop()
+        {
+           
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged Member
+
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
---
+++ CrypPlugins/RegularExpressions/RegExMatchSettings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Cryptool.PluginBase;
+using System.ComponentModel;
+
+namespace RegularExpressions
+{
+    class RegExMatchSettings : ISettings
+    {
+        private bool hasChanges = false;
+
+        #region ISettings Member
+
+        public bool HasChanges
+        {
+            get
+            {
+                return hasChanges;
+            }
+            set
+            {
+                hasChanges = value;
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged Member
+
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
