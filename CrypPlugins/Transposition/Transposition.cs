b4c3eb7c56eee221d8848dca0bda492e980e4c67
kohnen <kohnen@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2009-10-23 21:51:29 +0000

    added some encryption/decryption functions
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@774 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/Transposition/Transposition.cs
+++ CrypPlugins/Transposition/Transposition.cs
@@ -200,6 +200,7 @@ namespace ClassLibrary1
                     }
                 }
             }
+
             return enc;
         }
 
@@ -256,8 +257,10 @@ namespace ClassLibrary1
                     }
                 }
             }
-
             String dec = "";
+
+            // Spaltenweise auslesen
+            // Read by line
             for (int j = 0; j < hoehe; j++)
             {
                 for (int i = 0; i < breite; i++)
@@ -265,9 +268,283 @@ namespace ClassLibrary1
                     dec += matrix[i, j];
                 }
             }
+            // Ende Spaltenweise auslesen
             return dec;
         }
 
+        private char[,] enc_read_in_by_row(String input, int keyword_length)
+        {
+            int size = input.Length / keyword_length;
+
+            if (input.Length % keyword_length != 0)
+            {
+                size++;
+            }
+
+            int pos = 0;
+
+            char[,] matrix = new char[keyword_length, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < keyword_length; j++)
+                {
+                    if (pos < input.Length)
+                    {
+                        matrix[j, i] = input[pos];
+                        pos++;
+                    }                    
+                }
+            }
+
+            return matrix;
+        }
+
+        private char[,] enc_read_in_by_column(String input, int keyword_length)
+        {
+            int size = input.Length / keyword_length;
+            int offs = input.Length % keyword_length;
+            if (offs != 0)
+            {
+                size++;
+            }
+
+            int pos = 0;
+
+            char[,] matrix = new char[keyword_length, size];
+            for (int i = 0; i < keyword_length; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (pos < input.Length)
+                    {
+                        if (offs > 0 && j == size - 1 && i >= offs) { }
+                        else
+                        {
+                            matrix[i, j] = input[pos];
+                            pos++;
+                        }
+                    }
+                }
+            }
+
+            return matrix;
+        }
+
+        private char[,] dec_read_in_by_row(String input, int[] keyword)
+        {
+            int size = input.Length / keyword.Length;
+
+            int offs = input.Length % keyword.Length;
+            if (offs != 0)
+            {
+                size++;
+            }
+
+            char[,] matrix = new char[keyword.Length, size];
+            int pos = 0;
+
+            for (int i = 0; i < keyword.Length; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (pos < input.Length)
+                    {
+                        if ((!offs.Equals(0)) && j.Equals(size - 1))
+                        {
+                            bool ok = false;
+
+                            for (int k = 0; k < offs; k++)
+                            {
+                                Console.WriteLine(k);
+                                if((keyword[k]-1).Equals(i))
+                                {
+                                    ok = true;
+                                }
+                            }
+
+                            if (ok)
+                            {
+                                matrix[i, j] = input[pos];
+                                pos++;
+                            }
+
+                        }
+                        else
+                        {
+                            matrix[i, j] = input[pos];
+                            pos++;
+                        }
+                    }
+                }
+            }
+
+
+            return matrix;
+        }
+
+        private char[,] dec_read_in_by_column(String input, int[] keyword)
+        {
+            int size = input.Length / keyword.Length;
+
+            int offs = input.Length % keyword.Length;
+            if (offs != 0)
+            {
+                size++;
+            }
+
+            char[,] matrix = new char[keyword.Length, size];
+            int pos = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < keyword.Length; j++)
+                {
+                    if (pos < input.Length)
+                    {
+                        if ((!offs.Equals(0)) && i.Equals(size - 1))
+                        {
+                            bool ok = false;
+
+                            for (int k = 0; k < offs; k++)
+                            {
+                                Console.WriteLine(k);
+                                if ((keyword[k] - 1).Equals(j))
+                                {
+                                    ok = true;
+                                }
+                            }
+
+                            if (ok)
+                            {
+                                matrix[j, i] = input[pos];
+                                pos++;
+                            }
+
+                        }
+                        else
+                        {
+                            matrix[j, i] = input[pos];
+                            pos++;
+                        }
+                    }
+                }
+            }
+
+
+            return matrix;
+        }
+
+        private char[,] enc_permut_by_column(char[,] readin_matrix, int[] keyword)
+        {
+            int x = keyword.Length;
+            int y = readin_matrix.Length / keyword.Length;
+
+            char[,] matrix = new char[x,y];
+
+            int pos = 0;
+
+            for (int i = 1; i <= keyword.Length; i++)
+            {
+                for (int j = 0; j < keyword.Length; j++)
+                {
+                    if(i.Equals(keyword[j]))
+                    {
+                        Console.WriteLine("true");
+                        pos = j;
+                    }
+                }
+
+                for (int j = 0; j < y; j++)
+                {
+                    matrix[i - 1, j] = readin_matrix[pos, j];
+                }
+            }
+
+            return matrix;
+        }
+
+        private String enc_read_out_by_row(char[,] matrix, int keyword_length)
+        {
+            int x = keyword_length;
+            int y = matrix.Length / keyword_length;
+
+            String enc = "";
+            char empty_char = new char();
+
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    char tmp = matrix[j, i];
+                    if (! tmp.Equals(empty_char))
+                    {
+                        enc += tmp;
+                    }
+                }
+            }
+
+            return enc;
+        }
+
+        private String enc_read_out_by_column(char[,] matrix, int keyword_length)
+        {
+            int x = keyword_length;
+            int y = matrix.Length / keyword_length;
+
+            String enc = "";
+            char empty_char = new char();
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    char tmp = matrix[i, j];
+                    if (!tmp.Equals(empty_char))
+                    {
+                        enc += tmp;
+                    }
+                }
+            }
+
+            return enc;
+        }
+
+        private int[] get_Keyword_Array(String keyword)
+        {
+            int length = keyword.Length/2 + 1;
+            int[] keys = new int[length];
+
+            for (int i = 0; i < keyword.Length; i = i + 2)
+            {
+                keys[i / 2] = (int)Char.GetNumericValue(keyword[i]);
+            }
+
+            return keys;
+        }
+
+        private bool is_Valid_Keyword(int[] keyword)
+        {
+            for (int i = 1; i <= keyword.Length; i++)
+            {
+                bool exists = false;
+
+                for (int j = 0; j < keyword.Length; j++)
+                {
+                    if (i.Equals(keyword[j]))
+                    {
+                        exists = true;
+                    }
+                }
+
+                if (!exists)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
         # endregion
     }
 }
