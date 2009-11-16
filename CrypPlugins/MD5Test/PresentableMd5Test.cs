2f03a4e20953363ed19f55c3126b5a289639bb0c
pretzsch <pretzsch@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2009-11-16 17:27:02 +0000

    + MD5 plugin: Additional tests
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@855 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/MD5Test/PresentableMd5Test.cs
+++ CrypPlugins/MD5Test/PresentableMd5Test.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Text;
 using NUnit.Framework;
 using System.IO;
+using Cryptool.MD5;
 
-namespace Cryptool.MD5.Test
+namespace Test.Cryptool.MD5
 {
     [TestFixture]
     class PresentableMd5Test
@@ -19,32 +20,55 @@ namespace Cryptool.MD5.Test
         }
 
         [Test]
-        public void CompareResultsToBuiltinMd5()
+        public void VerifyResultsForRandomData()
         {
             const int TEST_RUNS = 20;
-
-            for (int i = 0; i < TEST_RUNS; i++)
-            {
-                byte[] testData = GenerateTestData();
-
-                System.Security.Cryptography.MD5 builtinMD5 = System.Security.Cryptography.MD5.Create();
-                byte[] builtinResult = builtinMD5.ComputeHash(testData);
-
-                PresentableMd5 presentableMd5 = new PresentableMd5(new MemoryStream(testData));
-                presentableMd5.NextStepUntilFinished();
-                byte[] presentableMd5Result = presentableMd5.HashValueBytes;
-
-                Assert.AreEqual(builtinResult, presentableMd5Result);
-            }
-        }
-
-        private byte[] GenerateTestData()
-        {
             const int MIN_TEST_DATA_LENGTH = 1;
             const int MAX_TEST_DATA_LENGTH = 1000;
 
-            int resultLength = MIN_TEST_DATA_LENGTH + RNG.Next(MAX_TEST_DATA_LENGTH - MIN_TEST_DATA_LENGTH) + 1;
-            byte[] result = new byte[resultLength];
+            for (int i = 0; i < TEST_RUNS; i++)
+            {
+                byte[] testData = GenerateTestData(MIN_TEST_DATA_LENGTH, MAX_TEST_DATA_LENGTH);
+                VerifyResult(testData);
+            }
+        }
+
+        [Test]
+        public void VerifyResultForEmptyData()
+        {
+            byte[] emptyByteArray = new byte[0];
+            VerifyResult(emptyByteArray);
+        }
+
+        [Test]
+        public void VerifyResultForRandomDataWithInterestingLength()
+        {
+            VerifyResult(GenerateTestData(54));
+            VerifyResult(GenerateTestData(55));
+            VerifyResult(GenerateTestData(56));
+        }
+
+        private void VerifyResult(byte[] data)
+        {
+            System.Security.Cryptography.MD5 builtinMD5 = System.Security.Cryptography.MD5.Create();
+            byte[] builtinResult = builtinMD5.ComputeHash(data);
+
+            PresentableMd5 presentableMd5 = new PresentableMd5(new MemoryStream(data));
+            presentableMd5.NextStepUntilFinished();
+            byte[] presentableMd5Result = presentableMd5.HashValueBytes;
+
+            Assert.AreEqual(builtinResult, presentableMd5Result);
+        }
+
+        private byte[] GenerateTestData(int minLength, int maxLength)
+        {
+            int resultLength = minLength + RNG.Next(maxLength - minLength) + 1;
+            return GenerateTestData(resultLength);
+        }
+
+        private byte[] GenerateTestData(int length)
+        {
+            byte[] result = new byte[length];
 
             RNG.NextBytes(result);
 
