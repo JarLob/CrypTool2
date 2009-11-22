2e32f4d8f9453fe8efc75a7ad6d4151bb150965e
enkler <enkler@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2009-11-22 15:35:20 +0000

    Improved Lucene-Net usage
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@876 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypCoreSearchTest/Program.cs
+++ CrypCoreSearchTest/Program.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Reflection;
 using Cryptool.Core;
 
-
 namespace CrypCoreSearchTest
 {
     class Program
@@ -13,16 +12,21 @@ namespace CrypCoreSearchTest
             var Provider = new SearchProvider
                                {
                                    HelpFilePath =
-                                       (Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\HelpFiles"),
+                                       Path.GetFullPath(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\..\\..\\HelpFiles"),
                                    IndexPath =
-                                       (Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\HelpIndex")
+                                       Path.GetFullPath(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\..\\..\\HelpIndex")
                                };
             Provider.CreateIndexes();
+            Console.WriteLine("Bitte Suchwort eingeben");
+            int counter= 1;
             foreach(var Result in Provider.Search(Console.ReadLine()))
             {
-                Console.WriteLine(Result.Plugin);
-                Console.WriteLine(Result.Score);
-                Console.WriteLine(Result.Context);
+                Console.WriteLine(string.Format("{0}. PlugIn: {1}",counter,Result.Plugin));
+                foreach (var context in Result.Contexts)
+                {
+                    Console.WriteLine(string.Format("     Kontext: {0}", context));
+                }
+                counter++;
             }
             Console.ReadKey();
         }
