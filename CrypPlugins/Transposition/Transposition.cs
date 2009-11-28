ad848b4fd276182075f584df7592f0bff1d566c7
weyers <weyers@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2009-11-28 17:32:23 +0000

    important changes for the Presentation
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@913 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/Transposition/Transposition.cs
+++ CrypPlugins/Transposition/Transposition.cs
@@ -163,7 +163,7 @@ namespace Transposition
             {
                 ((TranspositionControl)controlSlave).onStatusChanged();
             }
-            //myPresentation.main(Read_in_matrix,Permuted_matrix,key,Keyword,Input,Output,this.settings.Permutation,this.settings.ReadIn,this.settings.ReadOut);   
+           // myPresentation.main(Read_in_matrix,Permuted_matrix,key,Keyword,Input,Output,this.settings.Permutation,this.settings.ReadIn,this.settings.ReadOut,this.settings.Action);   
         }
 
         public void Initialize()
