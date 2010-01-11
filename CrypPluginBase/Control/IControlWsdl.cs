6321e89f29dc5896044efd3d4db3e7bb0709f3b5
podeszwa <podeszwa@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-01-11 17:57:23 +0000

    Interface for Wsdl export added
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@1061 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPluginBase/Control/IControlWsdl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Cryptool.PluginBase.Control
+{
+    public interface IControlWsdl : IControl
+    {
+        XmlDocument Wsdl
+        {
+            set;
+            get;
+        }
+
+        void setWsdl(XmlDocument wsdlDocument);
+    }
+}
