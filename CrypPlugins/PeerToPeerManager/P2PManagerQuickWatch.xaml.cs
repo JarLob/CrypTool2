f2a33b68f48f7c2677013459cda7fe5c565072c2
arnold <arnold@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-01-12 13:19:50 +0000

    Added QuickWatchPresentation
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@1071 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/PeerToPeerManager/P2PManagerQuickWatch.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using KeySearcher;
+
+namespace Cryptool.Plugins.PeerToPeer
+{
+    /// <summary>
+    /// Interaction logic for P2PManagerQuickWatch.xaml
+    /// </summary>
+    public partial class P2PManagerQuickWatch : UserControl
+    {
+        public ObservableCollection<ResultEntry> entries = new ObservableCollection<ResultEntry>();
+
+        public P2PManagerQuickWatch()
+        {
+            InitializeComponent();
+            SizeChanged += new SizeChangedEventHandler(P2PManagerQuickWatch_SizeChanged);
+            this.DataContext = entries;
+        }
+
+        void P2PManagerQuickWatch_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            double height = this.ActualHeight - this.Grid.ActualHeight;
+            if (height < 0)
+            {
+                height = 0;
+            }
+            this.ListView.Height = height;
+            this.ListView.Width = this.ActualWidth;
+
+            double heightTransform = (this.ActualHeight - height) / this.Grid.ActualHeight;
+            double widthTransform = this.ActualWidth / this.Grid.ActualWidth;
+
+            if (widthTransform > heightTransform)
+            {
+                widthTransform = heightTransform;
+            }
+
+
+            this.Grid.RenderTransform = new ScaleTransform(widthTransform, heightTransform);
+        }
+    }
+}
