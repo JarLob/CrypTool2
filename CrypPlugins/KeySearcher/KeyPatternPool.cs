b2dfb2bbb46f02fdb3e3f92b33dd6e0969c1bbbb
rech <rech@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-01-31 18:01:00 +0000

    new splitting algorithm for KeyPatternPool, which is needed for peer2peer
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@1117 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/KeySearcher/KeyPatternPool.cs
+++ CrypPlugins/KeySearcher/KeyPatternPool.cs
@@ -3,50 +3,185 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Cryptool.PluginBase.Miscellaneous;
+using System.Collections;
 
 namespace KeySearcher
 {
+    /**
+     * This class is able to split a KeyPattern into several disjunct parts, which are guaranteed to have equal size.
+     * It tries to split the pattern in such a way, that the parts have nearly the given partsize.
+     **/
     class KeyPatternPool
     {
         private BigInteger partsize;
-        private Stack<KeyPattern> stack;
+        private KeyPattern pattern;
+        private Stack<KeyPattern> stack = new Stack<KeyPattern>();
+        private int[] splittingQuotient;
+        private int[] splittingCounter;
+        private bool end = false;               
 
-        private void makePool(KeyPattern pattern)
+        /*private BigInteger calculateSplitting(int i)
         {
-            if (pattern.size() > partsize)
+            //This method is better, but too slow :(
+            if (i >= pattern.wildcardList.Count)            
+                return getPartSize();
+
+            int best = 0;
+            BigInteger bestval = -1;
+            int c = ((Wildcard)pattern.wildcardList[i]).size();
+            if (c == 1)
             {
-                KeyPattern[] patterns = pattern.split();
-                stack.Push(patterns[1]);
-                makePool(patterns[0]);
+                splittingQuotient[i] = 1;
+                return calculateSplitting(i + 1);
             }
-            else
+            for (int k = 1; k <= c; k++)
+                if (c % k == 0)
+                {
+                    splittingQuotient[i] = k;
+                    BigInteger res = calculateSplitting(i + 1);
+                    if ((bestval == -1) || ((res-partsize).abs() < (bestval-partsize).abs()))
+                    {
+                        bestval = res;
+                        best = k;
+                    }
+                }
+            splittingQuotient[i] = best;
+            calculateSplitting(i + 1);
+            return bestval;
+        }*/
+
+        private void calculateSplitting()
+        {
+            for (int c = pattern.wildcardList.Count - 1; c >= 0; c--)
+                splittingQuotient[c] = 1;
+
+            BigInteger bestSize = getPartSize();
+
+            for (int c = pattern.wildcardList.Count - 1; c >= 0; c--)
             {
-                stack.Push(pattern);                
+                for (int k = 1; k <= c; k++)
+                {
+                    int d = ((Wildcard)pattern.wildcardList[c]).size();
+                    if (d % k == 0)
+                    {
+                        int tmp = splittingQuotient[c];
+                        splittingQuotient[c] = d;
+                        BigInteger size = getPartSize();
+                        if ((size - partsize).abs() < (bestSize - partsize).abs())                        
+                            bestSize = size;                        
+                        else
+                            splittingQuotient[c] = tmp;
+                    }
+                }
             }
         }
-
-        public KeyPattern getNext()
+                
+        private bool succCounter()
         {
-            if (stack.Count == 0)
+            for (int k = pattern.wildcardList.Count-1; k >= 0; k--)
+            {
+                Wildcard wc = ((Wildcard)pattern.wildcardList[k]);
+                splittingCounter[k]++;
+                if (splittingCounter[k] >= splittingQuotient[k])
+                    splittingCounter[k] = 0;
+                else
+                    return true;
+            }
+            return false;
+        }
+
+        public bool contains(KeyPattern pattern)
+        {
+            if (pattern.wildcardList.Count != this.pattern.wildcardList.Count)
+                return false;
+            if (pattern.GetPattern() != this.pattern.GetPattern())
+                return false;
+
+            for (int k = 0; k < pattern.wildcardList.Count; k++)
+            {
+                Wildcard wc = ((Wildcard)pattern.wildcardList[k]);
+                Wildcard thiswc = ((Wildcard)this.pattern.wildcardList[k]);
+                if (wc.size() != (thiswc.size() / splittingQuotient[k]))
+                    return false;
+
+                bool contains2 = true;
+                for (int j = 0; j < splittingQuotient[k]; j++)
+                {
+                    bool contains = true;
+                    for (int i = 0; i < wc.size(); i++)
+                    {
+                        if (wc.getChar(i - wc.count()) != thiswc.getChar(i + j * wc.size()))
+                        {
+                            contains = false;
+                            break;
+                        }
+                    }
+                    if (contains)
+                    {
+                        contains2 = true;
+                        break;
+                    }
+                }
+                if (!contains2)
+                    return false;
+
+            }
+            return true;
+        }
+
+        public void push(KeyPattern pattern)
+        {
+            if (!contains(pattern))
+                stack.Push(pattern);
+            else
+                throw new Exception("Pattern already given.");
+        }
+
+        public KeyPattern pop()
+        {
+            if (stack.Count != 0)
+                return (KeyPattern)stack.Pop();
+
+            if (end)
                 return null;
 
-            KeyPattern top = stack.Pop();
-            if (top.size() > partsize)
-            {
-                KeyPattern[] patterns = top.split();
-                stack.Push(patterns[1]);
-                stack.Push(patterns[0]);
-                return getNext();
+            KeyPattern part = new KeyPattern(pattern.GetPattern());
+            part.wildcardList = new ArrayList();
+            for (int k = 0; k < pattern.wildcardList.Count; k++)
+            {                
+                Wildcard wc = ((Wildcard)pattern.wildcardList[k]);
+                char[] values = new char[256];
+                int length = wc.size() / splittingQuotient[k];
+                for (int i = 0; i < length; i++)
+                    values[i] = wc.getChar(i + splittingCounter[k] * length);
+                Wildcard newwc = new Wildcard(values, length);
+                part.wildcardList.Add(newwc);
             }
-            else
-                return top;
+
+            if (!succCounter())
+                end = true;
+
+            return part;
+        }
+
+        public BigInteger getPartSize()
+        {
+            BigInteger res = 1;
+            for (int k = 0; k < pattern.wildcardList.Count; k++)
+            {
+                Wildcard wc = ((Wildcard)pattern.wildcardList[k]);
+                res *= wc.size() / splittingQuotient[k];
+            }
+            return res;
         }
 
         public KeyPatternPool(KeyPattern pattern, BigInteger partsize)
         {
             this.partsize = partsize;
-            stack = new Stack<KeyPattern>();
-            makePool(pattern);
+            this.pattern = pattern;
+            splittingQuotient = new int[pattern.wildcardList.Count];
+            calculateSplitting();
+            splittingCounter = new int[pattern.wildcardList.Count];
         }
     }
 }
---
+++ CrypPlugins/KeySearcher/Wildcard.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KeySearcher
+{
+    internal class Wildcard
+    {
+        private char[] values = new char[256];
+        private int length;
+        private int counter;
+        public bool isSplit
+        {
+            get;
+            private set;
+        }
+
+        public Wildcard(string valuePattern)
+        {
+            isSplit = false;
+            counter = 0;
+            if (valuePattern.Length == 1)
+            {
+                length = 1;
+                values[0] = valuePattern[0];
+            }
+            else
+            {
+                length = 0;
+                int i = 1;
+                while (valuePattern[i] != ']')
+                {
+                    if (valuePattern[i + 1] == '-')
+                    {
+                        for (char c = valuePattern[i]; c <= valuePattern[i + 2]; c++)
+                            values[length++] = c;
+                        i += 2;
+                    }
+                    else
+                        values[length++] = valuePattern[i];
+                    i++;
+                }
+            }
+        }
+
+        public Wildcard(Wildcard wc)
+        {
+            isSplit = wc.isSplit;
+            length = wc.length;
+            counter = wc.counter;
+            for (int i = 0; i < 256; i++)
+                values[i] = wc.values[i];
+        }
+
+        public Wildcard(char[] values, int length)
+        {
+            isSplit = false;
+            this.length = length;
+            this.values = values;
+            this.counter = 0;
+        }
+
+        private Wildcard()
+        {
+        }
+
+        public Wildcard[] split()
+        {
+            if (length <= 1)
+                return null;
+            int length1 = this.length - this.counter;
+            Wildcard[] wcs = new Wildcard[2];
+            wcs[0] = new Wildcard();
+            wcs[0].counter = 0;
+            wcs[0].length = length1 / 2;
+            wcs[1] = new Wildcard();
+            wcs[1].counter = 0;
+            wcs[1].length = length1 - wcs[0].length;
+            for (int i = 0; i < wcs[0].length; i++)
+                wcs[0].values[i] = values[this.counter + i];
+            for (int i = 0; i < wcs[1].length; i++)
+                wcs[1].values[i] = values[i + this.counter + wcs[0].length];
+            wcs[0].isSplit = true;
+            wcs[1].isSplit = true;
+            return wcs;
+        }
+
+        public char getChar()
+        {
+            return values[counter];
+        }
+
+        public char getChar(int add)
+        {
+            return values[(counter + add) % length];
+        }
+
+        public bool succ()
+        {
+            counter++;
+            if (counter >= length)
+            {
+                counter = 0;
+                return true;
+            }
+            return false;
+        }
+
+        public int size()
+        {
+            return length;
+        }
+
+        public int count()
+        {
+            return counter;
+        }
+
+        public void resetCounter()
+        {
+            counter = 0;
+        }
+
+        public string getRepresentationString()
+        {
+            if (length == 1)
+                return "" + values[0];
+            string res = "[";
+            int begin = 0;
+            for (int i = 1; i < length; i++)
+            {
+                if (values[i - 1] != values[i] - 1)
+                {
+                    if (begin == i - 1)
+                        res += values[begin];
+                    else
+                    {
+                        if (i - 1 - begin == 1)
+                            res += values[begin] + "" + values[i - 1];
+                        else
+                            res += values[begin] + "-" + values[i - 1];
+                    }
+                    begin = i;
+                }
+            }
+            if (begin == length - 1)
+                res += values[begin];
+            else
+            {
+                if (length - 1 - begin == 1)
+                    res += values[begin] + "" + values[length - 1];
+                else
+                    res += values[begin] + "-" + values[length - 1];
+            }
+
+            res += "]";
+            return res;
+        }
+
+        public bool contains(Wildcard wc)
+        {
+            if (wc == null)
+                return false;
+            for (int i = 0; i < wc.length; i++)
+            {
+                bool contains = false;
+                for (int j = 0; j < this.length; j++)
+                {
+                    if (this.values[j] == wc.values[i])
+                    {
+                        contains = true;
+                        break;
+                    }
+                }
+                if (!contains)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
