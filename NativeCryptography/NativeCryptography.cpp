98f7ef11f6efbf45186ae162f4cb11a895f8f9df
rech <rech@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-02-26 01:46:36 +0000

    some cleanups and fixes
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@1196 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- NativeCryptography/NativeCryptography.cpp
+++ NativeCryptography/NativeCryptography.cpp
@@ -2,7 +2,101 @@
 
 #include "NativeCryptography.h"
 
-// This is the main DLL file.
+namespace NativeCryptography {
 
-#include "NativeCryptography.h"
+	/* Fast way to xor an AES block */
+	void Crypto::xorBlockAES(int *t1, int *t2)
+	{
+		t1[0] ^= t2[0];
+		t1[1] ^= t2[1];
+		t1[2] ^= t2[2];
+		t1[3] ^= t2[3];
+	}
 
+	/* Fast way to xor an DES block */
+	void Crypto::xorBlockDES(int *t1, int *t2)
+	{
+		t1[0] ^= t2[0];
+		t1[1] ^= t2[1];
+	}
+
+	void Crypto::encrypt(unsigned char* in, unsigned char* out, const cryptMethod method, AES_KEY* aeskey, DES_key_schedule* deskey)
+	{
+		if (method == cryptMethod::methodAES)
+			AES_encrypt(in, out, aeskey);
+		else
+			DES_ecb_encrypt((const_DES_cblock*)in, (const_DES_cblock*)out, deskey, DES_ENCRYPT);
+	}
+
+	void Crypto::decrypt(unsigned char* in, unsigned char* out, const cryptMethod method, AES_KEY* aeskey, DES_key_schedule* deskey)
+	{
+		if (method == cryptMethod::methodAES)
+			AES_decrypt(in, out, aeskey);
+		else
+			DES_ecb_encrypt((const_DES_cblock*)in, (const_DES_cblock*)out, deskey, DES_DECRYPT);
+	}
+
+
+	void Crypto::xorblock(unsigned char* t1, unsigned char* t2, const cryptMethod method) {
+		if (method == cryptMethod::methodAES)
+			xorBlockAES((int*)t1, (int*)t2);
+		else
+			xorBlockDES((int*)t1, (int*)t2); 
+	}
+
+	array<unsigned char>^ Crypto::decryptAESorDES(array<unsigned char>^ Input, array<unsigned char>^ Key, array<unsigned char>^ IV, const int bits, const int length, const int mode, const int blockSize, const cryptMethod method)
+	{
+		int numBlocks = length / blockSize;
+		if (length % blockSize != 0)
+			numBlocks++;
+
+		if (IV == nullptr)
+			IV = gcnew array<unsigned char>(blockSize);
+
+		pin_ptr<unsigned char> input = &Input[0];
+		pin_ptr<unsigned char> key = &Key[0];
+		pin_ptr<unsigned char> iv = &IV[0];
+
+		array<unsigned char>^ output = gcnew array<unsigned char>(length);
+		pin_ptr<unsigned char> outp = &output[0];	
+
+		AES_KEY aeskey;
+		DES_key_schedule deskey;
+		if (mode == 2)	//CFB
+		{
+			if (method == cryptMethod::methodAES) 
+				AES_set_encrypt_key(key, bits, &aeskey);
+			else 
+				DES_set_key_unchecked((const_DES_cblock*)key, &deskey);
+
+			encrypt(iv, outp, method, &aeskey, &deskey);
+			xorblock(outp, input, method);
+
+			for (int c = 0; c < numBlocks-1; c++)
+			{
+				encrypt(input+c*blockSize, outp+(c+1)*blockSize, method, &aeskey, &deskey);					
+				xorblock(outp+(c+1)*blockSize, input+(c+1)*blockSize, method);
+			}
+		}
+		else	//CBC or ECB
+		{
+			if (method == cryptMethod::methodAES)
+				AES_set_decrypt_key(key, bits, &aeskey);
+			else
+				DES_set_key_unchecked((const_DES_cblock*)key, &deskey);
+
+			decrypt(input, outp, method, &aeskey, &deskey);				
+			if (mode == 1)		//CBC
+				xorblock(outp, iv, method);	
+			for (int c = 1; c < numBlocks; c++)
+			{
+				decrypt(input+c*blockSize, outp+c*blockSize, method, &aeskey, &deskey);
+				if (mode == 1)		//CBC
+					xorblock(outp+c*blockSize, input+(c-1)*blockSize, method);				
+			}
+		}
+
+		return output;
+	}
+
+}
--- NativeCryptography/NativeCryptography.h
+++ NativeCryptography/NativeCryptography.h
@@ -10,108 +10,26 @@ namespace NativeCryptography {
 	public ref class Crypto
 	{
 	private:
-		static void xorBlockAES(int *t1, int *t2)
-		{
-			t1[0] ^= t2[0];
-			t1[1] ^= t2[1];
-			t1[2] ^= t2[2];
-			t1[3] ^= t2[3];
-		}
-
-		static void xorBlockDES(int *t1, int *t2)
-		{
-			t1[0] ^= t2[0];
-			t1[1] ^= t2[1];
-		}
+		enum class cryptMethod {methodAES, methodDES};
+		
+		static void xorBlockAES(int *t1, int *t2);
+		static void xorBlockDES(int *t1, int *t2);
+		static void encrypt(unsigned char* in, unsigned char* out, const cryptMethod method, AES_KEY* aeskey, DES_key_schedule* deskey);
+		static void decrypt(unsigned char* in, unsigned char* out, const cryptMethod method, AES_KEY* aeskey, DES_key_schedule* deskey);
+		static void xorblock(unsigned char* t1, unsigned char* t2, const cryptMethod method);
+		static array<unsigned char>^ decryptAESorDES(array<unsigned char>^ Input, array<unsigned char>^ Key, array<unsigned char>^ IV, const int bits, const int length, const int mode, const int blockSize, const cryptMethod method);
 
 	public:
 		static array<unsigned char>^ decryptAES(array<unsigned char>^ Input, array<unsigned char>^ Key, array<unsigned char>^ IV, const int bits, const int length, const int mode)
 		{
 			const int blockSize = 16;
-			int numBlocks = length / blockSize;
-			if (length % blockSize != 0)
-				numBlocks++;
-
-			pin_ptr<unsigned char> input = &Input[0];
-			pin_ptr<unsigned char> key = &Key[0];
-			pin_ptr<unsigned char> iv = &IV[0];
-
-			array<unsigned char>^ output = gcnew array<unsigned char>(length);
-			pin_ptr<unsigned char> outp = &output[0];	
-
-			AES_KEY aeskey;			
-			if (mode == 2)	//CFB
-			{
-				AES_set_encrypt_key(key, bits, &aeskey);
-
-				AES_encrypt(iv, outp, &aeskey);
-				xorBlockAES((int*)(outp), (int*)(input));
-
-				for (int c = 0; c < numBlocks-1; c++)
-				{
-					AES_encrypt((input+c*blockSize), outp+(c+1)*blockSize, &aeskey);					
-					xorBlockAES((int*)(outp+(c+1)*blockSize), (int*)(input+(c+1)*blockSize));
-				}
-			}
-			else
-			{
-				AES_set_decrypt_key(key, bits, &aeskey);
-				AES_decrypt(input, outp, &aeskey);
-				if (mode == 1)		//CBC
-					xorBlockAES((int*)(outp), (int*)iv);	
-				for (int c = 1; c < numBlocks; c++)
-				{
-					AES_decrypt((input+c*blockSize), outp+c*blockSize, &aeskey);
-					if (mode == 1)		//CBC
-						xorBlockAES((int*)(outp+c*blockSize), (int*)(input+(c-1)*blockSize));				
-				}
-			}
-
-			return output;
+			return decryptAESorDES(Input, Key, IV, bits, length, mode, blockSize, cryptMethod::methodAES);
 		}
 
 		static array<unsigned char>^ decryptDES(array<unsigned char>^ Input, array<unsigned char>^ Key, array<unsigned char>^ IV, const int length, const int mode)
 		{
 			const int blockSize = 8;
-			int numBlocks = length / blockSize;
-			if (length % blockSize != 0)
-				numBlocks++;
-
-			pin_ptr<unsigned char> input = &Input[0];
-			pin_ptr<unsigned char> key = &Key[0];
-			pin_ptr<unsigned char> iv = &IV[0];
-
-			array<unsigned char>^ output = gcnew array<unsigned char>(length);
-			pin_ptr<unsigned char> outp = &output[0];			
-
-			DES_key_schedule deskey;
-			DES_set_key_unchecked((const_DES_cblock*)key, &deskey);
-
-			if (mode == 2)	//CFB
-			{				
-				DES_ecb_encrypt((const_DES_cblock*)iv, (const_DES_cblock*)outp, &deskey, DES_ENCRYPT);
-				xorBlockAES((int*)(outp), (int*)(input));
-
-				for (int c = 0; c < numBlocks-1; c++)
-				{
-					DES_ecb_encrypt((const_DES_cblock*)(input+c*blockSize), (const_DES_cblock*)(outp+(c+1)*blockSize), &deskey, DES_ENCRYPT);
-					xorBlockDES((int*)(outp+(c+1)*blockSize), (int*)(input+(c+1)*blockSize));
-				}
-			}
-			else
-			{
-				DES_ecb_encrypt((const_DES_cblock*)input, (const_DES_cblock*)outp, &deskey, DES_DECRYPT);
-				if (mode == 1)		//CBC
-					xorBlockDES((int*)(outp), (int*)iv);	
-				for (int c = 1; c < numBlocks; c++)
-				{
-					DES_ecb_encrypt((const_DES_cblock*)(input+c*blockSize), (const_DES_cblock*)(outp+c*blockSize), &deskey, DES_DECRYPT);
-					if (mode == 1)		//CBC
-						xorBlockDES((int*)(outp+c*blockSize), (int*)(input+(c-1)*blockSize));
-				}
-			}
-
-			return output;
+			return decryptAESorDES(Input, Key, IV, 0, length, mode, blockSize, cryptMethod::methodDES);
 		}
 	};
 }
