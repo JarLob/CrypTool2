e1d07c5ccd77f1a388fbd240ca1423971a09f8c5
podeszwa <podeszwa@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-02-28 14:49:42 +0000

    PublicKey Controller Interface added
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@1217 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPluginBase/Control/IControlPublicKey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cryptool.PluginBase.Control
+{
+    public interface IControlPublicKey:IControl
+
+    {
+        string publicKey { get; set; }
+        void setPublicKey(string publicKey);
+
+    }
+}
