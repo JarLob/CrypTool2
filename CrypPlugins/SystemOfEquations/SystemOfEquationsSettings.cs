8d5d3da5d4165d39334dd957ad94fa754485b25b
bourbahh <bourbahh@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-04-25 00:03:32 +0000

    new Plugins for algebraic attack on Stream cipher : 1) compute XZ: compute the sets XZ that one need to generate a system of equations for algebraic attack 2) compute annihilators: compute annihilators of the sets XZ (Z-funktions) and annihilators for boolean function 3) system of equations: use Z-functions and known keystream to generate a system of equations that can be used for an algebraic attack for more details see plugins descriptions
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@1352 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/SystemOfEquations/SystemOfEquationsSettings.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using Cryptool.PluginBase;
+using Cryptool.PluginBase.IO;
+using Cryptool.PluginBase.Miscellaneous;
+
+namespace Cryptool.SystemOfEquations
+{
+    class SystemOfEquationsSettings : ISettings
+    {
+        #region Private variables
+        private string keystream;
+        private string feedbackpolynomials;
+        private string lfsrsoutputs;
+        private bool hasChanges;
+        #endregion
+        #region ISettings Members
+        [TaskPane("Feedback polynomials of LFSRs", "Feedback polynomials of LFSRs in bit presentation ", null, 1, false, DisplayLevel.Beginner, ControlType.TextBox, ValidationType.RegEx, null)]
+        public string Feedbackpolynomials
+        {
+            get { return this.feedbackpolynomials; }
+            set
+            {
+                if (value != feedbackpolynomials) hasChanges = true;
+                {
+                    this.feedbackpolynomials = value;
+                    OnPropertyChanged("Feedbackpolynomials");
+                }
+            }
+        }
+        [TaskPane("Output cells of LFSRs", " Output  cells of LFSRS in bit presentation ", null, 2, false, DisplayLevel.Beginner, ControlType.TextBox, ValidationType.RegEx, null)]
+        public string Lfsrsoutputs
+        {
+            get { return this.lfsrsoutputs; }
+            set
+            {
+                if (value != lfsrsoutputs)
+                {
+                    this.lfsrsoutputs = value;
+                    OnPropertyChanged("Lfsrsoutputs");
+                }
+            }
+        }
+        [TaskPane("Keystream sequences", "known keystream sequences", null, 3, false, DisplayLevel.Beginner, ControlType.TextBox, ValidationType.RegEx, null)]
+        public string Keystream
+        {
+            get { return this.keystream; }
+            set
+            {
+                if (value != keystream)
+                {
+                    this.keystream = value;
+                    OnPropertyChanged("Keystream");
+                }
+            }
+        }
+        public delegate void SystemOfEquationsLogMessage(string msg, NotificationLevel logLevel);
+        public event SystemOfEquationsLogMessage LogMessage;
+        public bool HasChanges
+        {
+            get { return hasChanges; }
+            set { hasChanges = value; }
+        }
+        #endregion
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(String name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        #endregion
+    }
+}
