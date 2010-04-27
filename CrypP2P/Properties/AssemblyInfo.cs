049d097736f3dd439e40a4fd3a6649f575fa19c2
lelgemann <lelgemann@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-04-27 07:44:41 +0000

    o Fixed AssemblyInfo: according to guidelines
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@1366 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypP2P/Properties/AssemblyInfo.cs
+++ CrypP2P/Properties/AssemblyInfo.cs
@@ -1,36 +1,27 @@
-﻿using System.Reflection;
+﻿/*
+   Copyright 2010 Paul Lelgemann, University of Duisburg-Essen
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Allgemeine Informationen über eine Assembly werden über die folgenden 
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die mit einer Assembly verknüpft sind.
-[assembly: AssemblyTitle("CrypP2P")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("CrypP2P")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
-// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
-// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: AssemblyTitle("CrypTool CrypP2P")]
+[assembly: AssemblyCompany("Lehrstuhl Verteilte Systeme - Universität Duisburg-Essen")]
+[assembly: AssemblyProduct("CrypTool CrypP2P")]
+[assembly: AssemblyCopyright("Copyright © Paul Lelgemann 2010")]
 [assembly: ComVisible(false)]
-
-// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: AssemblyVersion("2.0.*")]
 [assembly: Guid("4207e092-bf1f-4795-9473-6acf9805ff34")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
