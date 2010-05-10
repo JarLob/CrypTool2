0f67c0377180c0fcd69c133543f46050471ed441
saternus <saternus@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-05-10 14:30:56 +0000

    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@1416 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypStartup/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace CrypStartup
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Cryptool.CrypWin.MainWindow wnd = new Cryptool.CrypWin.MainWindow();
+            wnd.Show();
+        }
+    }
+}
