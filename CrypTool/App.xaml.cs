b4f9e3b7eeed878a855f399668eef8a22c124cd7
saternus <saternus@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-05-11 14:37:44 +0000

    Add files
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@1422 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypTool/App.xaml.cs
+++ CrypTool/App.xaml.cs
@@ -4,6 +4,8 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using System.Threading;
+using Cryptool.Core;
 
 namespace CrypTool
 {
@@ -12,5 +14,50 @@ namespace CrypTool
     /// </summary>
     public partial class App : Application
     {
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            new Thread(LoadPlugins).Start();
+        }
+
+        private void LoadPlugins()
+        {
+            PluginManager pluginMgr = new PluginManager();
+            pluginMgr.OnPluginLoaded += new CrypCorePluginLoadedHandler(OnPluginLoaded);
+            Dictionary<string, Type> plugins = pluginMgr.LoadTypes(AssemblySigningRequirement.LoadAllAssemblies);
+            OpenMainWindow(plugins);
+        }
+
+        private void OnPluginLoaded(object sender, PluginLoadedEventArgs args)
+        {
+            if (!Application.Current.Dispatcher.CheckAccess())
+            {
+                Application.Current.Dispatcher.BeginInvoke(new Action<Object, PluginLoadedEventArgs>(OnPluginLoaded), sender, args);
+            }
+            else
+            {
+                SplashScreen screen = (SplashScreen)this.MainWindow;
+                screen.Text = args.AssemblyName;
+                screen.Value = args.CurrentPluginNumber == 0 ? 0 : (int)((double)args.CurrentPluginNumber / (double)args.NumberPluginsFound * 100);
+            }
+        }
+
+        private void OpenMainWindow(Dictionary<string, Type> plugins)
+        {
+
+            if (!Application.Current.Dispatcher.CheckAccess())
+            {
+                Application.Current.Dispatcher.BeginInvoke(new Action<Dictionary<string, Type>>(OpenMainWindow), plugins);
+            }
+            else
+            {
+                SplashScreen screen = (SplashScreen)this.MainWindow;
+                MainWindow wnd = new CrypTool.MainWindow();
+                wnd.LoadedTypes = plugins;
+                Application.Current.MainWindow = wnd;
+                
+                screen.Close();
+                Application.Current.MainWindow.Show();
+            }
+        }
     }
 }
--- CrypTool/MainWindow.xaml.cs
+++ CrypTool/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Cryptool.PluginBase;
 
 namespace CrypTool
 {
@@ -19,9 +20,52 @@ namespace CrypTool
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Dictionary<string, List<Type>> currentPlugins = new Dictionary<string, List<Type>>();
+
+        
+
+        
+        public Dictionary<string, Type> LoadedTypes { get; set; }
+        
+
+
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+
+
+
+        private void LoadPlugins()
+        {
+            foreach (Type pluginType in this.LoadedTypes.Values)
+            {
+                if (pluginType.GetEditorSpecificPluginAttribute() != null)
+                    continue;
+                
+                foreach (string interfaceName in PluginExtension.Interfaces)
+                {
+                    if (pluginType.GetInterface(interfaceName) != null)
+                    {
+                        if (!this.currentPlugins.ContainsKey(interfaceName))
+                            this.currentPlugins.Add(interfaceName, new List<Type>());
+                        this.currentPlugins[interfaceName].Add(pluginType);
+
+
+
+                    }
+                }
+            }
+
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadPlugins();
+        }
+
     }
 }
