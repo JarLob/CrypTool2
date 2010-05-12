b342e622774122f93311f63a7d7d886e0c70464f
matkovic <matkovic@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-05-12 00:35:03 +0000

    - Open source Ribbon implemented (for Saternus) - LoadPlugins() problems(?) - added open source Ribbon dll
    override-bad-extension: Odyssey.dll
    
    
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@1423 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypTool/MainWindow.xaml.cs
+++ CrypTool/MainWindow.xaml.cs
@@ -11,61 +11,56 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Cryptool.PluginBase;
+using Odyssey.Controls;
+using Odyssey.Controls.Classes;
 
 namespace CrypTool
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : RibbonWindow
     {
         private Dictionary<string, List<Type>> currentPlugins = new Dictionary<string, List<Type>>();
 
-        
-
-        
         public Dictionary<string, Type> LoadedTypes { get; set; }
-        
-
-
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-
-
+        private void RibbonButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (SkinManager.SkinId == SkinId.OfficeBlue)
+                SkinManager.SkinId = SkinId.OfficeBlack;
+            else
+                SkinManager.SkinId = SkinId.OfficeBlue;
+        }
 
         private void LoadPlugins()
         {
-            foreach (Type pluginType in this.LoadedTypes.Values)
-            {
-                if (pluginType.GetEditorSpecificPluginAttribute() != null)
-                    continue;
-                
-                foreach (string interfaceName in PluginExtension.Interfaces)
-                {
-                    if (pluginType.GetInterface(interfaceName) != null)
-                    {
-                        if (!this.currentPlugins.ContainsKey(interfaceName))
-                            this.currentPlugins.Add(interfaceName, new List<Type>());
-                        this.currentPlugins[interfaceName].Add(pluginType);
+            //foreach (Type pluginType in this.LoadedTypes.Values)
+            //{
+            //    if (pluginType.GetEditorSpecificPluginAttribute() != null)
+            //        continue;
 
+            //    foreach (string interfaceName in PluginExtension.Interfaces)
+            //    {
+            //        if (pluginType.GetInterface(interfaceName) != null)
+            //        {
+            //            if (!this.currentPlugins.ContainsKey(interfaceName))
+            //                this.currentPlugins.Add(interfaceName, new List<Type>());
 
-
-                    }
-                }
-            }
-
-
+            //            this.currentPlugins[interfaceName].Add(pluginType);
+            //        }
+            //    }
+            //}
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             LoadPlugins();
         }
-
     }
 }
