653e862011afc1ab672ddcf256a1e2353c7c03e0
rech <rech@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-05-20 22:43:20 +0000

    some additional BigIntegerHelper stuff (added probable prime test)
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@1455 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPluginBase/Miscellaneous/BigIntegerHelper.cs
+++ CrypPluginBase/Miscellaneous/BigIntegerHelper.cs
@@ -196,13 +196,13 @@ namespace Cryptool.PluginBase.Miscellaneous
         }
 
         /*
-         * Returns the modulo inverse of this.  Throws ArithmeticException if
+         * Returns the modulo inverse of "input".  Throws ArithmeticException if
          * the inverse does not exist.  (i.e. gcd(this, modulus) != 1)
          * 
-         * This method is taken from the BigInteger class of
+         * This method is taken from the BigInteger class written by
          * Chew Keong TAN (source: http://www.codeproject.com/KB/cs/biginteger.aspx)
          * (but modified by us)
-        */
+         */
 
         public static BigInteger ModInverse(BigInteger input, BigInteger modulus)
         {
@@ -250,11 +250,103 @@ namespace Cryptool.PluginBase.Miscellaneous
             return result;
         }
 
-        
-        public static bool isProbablePrime(BigInteger p)
+        #region primesBelow2000
+        // primes smaller than 2000 to test the generated prime number (taken from BigInteger class written by Chew Keong TAN)
+
+        public static readonly int[] primesBelow2000 = {
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+            101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199,
+	    211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293,
+	    307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397,
+	    401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499,
+	    503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599,
+	    601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691,
+	    701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,
+	    809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887,
+	    907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997,
+	    1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097,
+	    1103, 1109, 1117, 1123, 1129, 1151, 1153, 1163, 1171, 1181, 1187, 1193,
+	    1201, 1213, 1217, 1223, 1229, 1231, 1237, 1249, 1259, 1277, 1279, 1283, 1289, 1291, 1297,
+	    1301, 1303, 1307, 1319, 1321, 1327, 1361, 1367, 1373, 1381, 1399,
+	    1409, 1423, 1427, 1429, 1433, 1439, 1447, 1451, 1453, 1459, 1471, 1481, 1483, 1487, 1489, 1493, 1499,
+	    1511, 1523, 1531, 1543, 1549, 1553, 1559, 1567, 1571, 1579, 1583, 1597,
+	    1601, 1607, 1609, 1613, 1619, 1621, 1627, 1637, 1657, 1663, 1667, 1669, 1693, 1697, 1699,
+	    1709, 1721, 1723, 1733, 1741, 1747, 1753, 1759, 1777, 1783, 1787, 1789,
+	    1801, 1811, 1823, 1831, 1847, 1861, 1867, 1871, 1873, 1877, 1879, 1889,
+	    1901, 1907, 1913, 1931, 1933, 1949, 1951, 1973, 1979, 1987, 1993, 1997, 1999 };
+
+        #endregion
+
+        /*
+         * This code is heavily inspired by the code from the BigInteger class written by Chew Keong TAN
+         */
+        public static bool isProbablePrime(BigInteger thisVal)
         {
-            return true;    //TODO: Implement this!!
-            throw new NotImplementedException();
+            thisVal = BigInteger.Abs(thisVal);
+
+            //test small numbers
+            if (thisVal == 0 || thisVal == 1)
+                return false;
+            if (thisVal == 2 || thisVal == 3)
+                return true;            
+
+            if (thisVal.IsEven)     // even numbers
+                return false;
+
+            // test for divisibility by primes < 2000
+            for (int p = 0; p < primesBelow2000.Length; p++)
+            {
+                BigInteger divisor = primesBelow2000[p];
+
+                if (divisor >= thisVal)
+                    break;
+
+                BigInteger resultNum = thisVal % divisor;
+                if (resultNum == 0)                
+                    return false;                
+            }
+
+            // Perform BASE 2 Rabin-Miller Test
+
+            // calculate values of s and t
+            int s = 0;
+
+            BigInteger t = thisVal - 1;
+            while ((t & 0x01) == 0)     //TODO: This could be implemented more efficient
+            {
+                t = t >> 1;
+                s++;
+            }
+
+            BigInteger a = 2;
+
+            // b = a^t mod p
+            BigInteger b = BigInteger.ModPow(a, t, thisVal);
+            bool result = false;
+
+            if (b == 1)         // a^t mod p = 1
+                result = true;
+
+            BigInteger p_sub1 = thisVal - 1;
+            for (int j = 0; result == false && j < s; j++)
+            {
+                if (b == p_sub1)         // a^((2^j)*t) mod p = p-1 for some 0 <= j <= s-1
+                {
+                    result = true;
+                    break;
+                }
+
+                b = (b * b) % thisVal;
+            }
+
+            /*  TODO: Implement this:
+            // if number is strong pseudoprime to base 2, then do a strong lucas test
+            if (result)
+                result = LucasStrongTestHelper(thisVal);
+            */
+
+            return result;
         }
+
     }
 }
