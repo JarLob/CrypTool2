f3e359ba739ae9a4b52a9a6ee12961c267c823cf
wander <wander@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-05-26 17:54:12 +0000

    Updated plugin template. The template shall become the default way to create a new CT2 plugin.
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@1507 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/CrypPluginTemplate/ExamplePluginCT2.cs
@@ -0,0 +1,166 @@
+﻿/* HOWTO: Change year, author name and organization.
+   Copyright 2010 Your Name, University of Duckburg
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cryptool.PluginBase.IO;
+using Cryptool.PluginBase;
+using System.ComponentModel;
+using Cryptool.PluginBase.Miscellaneous;
+using System.Windows.Controls;
+
+namespace Cryptool.Plugin
+{
+    // HOWTO: Change author name, email address, organization and URL.
+    [Author("Matthäus Wander", "wander@cryptool.org", "Universität Duisburg-Essen, Fachgebiet Verteilte Systeme", "http://www.vs.uni-due.de")]
+    // HOWTO: Change plugin caption (title to appear in CT2) and tooltip. You can (and should) provide a description XAML and an own icon as well.
+    [PluginInfo(false, "Example Plugin", "Subtract one number from another", null, "CrypWin/images/default.png")]
+    // HOWTO: Change interface to one that fits to your plugin (see CrypPluginBase).
+    public class ExamplePluginCT2 : IIOMisc
+    {
+        #region Private Variables
+
+        // HOWTO: You need to adapt the settings class as well, see the corresponding file.
+        private readonly ExamplePluginCT2Settings settings = new ExamplePluginCT2Settings();
+
+        #endregion
+
+        #region Data Properties
+
+        /// <summary>
+        /// HOWTO: Input interface to read the input data. 
+        /// You can add more input properties of other type if needed.
+        /// </summary>
+        [PropertyInfo(Direction.InputData, "Minuend", "Input number", null, DisplayLevel.Beginner)]
+        public int Minuend
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// HOWTO: Output interface to write the output data.
+        /// You can add more output properties ot other type if needed.
+        /// </summary>
+        [PropertyInfo(Direction.OutputData, "Difference", "Result", null, DisplayLevel.Beginner)]
+        public int Difference
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region IPlugin Members
+
+        public ISettings Settings
+        {
+            get { return settings; }
+        }
+
+        /// <summary>
+        /// HOWTO: You can provide a custom (tabbed) presentation to visualize your algorithm.
+        /// Return null if you don't provide one.
+        /// </summary>
+        public UserControl Presentation
+        {
+            get { return null; }
+        }
+
+        /// <summary>
+        /// HOWTO: You can provide custom (quickwatch) presentation to visualize your algorithm.
+        /// Return null if you don't provide one.
+        /// </summary>
+        public UserControl QuickWatchPresentation
+        {
+            get { return null; }
+        }
+
+        public void PreExecution()
+        {
+        }
+
+        /// <summary>
+        /// HOWTO: Enter the algorithm you'd like to implement in this method.
+        /// </summary>
+        public void Execute()
+        {
+            // HOWTO: Use this to show the progress of a plugin algorithm execution in the editor.
+            ProgressChanged(0, 1);
+
+            // HOWTO: After you have changed an output property, make sure you announce the name of the changed property to the CT2 core.
+            Difference = Minuend - settings.Subtrahend;
+            OnPropertyChanged("Difference");
+
+            // HOWTO: You can pass error, warning, info or debug messages to the CT2 main window.
+            if (settings.Subtrahend < 0)
+                GuiLogMessage("Subtrahend is negative", NotificationLevel.Debug);
+
+            // HOWTO: Make sure the progress bar is at maximum when your Execute() finished successfully.
+            ProgressChanged(1, 1);
+        }
+
+        public void PostExecution()
+        {
+        }
+
+        public void Pause()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+
+        public void Initialize()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+        #endregion
+
+        #region Event Handling
+
+        public event StatusChangedEventHandler OnPluginStatusChanged;
+
+        public event GuiLogNotificationEventHandler OnGuiLogNotificationOccured;
+
+        public event PluginProgressChangedEventHandler OnPluginProgressChanged;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void GuiLogMessage(string message, NotificationLevel logLevel)
+        {
+            EventsHelper.GuiLogMessage(OnGuiLogNotificationOccured, this, new GuiLogEventArgs(message, this, logLevel));
+        }
+
+        private void OnPropertyChanged(string name)
+        {
+            EventsHelper.PropertyChanged(PropertyChanged, this, new PropertyChangedEventArgs(name));
+        }
+
+        private void ProgressChanged(double value, double max)
+        {
+            EventsHelper.ProgressChanged(OnPluginProgressChanged, this, new PluginProgressEventArgs(value, max));
+        }
+
+        #endregion
+    }
+}
---
+++ CrypPlugins/CrypPluginTemplate/ExamplePluginCT2Settings.cs
@@ -0,0 +1,86 @@
+﻿/* HOWTO: Change year, author name and organization.
+   Copyright 2010 Your Name, University of Duckburg
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cryptool.PluginBase;
+using System.ComponentModel;
+
+namespace Cryptool.Plugin
+{
+    public class ExamplePluginCT2Settings : ISettings
+    {
+        #region Private Variables
+
+        private bool hasChanges = false;
+        private int subtrahend = 0;
+
+        #endregion
+
+        #region TaskPane Settings
+
+        /// <summary>
+        /// HOWTO: This is an example for a setting entity shown in the settings pane on the right of the CT2 main window.
+        /// This example setting uses a number field input, but there are many more input types available, see ControlType enumeration.
+        /// </summary>
+        [TaskPane("Subtrahend", "Amount to subtract from input number", null, 1, false, DisplayLevel.Beginner, ControlType.NumericUpDown, ValidationType.RangeInteger, 0, Int32.MaxValue)]
+        public int Subtrahend
+        {
+            get
+            {
+                return subtrahend;
+            }
+            set
+            {
+                // HOWTO: If a setting changes, you must set hasChanges manually to true.
+                if (subtrahend != value)
+                {
+                    subtrahend = value;
+                    hasChanges = true;
+                }
+            }
+        }
+
+        #endregion
+
+        #region ISettings Members
+
+        /// <summary>
+        /// HOWTO: This flags indicates whether some setting has been changed since the last save.
+        /// If a property was changed, this becomes true, hence CrypTool will ask automatically if you want to save your changes.
+        /// </summary>
+        public bool HasChanges
+        {
+            get
+            {
+                return hasChanges;
+            }
+            set
+            {
+                hasChanges = value;
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
