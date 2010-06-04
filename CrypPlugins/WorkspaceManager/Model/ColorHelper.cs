eb2f2f842ff445cad67d046a1ade8626bcaa6fc7
kopal <kopal@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-06-04 14:22:22 +0000

    added ColorHelper (gives colors for data types of connectors/connections)
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@1586 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/WorkspaceManager/Model/ColorHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Windows.Media;
+
+namespace WorkspaceManager.Model
+{
+    public class ColorHelper
+    {       
+        /// <summary>
+        /// Returns a Color for a given Type
+        /// 
+        /// example:
+        ///     System.String -> Colors.LightGray;
+        ///     
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static Color getColor(Type type)
+        {
+            if (type.FullName == "System.String")
+            {                
+                return Colors.LightGray;
+            }
+            else if (   type.FullName == "System.Int16" || 
+                        type.FullName == "System.Int32" ||
+                        type.FullName == "System.Int64" || 
+                        type.FullName == "System.Int16[]" || 
+                        type.FullName == "System.Int32[]" ||
+                        type.FullName == "System.Int64[]")
+            {
+                return Colors.SkyBlue;
+            }
+            else if (type.FullName == "Cryptool.PluginBase.IO.CryptoolStream")
+            {
+                return Colors.Orange;
+            }
+            else if (type.FullName == "System.Byte" || type.FullName == "System.Byte[]")
+            {
+                return Colors.DarkGreen;
+            }
+            else if (type.FullName == "System.Boolean" || type.FullName == "System.Boolean[]")
+            {
+                return Colors.DarkRed;
+            }
+            else if (type.FullName == "System.Numerics.BigInteger")
+            {
+                return Colors.Purple;
+            }
+            else
+            {
+                return Colors.Black;
+            }
+        }
+    }
+}
