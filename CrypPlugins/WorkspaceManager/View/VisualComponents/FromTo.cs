649981535872ceea4332b7660aba9e68f3256737
matkovic <matkovic@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-06-15 06:52:56 +0000

    -added missing file
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@1641 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/WorkspaceManager/View/VisualComponents/FromTo.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace WorkspaceManager.View.VisualComponents
+{
+    class FromTo
+    {
+        public Point From { get; private set; }
+        public Point To { get; private set; }
+        public SortedSet<Point> Intersection { get; private set; }
+
+        public FromTo(Point from, Point to)
+        {
+            this.From = from;
+            this.To = to;
+            this.Intersection = new SortedSet<Point>(new InLineSorter(From, To));
+        }
+
+    }
+
+    class InLineSorter : IComparer<Point>
+    {
+        private enum DirSort
+        {
+            X_DESC,
+            Y_DESC,
+            X_ASC,
+            Y_ASC
+        };
+
+        private DirSort dirSort;
+
+        public InLineSorter(Point From, Point To)
+        {
+            if (From.X == To.X)
+            {
+                if (From.Y > To.Y)
+                    dirSort = DirSort.Y_ASC;
+                else
+                    dirSort = DirSort.Y_DESC;
+            }
+            else if (From.Y == To.Y)
+            {
+                if (From.X > To.X)
+                    dirSort = DirSort.X_ASC;
+                else
+                    dirSort = DirSort.X_DESC;
+            }
+            else
+                throw new Exception("90° only");
+        }
+
+        // Returns:
+        //     Value               Meaning
+        //  Less than zero      a is less than b
+        //  Zero                a equals b
+        //  Greater than zero   a is greater than b
+        public int Compare(Point a, Point b)
+        {
+            switch (dirSort)
+            {
+                case DirSort.Y_DESC:
+                    return a.Y.CompareTo(b.Y);
+                case DirSort.Y_ASC:
+                    return b.Y.CompareTo(a.Y);
+                case DirSort.X_DESC:
+                    return a.X.CompareTo(b.X);
+                case DirSort.X_ASC:
+                    return b.X.CompareTo(a.X);
+                default:
+                    throw new Exception("error");
+            }
+        }
+    }
+}
