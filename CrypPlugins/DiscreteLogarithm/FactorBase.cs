ccf60a555d3bdd2a3dfa869a68258a2a124f6dac
rech <rech@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-08-10 18:43:32 +0000

    Some work on discrete logarithm stuff (preparation for index calculus algorithm implementation)
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@1799 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/DiscreteLogarithm/FactorBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace DiscreteLogarithm
+{
+    class FactorBase
+    {
+        private List<int> primes;
+
+        /**
+         * Generates the factor base.
+         **/
+        public void Generate(int max)
+        {
+            primes = new List<int>();
+
+            /** Implementation of sieve of Eratosthenes **/
+            bool[] sieve = new bool[max];
+            for (int c = 2; c < max; c++)
+                sieve[c] = true;
+
+            for (int c = 2; c < max; c++)
+            {
+                if (sieve[c])
+                    primes.Add(c);
+                for (int i = c + 1; i < max; i++)
+                {
+                    if (i % c == 0)
+                        sieve[i] = false;
+                }
+            }
+        }
+
+        /**
+         * Tries to factor parameter "number" with the factor base and returns an array giving the relations.
+         **/
+        public int[] Factorize(BigInteger number)
+        {
+            int[] result = new int[primes.Count];
+            for (int c = 0; c < primes.Count; c++)
+            {
+                while (number % primes[c] == 0)
+                {
+                    result[c]++;
+                    number /= primes[c];
+                }
+            }
+
+            if (number != 1)
+                return null;
+
+            return result;
+        }
+
+    }
+}
---
+++ CrypPlugins/DiscreteLogarithm/LinearSystemOfEquations.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Diagnostics;
+using Cryptool.PluginBase.Miscellaneous;
+
+namespace DiscreteLogarithm
+{
+    class LinearSystemOfEquations
+    {
+        private BigInteger mod;
+        private BigInteger size;
+        private List<int[]> matrix;
+
+        public LinearSystemOfEquations(BigInteger mod, BigInteger size)
+        {
+            this.mod = mod;
+            this.size = size;
+            matrix = new List<int[]>((int)size);
+        }
+
+        public void AddEquation(int[] coefficients, int b)
+        {
+            Debug.Assert(coefficients.Length == size);            
+
+            int[] row = new int[coefficients.Length + 1];
+            for (int c = 0; c < coefficients.Length; c++)
+                row[c] = coefficients[c];
+            row[row.Length - 1] = b;
+
+            if (!LinearDependent(row))
+                matrix.Add(row);
+        }
+
+        private bool LinearDependent(int[] row)
+        {
+            foreach (int[] mr in matrix)
+            {
+                bool linear = true;
+                BigInteger row0invers = BigIntegerHelper.ModInverse(row[0], mod);
+                BigInteger mr0 = new BigInteger(mr[0]);
+                BigInteger div = (mr0 * row0invers) % mod;
+                for (int i = 1; i < size + 1; i++)
+                {
+                    if (row[i] * div != mr[0])
+                    {
+                        linear = false;
+                        break;
+                    }
+                }
+                if (linear)
+                    return true;
+            }
+            return false;
+        }
+
+        public bool MoreEquations()
+        {
+            return (matrix.Count < size);
+        }
+
+        public int[] Solve()
+        {
+            //TODO: Gauss here
+        }
+
+    }
+}
