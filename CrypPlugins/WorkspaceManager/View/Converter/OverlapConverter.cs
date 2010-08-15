b085ed68f9bcfdd339afe186e7dd6ff0bc0408b4
matkovic <matkovic@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-08-15 23:07:22 +0000

    -fixed input control bug
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@1813 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/WorkspaceManager/View/Converter/OverlapConverter.cs
+++ CrypPlugins/WorkspaceManager/View/Converter/OverlapConverter.cs
@@ -5,38 +5,38 @@ using System.Text;
 using System.Windows.Data;
 using WorkspaceManager.View.Container;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace WorkspaceManager.View.Converter
 {
-    class ViewStateConverter : IValueConverter
+    class OverlapConverter : IValueConverter
     {
+        #region IValueConverter Members
+
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             PluginViewState state = (PluginViewState)value;
             String caption = (String)parameter;
             if (caption == "PresentationPanel" && state == PluginViewState.Presentation)
-                return Visibility.Visible;
+                return 1000;
 
             if (caption == "SettingsPanel" && state == PluginViewState.Setting)
-                return Visibility.Visible;
+                return 1000;
 
             if (caption == "LogPanel" && state == PluginViewState.Log)
-                return Visibility.Visible;
+                return 1000;
 
             if (caption == "DataPanel" && state == PluginViewState.Data)
-                return Visibility.Visible;
+                return 1000;
 
-            if(caption == "OptionPanel" && state == PluginViewState.Min)
-                return Visibility.Collapsed;
-            else if(caption == "OptionPanel" && state != PluginViewState.Min)
-                return Visibility.Visible;
-
-            return Visibility.Hidden;
+            return 0;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             throw new NotImplementedException();
         }
+
+        #endregion
     }
 }
