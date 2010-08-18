60d1426e785eb80b1dce969170c300021d4a2722
rech <rech@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-08-18 12:45:56 +0000

    added missing file
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@1832 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/DiscreteLogarithm/NotInvertibleException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiscreteLogarithm
+{    
+    class NotInvertibleException : Exception
+    {
+        public int NotInvertibleNumber
+        {
+            get;
+            private set;
+        }
+
+        public NotInvertibleException(int notInvertible)
+        {
+            NotInvertibleNumber = notInvertible;
+        }
+    }
+}
