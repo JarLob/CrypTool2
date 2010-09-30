caa7076da99b7877872c62b63ad9482da0351db1
gomez <gomez@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-09-30 17:16:03 +0000

    Begin of the plugin SZ42 German Lorenz Machine until now the plugin works fine but need to be test it
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@1941 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/SZ42/Lorenz.cs
@@ -0,0 +1,306 @@
+﻿/* HOWTO: Change year, author name and organization.
+   Copyright 2010 Your Name, University of Duckburg
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cryptool.PluginBase;
+using System.ComponentModel;
+using Cryptool.PluginBase.IO;
+using Cryptool.PluginBase.Miscellaneous;
+using System.Windows.Controls;
+using Cryptool.PluginBase.Cryptography;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Cryptool.Plugins.SZ42
+{
+    [Author("Wilmer Daza", "mr.wadg@gmail.com", "University of Magdalena", "http://www.unimagdalena.edu.co")]
+    //[PluginInfo(false, "Lorenz SZ42", "Represents the SZ42 Lorenz machine", null, "CrypWin/images/default.png")]
+    //[PluginInfo(false, "Lorenz SZ42", "Represents the SZ42 Lorenz machine", "SZ42/DetailedDescription/Description.xaml", "SZ42/Images/sz42.png")]
+    [PluginInfo(false, "Lorenz SZ42", "Represents the SZ42 Lorenz machine", "", "SZ42/Images/sz42.png")]
+    [EncryptionType(EncryptionType.Classic)]
+    
+    public class Lorenz : IEncryption
+    {
+        #region Private Variables
+
+        private readonly LorenzSettings settings;
+        private string inputString;
+        private string outputString;
+        private string parsedString;
+        private bool first = true;
+        private bool isPlayMode = false;
+        private List<CryptoolStream> listCryptoolStreamsOut = new List<CryptoolStream>();
+        private SZ42 sz42Encrypt, sz42Decrypt, reset;
+        private BinaryFormatter bf;
+        private MemoryStream ms;
+
+        #endregion
+
+        #region Public Interface
+
+        public Lorenz() 
+        {
+            settings = new LorenzSettings();
+
+            reset = new SZ42();
+
+            //ConfigMachine(reset);
+
+            settings.LogMessage += Lorenz_LogMessage;
+            settings.ReExecute += Lorenz_ReExecute;
+        }
+
+        #endregion
+
+        #region Data Properties
+
+        [PropertyInfo(Direction.InputData, "Text Input", "Input a string to be processed by the SZ42 cipher", null, false, false, DisplayLevel.Beginner, QuickWatchFormat.Text, null)]
+        public string InputString
+        {
+            get { return this.inputString; }
+            set
+            {
+                if (value != inputString)
+                {
+                    this.inputString = value;
+                    OnPropertyChanged("InputString");
+                }
+            }
+        }
+
+        [PropertyInfo(Direction.OutputData, "Text output", "The string after processing with the SZ42 cipher", "", false, false, DisplayLevel.Beginner, QuickWatchFormat.Text, null)]
+        public string OutputString
+        {
+            get { return this.outputString; }
+            set
+            {
+                this.outputString = value;
+                OnPropertyChanged("OutputString");
+            }
+        }
+
+        #endregion
+
+        #region IPlugin Members
+
+        public ISettings Settings
+        {
+            get { return settings; }
+        }
+
+        /// <summary>
+        /// HOWTO: You can provide a custom (tabbed) presentation to visualize your algorithm.
+        /// Return null if you don't provide one.
+        /// </summary>
+        public UserControl Presentation
+        {
+            get { return null; }
+        }
+
+        /// <summary>
+        /// HOWTO: You can provide custom (quickwatch) presentation to visualize your algorithm.
+        /// Return null if you don't provide one.
+        /// </summary>
+        public UserControl QuickWatchPresentation
+        {
+            get { return null; }
+        }
+
+        public void PreExecution()
+        {
+            Dispose();
+        }
+
+        public void Execute()
+        {
+            if (first)
+            {
+                ConfigMachine(reset);
+                first = false;
+            }
+
+            isPlayMode = true;
+
+            if (!string.IsNullOrEmpty(inputString))
+            {
+                if (settings.Action == 0)
+                {
+                    Lorenz_LogMessage("encrypting", NotificationLevel.Info);
+                    sz42Encrypt = ResetMachine();
+                    Encrypt();
+                }
+                else if (settings.Action == 1)
+                {
+                    Lorenz_LogMessage("decrypting", NotificationLevel.Info);
+                    sz42Decrypt = ResetMachine();
+                    Decrypt();
+                }
+
+                OnPropertyChanged("OutputString");
+            }
+        }
+
+        private void Encrypt() 
+        {
+            if (settings.Limitation == 1)
+                sz42Encrypt.TheresLimitation = false;
+            else
+                sz42Encrypt.TheresLimitation = true;
+
+            sz42Encrypt.Trace.Clear();
+
+            if (settings.InputParsed)
+                parsedString = inputString;
+            else
+                parsedString = sz42Encrypt.ParseInput(inputString);
+
+            outputString = sz42Encrypt.ActionMachine(parsedString);
+        }
+
+        private void Decrypt() 
+        {
+            if (settings.Limitation == 1)
+                sz42Decrypt.TheresLimitation = false;
+            else
+                sz42Decrypt.TheresLimitation = true;
+
+            sz42Decrypt.Trace.Clear();
+
+            parsedString = sz42Decrypt.ActionMachine(inputString);
+
+            if (settings.OutputParsed)
+                outputString = parsedString;
+            else
+                outputString = sz42Decrypt.ParseOutput(parsedString);
+        }
+
+        private SZ42 ResetMachine() 
+        {
+            ms = new MemoryStream();
+            bf = new BinaryFormatter();
+            bf.Serialize(ms, reset);
+            ms.Position = 0;
+            SZ42 sz42 = (SZ42)bf.Deserialize(ms);
+            ms.Close();
+            return sz42;
+        }
+
+        private void ConfigMachine(SZ42 machine)
+        {
+            string[] positions, patterns;
+            int i = 0;
+
+            positions = settings.Position.Split(new string[] { "," }, 12, StringSplitOptions.None);
+
+            patterns = settings.Pattern.Split(new string[] { "," }, 12, StringSplitOptions.None);
+
+            foreach (Wheel wheel in machine.ChiWheels)
+            {
+                wheel.CurrentPosition = Convert.ToInt32(positions[i]);
+                wheel.Pattern = patterns[i].ToCharArray();
+                i++;
+            }
+
+            foreach (Wheel wheel in machine.PsiWheels)
+            {
+                wheel.CurrentPosition = Convert.ToInt32(positions[i]);
+                wheel.Pattern = patterns[i].ToCharArray();
+                i++;
+            }
+
+            foreach (Wheel wheel in machine.MuWheels)
+            {
+                wheel.CurrentPosition = Convert.ToInt32(positions[i]);
+                wheel.Pattern = patterns[i].ToCharArray();
+                i++;
+            }
+        }
+
+        /// <summary>
+        /// Handles re-execution events from settings class
+        /// </summary>
+        private void Lorenz_ReExecute()
+        {
+            if (isPlayMode)
+                Execute();
+        }
+
+        /// <summary>
+        /// Handles log messages from the settings class
+        /// </summary>
+        private void Lorenz_LogMessage(string msg, NotificationLevel loglevel)
+        {
+            EventsHelper.GuiLogMessage(OnGuiLogNotificationOccured, this, new GuiLogEventArgs(msg, this, loglevel));
+        }
+
+        public void PostExecution()
+        {
+            isPlayMode = false;
+            Dispose();
+        }
+
+        public void Pause()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+
+        public void Initialize()
+        {
+        }
+
+        public void Dispose()
+        {
+            foreach (CryptoolStream stream in listCryptoolStreamsOut)
+                stream.Close();
+            
+            listCryptoolStreamsOut.Clear();
+        }
+
+        #endregion
+
+        #region Event Handling
+
+        public event StatusChangedEventHandler OnPluginStatusChanged;
+
+        public event GuiLogNotificationEventHandler OnGuiLogNotificationOccured;
+
+        public event PluginProgressChangedEventHandler OnPluginProgressChanged;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void GuiLogMessage(string message, NotificationLevel logLevel)
+        {
+            EventsHelper.GuiLogMessage(OnGuiLogNotificationOccured, this, new GuiLogEventArgs(message, this, logLevel));
+        }
+
+        private void OnPropertyChanged(string name)
+        {
+            EventsHelper.PropertyChanged(PropertyChanged, this, new PropertyChangedEventArgs(name));
+        }
+
+        private void ProgressChanged(double value, double max)
+        {
+            EventsHelper.ProgressChanged(OnPluginProgressChanged, this, new PluginProgressEventArgs(value, max));
+        }
+
+        #endregion
+    }
+}
---
+++ CrypPlugins/SZ42/LorenzSettings.cs
@@ -0,0 +1,192 @@
+﻿/* HOWTO: Change year, author name and organization.
+   Copyright 2010 Your Name, University of Duckburg
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cryptool.PluginBase;
+using System.ComponentModel;
+
+namespace Cryptool.Plugins.SZ42
+{
+    public class LorenzSettings : ISettings
+    {
+        #region Private Variables
+
+        private int limitation = 0;
+        private string pattern = "";
+        private string position = "";
+        private int action = 0;
+        private bool inputParsed = false;
+        private bool outputParsed = false;
+        private bool hasChanges = false;
+
+        #endregion
+
+        #region Public Interface
+
+        /// <summary>
+        /// We use this delegate to send log messages from the settings class to the Caesar plugin
+        /// </summary>
+        public delegate void LorenzLogMessage(string msg, NotificationLevel loglevel);
+        public event LorenzLogMessage LogMessage;
+        public delegate void LorenzReExecute();
+        public event LorenzReExecute ReExecute;
+
+        #endregion
+
+        #region TaskPane Settings
+
+        [TaskPane("Action", "Select the Algorithm action", null, 0, true, DisplayLevel.Beginner, ControlType.ComboBox, new string[] { "Encrypt", "Decrypt" })]
+        public int Action
+        {
+            get
+            {
+                return this.action;
+            }
+            set
+            {
+                if (value != action) HasChanges = true;
+                this.action = value;
+                OnPropertyChanged("Action");
+
+                if (ReExecute != null) ReExecute();
+            }
+        }
+
+        [TaskPane("Limitation", "Select the limitation type", null, 1, true, DisplayLevel.Beginner, ControlType.ComboBox, new string[] { "Chi2 One Back", "None" })]
+        public int Limitation
+        {
+            get
+            {
+                return this.limitation;
+            }
+            set
+            {
+                if (value != limitation) HasChanges = true;
+                this.limitation = value;
+                OnPropertyChanged("Limitation");
+
+                if (ReExecute != null) ReExecute();
+            }
+        }
+
+        [TaskPane("Wheels Pattern", "Set the wheels pattern", null, 2, true, DisplayLevel.Beginner, ControlType.TextBox, "")]
+        public string Pattern
+        {
+            get
+            {
+                return this.pattern;
+            }
+            set
+            {
+                if (value != pattern) HasChanges = true;
+                this.pattern = value;
+                OnPropertyChanged("Pattern");
+
+                if (ReExecute != null) ReExecute();
+            }
+        }
+
+        [TaskPane("Wheels Position", "Set the wheels position", null, 3, true, DisplayLevel.Beginner, ControlType.TextBox, "")]
+        public string Position
+        {
+            get
+            {
+                return this.position;
+            }
+            set
+            {
+                if (value != position) HasChanges = true;
+                this.position = value;
+                OnPropertyChanged("Position");
+
+                if (ReExecute != null) ReExecute();
+            }
+        }
+
+        [TaskPane("Input Parsed", "Set if the input is parsed or not", null, 4, true, DisplayLevel.Beginner, ControlType.CheckBox, "")]
+        public bool InputParsed
+        {
+            get
+            {
+                return this.inputParsed;
+            }
+            set
+            {
+                if (value != inputParsed) HasChanges = true;
+                this.inputParsed = value;
+                OnPropertyChanged("InputParsed");
+
+                if (ReExecute != null) ReExecute();
+            }
+        }
+
+        [TaskPane("Output Parsed", "Set if the output is parsed or not", null, 5, true, DisplayLevel.Beginner, ControlType.CheckBox, "")]
+        public bool OutputParsed
+        {
+            get
+            {
+                return this.outputParsed;
+            }
+            set
+            {
+                if (value != outputParsed) HasChanges = true;
+                this.outputParsed = value;
+                OnPropertyChanged("OutputParsed");
+
+                if (ReExecute != null) ReExecute();
+            }
+        }
+        
+
+        #endregion
+
+        #region ISettings Members
+
+        /// <summary>
+        /// HOWTO: This flags indicates whether some setting has been changed since the last save.
+        /// If a property was changed, this becomes true, hence CrypTool will ask automatically if you want to save your changes.
+        /// </summary>
+        public bool HasChanges
+        {
+            get
+            {
+                return hasChanges;
+            }
+            set
+            {
+                hasChanges = value;
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        #endregion
+    }
+}
