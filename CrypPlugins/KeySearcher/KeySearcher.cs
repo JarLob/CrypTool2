662bfa28022f33c85eae9ceeede341709f1c594c
rech <rech@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-11-14 20:30:16 +0000

    better keys/sec representation in KeySearcher
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@2091 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/KeySearcher/KeySearcher.cs
+++ CrypPlugins/KeySearcher/KeySearcher.cs
@@ -852,7 +852,7 @@ namespace KeySearcher
                 localQuickWatchPresentation.Dispatcher.BeginInvoke(DispatcherPriority.Normal, (SendOrPostCallback)delegate
                 {
                     localQuickWatchPresentation.elapsedTime.Content = localBruteForceStopwatch.Elapsed;
-                    localQuickWatchPresentation.keysPerSecond.Content = "" + keysPerSecond;
+                    localQuickWatchPresentation.keysPerSecond.Content = String.Format("{0:N}", keysPerSecond);
                     if (timeleft != new TimeSpan(-1))
                     {
                         localQuickWatchPresentation.timeLeft.Content = "" + timeleft;
