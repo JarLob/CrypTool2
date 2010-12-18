ceca1276583eef6479187c06b33f25a40d184ea8
rech <rech@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-12-18 22:00:39 +0000

    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@2223 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPluginBase/Attributes/SettingsTabAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+
+namespace Cryptool.PluginBase.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class SettingsTabAttribute : Attribute
+    {
+        public string ResourceFile { get; private set; }
+        public string Caption { get; private set; }
+        public string Address { get; private set; }
+
+        public SettingsTabAttribute(string resourceFile, string caption, string address)
+        {
+            ResourceFile = resourceFile;
+            Caption = caption;
+            Address = address;
+        }
+    }
+}
---
+++ CrypPlugins/Wizard/WizardSettingsTab.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Cryptool.PluginBase.Attributes;
+
+namespace Wizard
+{
+    /// <summary>
+    /// Interaction logic for WizardSettingsTab.xaml
+    /// </summary>
+    [SettingsTab("Wizard.Resources.settingsRes", "WizardSettings", "/EditorSettings/")]
+    public partial class WizardSettingsTab : UserControl
+    {
+        public WizardSettingsTab(Style settingsStyle)
+        {
+            Resources.Add("settingsStyle", settingsStyle);
+            InitializeComponent();
+        }
+    }
+}
