32829d55109c7c1ba19e73bb462e1969137673b5
nolte <nolte@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2010-12-22 12:45:11 +0000

    Versionnumber fix + bonus feature for combining pos and neg MaschineIDs in the statistic from earlier calculations. (DON'T USE YET! Final Tests)
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@2242 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/KeySearcher/KeySearcher.cs
+++ CrypPlugins/KeySearcher/KeySearcher.cs
@@ -1236,22 +1236,28 @@ namespace KeySearcher
                     {
                         //get the statistic maschcount for this avatarname
                         Dictionary<long, Information> statMaschCount = statistic[avname];
-                        
+                        //ID of the statistic (has to be always positive)
+                        var mID = id;
+                        if(id < 0)
+                        {
+                            mID = id * (-1);
+                        }
+
                         //if the id of the Maschine already exists for this avatarname
-                        if (statMaschCount.ContainsKey(id))
+                        if (statMaschCount.ContainsKey(mID))
                         {
                             if (!initialized || ((MaschCount[id].Count == 1) && (MaschCount.Keys.Count == 1)))
                             {
-                                statMaschCount[id].Count = statMaschCount[id].Count + MaschCount[id].Count;
-                                statMaschCount[id].Hostname = MaschCount[id].Hostname;
-                                statMaschCount[id].Date = MaschCount[id].Date;
+                                statMaschCount[mID].Count = statMaschCount[mID].Count + MaschCount[id].Count;
+                                statMaschCount[mID].Hostname = MaschCount[id].Hostname;
+                                statMaschCount[mID].Date = MaschCount[id].Date;
                                 statistic[avname] = statMaschCount;
                             }
                         }
                         else
                         {
-                            //add a new id,count value for this avatarname
-                            statistic[avname].Add(id, MaschCount[id]);
+                            //add a new id,information value for this avatarname
+                            statistic[avname].Add(mID, MaschCount[id]);
                         }
                     }
                 }
