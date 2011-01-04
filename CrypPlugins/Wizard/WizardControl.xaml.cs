fd08b1e9e9b35bbf4f4c2bebac6c339d162820ed
sauer <sauer@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2011-01-04 12:45:59 +0000

    added preliminary configuration XML files for the wizard
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@2284 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/Wizard/WizardControl.xaml.cs
+++ CrypPlugins/Wizard/WizardControl.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
+using System.IO;
+using System.Reflection;
 
 namespace Wizard
 {
@@ -19,9 +22,80 @@ namespace Wizard
     /// </summary>
     public partial class WizardControl : UserControl
     {
+
+        private string culture = System.Globalization.CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
+        private const string configXMLPath = "Wizard.Config.wizard.config.start.xml";
+        private XElement wizardConfigXML;
+
         public WizardControl()
         {
+            try
+            {
+                // DEBUG HELP string[] names = this.GetType().Assembly.GetManifestResourceNames();
+
+                Stream fileStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(configXMLPath);
+                XElement xml = XElement.Load(fileStream);
+                GenerateXML(xml);
+            }
+            catch (Exception)
+            {
+                
+            }
             InitializeComponent();
         }
+
+        // Generate the full XML tree for the wizard 
+        private void GenerateXML(XElement xml)
+        {
+            try
+            {
+                //find all nested subcategories and add them to the tree
+                IEnumerable<XElement> categories = xml.Elements("category");
+                if (categories.Any())
+                {
+                    foreach (XElement cat in categories)
+                    {
+                        IEnumerable<XElement> files = cat.Elements("file");
+                        if (files.Any())
+                        {
+                            foreach (XElement element in files)
+                            {
+                                XAttribute att = element.Attribute("resource");
+                                if (att != null)
+                                {
+                                    string path = "Wizard.Config." + att.Value;
+                                    Stream fileStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
+                                    XElement sub = XElement.Load(fileStream);
+                                    IEnumerable<XElement> elems = sub.Elements();
+                                    if (elems.Any())
+                                    {
+                                        foreach (XElement ele in elems)
+                                        {
+                                            cat.Add(ele);
+                                        } 
+                                    }
+                                }
+                            }
+                        }
+
+                        GenerateXML(cat);
+
+                    } 
+                }
+
+                wizardConfigXML = xml;
+
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+
+        private void GetCategoryList()
+        {
+
+        }
+
     }
 }
