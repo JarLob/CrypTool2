772c74a70eb12dab3e5e1d3644fe178869584a23
sauer <sauer@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2011-01-05 17:49:17 +0000

    Wizard: * added functionality to click through the first categories (not fully configured yet)
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@2290 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/Wizard/WizardControl.xaml.cs
+++ CrypPlugins/Wizard/WizardControl.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Shapes;
 using System.Xml.Linq;
 using System.IO;
 using System.Reflection;
+using System.Windows.Threading;
+using System.Threading;
 
 namespace Wizard
 {
@@ -23,7 +25,6 @@ namespace Wizard
     public partial class WizardControl : UserControl
     {
 
-        private string culture = System.Globalization.CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
         private const string configXMLPath = "Wizard.Config.wizard.config.start.xml";
         private const string defaultLang = "en";
         private XElement wizardConfigXML;
@@ -43,6 +44,7 @@ namespace Wizard
                 
             }
             InitializeComponent();
+            SetupPage(wizardConfigXML);
         }
 
         // generate the full XML tree for the wizard (recursive)
@@ -91,9 +93,146 @@ namespace Wizard
             }
         }
 
-        private void GetCategoryList()
+        private void SetupPage(XElement element)
         {
+            XElement parent = element.Parent;
+            if (parent == null)
+            {
+                backButton.IsEnabled = false;
+                abortButton.IsEnabled = false;
+            }
+            else
+            {
+                backButton.IsEnabled = true;
+                abortButton.IsEnabled = true;
+            }
 
+            nextButton.IsEnabled = false;
+
+            string currentLang = System.Globalization.CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
+
+            //set headline
+            XElement headline = FindElementInElement(element, "headline");
+            if (headline != null)
+                taskHeader.Content = headline.Value;
+
+            //set description label
+            XElement desc = FindElementInElement(element, "desc");
+            if (desc != null)
+                descHeader.Content = desc.Value;
+
+            //generate radio buttons
+            IEnumerable<XElement> categories = element.Elements("category");
+            IEnumerable<XElement> items = element.Elements("item");
+
+            if (items.Any())
+                categories.Union(items);
+
+            if (categories.Any())
+            {
+                foreach (XElement ele in categories)
+                {
+                    Label l = new Label();
+                    l.Height = 30;
+                    XElement label = FindElementInElement(ele, "name");
+                    if (label != null)
+                        l.Content = label.Value;
+
+                    //Image i = new Image(); TODO
+
+                    StackPanel sp = new StackPanel();
+                    sp.VerticalAlignment = VerticalAlignment.Stretch;
+                    sp.HorizontalAlignment = HorizontalAlignment.Stretch;
+                    sp.Orientation = Orientation.Horizontal;
+                    //add image
+                    sp.Children.Add(l);
+
+                    RadioButton rb = new RadioButton();
+                    rb.HorizontalAlignment = HorizontalAlignment.Stretch;
+                    rb.HorizontalContentAlignment = HorizontalAlignment.Left;
+                    rb.Checked += rb_Checked;
+                    rb.Content = sp;
+                    rb.Tag = ele;
+
+                    radioButtonStackPanel.Children.Add(rb);
+                }
+            }
+
+        }
+
+        void rb_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton b = (RadioButton)sender;
+            XElement ele = (XElement)b.Tag;
+            XElement desc = FindElementInElement(ele, "description");
+            if (desc != null)
+                description.Text = desc.Value;
+            nextButton.IsEnabled = true;
+        }
+
+        //finds elements according to the current language
+        private XElement FindElementInElement(XElement element, string xname)
+        {
+            string currentLang = System.Globalization.CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
+            XElement foundElement = null;
+
+            IEnumerable<XElement> descriptions = element.Elements(xname);
+            if (descriptions.Any())
+            {
+                var description = from descln in descriptions where descln.Attribute("lang").Value == currentLang select descln;
+                if (!description.Any())
+                {
+                    description = from descln in descriptions where descln.Attribute("lang").Value == defaultLang select descln;
+                    if (description.Any())
+                        foundElement = description.First();
+                }
+                else
+                    foundElement = description.First();
+            }
+
+            return foundElement;
+        }
+
+        private void nextButton_Click(object sender, RoutedEventArgs e)
+        {
+            UIElement[] uiea = new UIElement[radioButtonStackPanel.Children.Count];
+            radioButtonStackPanel.Children.CopyTo(uiea, 0);
+            foreach (UIElement uie in uiea)
+            {
+                RadioButton b = (RadioButton)uie;
+                if (b.IsChecked != null && (bool)b.IsChecked)
+                {
+                    radioButtonStackPanel.Children.Clear();
+                    description.Text = "";
+                    SetupPage((XElement)b.Tag);
+                    break;
+                }
+            }
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            UIElement[] uiea = new UIElement[radioButtonStackPanel.Children.Count];
+            radioButtonStackPanel.Children.CopyTo(uiea, 0);
+            if (uiea.Length > 0)
+            {
+                RadioButton b = (RadioButton)uiea[0];
+                XElement ele = (XElement)b.Tag;
+                radioButtonStackPanel.Children.Clear();
+                description.Text = "";
+                XElement grandParent = ele.Parent.Parent;
+                if (grandParent != null)
+                    SetupPage(grandParent);
+                else
+                    SetupPage(wizardConfigXML); 
+            }
+        }
+
+        private void abortButton_Click(object sender, RoutedEventArgs e)
+        {
+            radioButtonStackPanel.Children.Clear();
+            description.Text = "";
+            SetupPage(wizardConfigXML);
         }
 
     }
