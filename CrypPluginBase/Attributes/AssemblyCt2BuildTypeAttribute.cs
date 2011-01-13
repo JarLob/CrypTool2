5ebb978a20dddd6cfa12ec3095126ce491fbba84
wander <wander@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2011-01-13 13:02:00 +0000

    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@2323 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPluginBase/Attributes/AssemblyCt2BuildTypeAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Cryptool.PluginBase.Attributes
+{
+    public enum Ct2BuildType
+    {
+        Developer = 0,
+        Nightly = 1,
+        Beta = 2,
+        Release = 3
+    }
+
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class AssemblyCt2BuildTypeAttribute : Attribute
+    {
+        public Ct2BuildType BuildType
+        {
+            get; set;
+        }
+
+        public AssemblyCt2BuildTypeAttribute(Ct2BuildType type)
+        {
+            this.BuildType = type;
+        }
+    }
+}
---
+++ CrypPluginBase/Miscellaneous/AssemblyHelper.cs
@@ -0,0 +1,75 @@
+﻿/*
+   Copyright 2011 Matthäus Wander, University of Duisburg-Essen
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Cryptool.PluginBase.Attributes;
+
+namespace Cryptool.PluginBase.Miscellaneous
+{
+    /*
+     * Access assembly information of entry assembly (starting exe).
+     */
+    public class AssemblyHelper
+    {
+        public static Ct2BuildType BuildType
+        {
+            get; private set;
+        }
+
+        public static string ProductName
+        {
+            get; private set;
+        }
+
+        public static Version Version
+        {
+            get; private set;
+        }
+
+        static AssemblyHelper()
+        {
+            {
+                object[] attributes =
+                    Assembly.GetEntryAssembly().GetCustomAttributes(typeof (AssemblyCt2BuildTypeAttribute), false);
+                if (attributes != null && attributes.Length >= 1)
+                {
+                    AssemblyCt2BuildTypeAttribute attr = (AssemblyCt2BuildTypeAttribute) attributes[0];
+                    AssemblyHelper.BuildType = attr.BuildType;
+                }
+                else
+                {
+                    AssemblyHelper.BuildType = Ct2BuildType.Developer;
+                }
+            }
+            {
+                object[] attributes =
+                    Assembly.GetEntryAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+                if (attributes != null && attributes.Length >= 1)
+                {
+                    AssemblyProductAttribute attr = (AssemblyProductAttribute) attributes[0];
+                    AssemblyHelper.ProductName = attr.Product;
+                }
+            }
+            {
+                AssemblyHelper.Version = Assembly.GetEntryAssembly().GetName().Version;
+            }
+        }
+    }
+}
