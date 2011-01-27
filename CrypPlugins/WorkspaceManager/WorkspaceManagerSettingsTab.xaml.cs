607b90bc3e9de3a73c4a8c12f33a8e5a561667fc
rech <rech@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2011-01-27 21:05:08 +0000

    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@2508 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/WorkspaceManager/WorkspaceManagerSettingsTab.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Cryptool.PluginBase.Attributes;
+
+namespace WorkspaceManager
+{
+    /// <summary>
+    /// Interaction logic for WorkspaceManagerSettingsTab.xaml
+    /// </summary>
+    [Localization("WorkspaceManager.Properties.Resources")]
+    [SettingsTab("WorkspaceManagerSettings", "/MainSettings/")]
+    public partial class WorkspaceManagerSettingsTab : UserControl
+    {
+        public WorkspaceManagerSettingsTab(Style settingsStyle)
+        {
+            Resources.Add("settingsStyle", settingsStyle);
+            InitializeComponent();
+        }
+    }
+}
