7d183b68fcbcd7c9a0f4f65b7fefededc5b765fb
rech <rech@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2011-03-17 19:24:15 +0000

    A lot of WPF stuff in P2PEditor JobDisplay. Colors are ugly now, someone has to fix this
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@3005 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/P2PEditor/Converters/PercentageConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Cryptool.P2PEditor.Converters
+{
+    [ValueConversion(typeof (Double), typeof (Double))]
+    public class PercentageConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter,
+                              CultureInfo culture)
+        {
+            double percentage = Double.Parse((string) parameter);
+            double val = (double)value;
+            return val*(percentage/100);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+                                  CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+}
