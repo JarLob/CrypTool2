a89c7cb657040b63d5fdf0e20663802e729752e9
rech <rech@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2011-03-24 18:58:24 +0000

    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@3037 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPluginBase/Attributes/ContextMenuAttribute.cs
+++ CrypPluginBase/Attributes/ContextMenuAttribute.cs
@@ -21,12 +21,54 @@ namespace Cryptool.PluginBase
     [AttributeUsage(AttributeTargets.Property)]
     public class ContextMenuAttribute : Attribute
     {
-        public readonly string Caption;
-        public readonly string ToolTip;        
+        private readonly string caption;
+        public string Caption
+        {
+            get
+            {
+                if (MultiLanguage && caption != null)
+                    return PluginType.GetPluginStringResource(caption);
+                else
+                    return caption;
+            }
+        }
+
+        private readonly string toolTip;
+        public string ToolTip
+        {
+            get
+            {
+                if (MultiLanguage && toolTip != null)
+                    return PluginType.GetPluginStringResource(toolTip);
+                else
+                    return toolTip;
+            }
+        }
+        
         public readonly int Order;
         public readonly ContextMenuControlType ControlType;        
         public readonly string[] ControlValues;
         public readonly int[] ArrImagesForControlValues;
+
+        # region translation helpers
+        private Type pluginType;
+
+        /// <summary>
+        /// Gets or sets the type of the plugin. This value is set by extension method if ResourceFile exists. 
+        /// It is used to access the plugins resources to translate the text elements.
+        /// </summary>
+        /// <value>The type of the plugin.</value>
+        public Type PluginType
+        {
+            get { return pluginType; }
+            set { pluginType = value; }
+        }
+
+        private bool MultiLanguage
+        {
+            get { return PluginType != null; }
+        }
+        # endregion translation helpers
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ContextMenuAttribute"/> class.
@@ -39,8 +81,8 @@ namespace Cryptool.PluginBase
         /// <param name="controlValues">Strings for the context menu entries.</param>
         public ContextMenuAttribute(string caption, string toolTip, int order, ContextMenuControlType controlType, int[] arrImagesForControlValues, params string[] controlValues)
         {
-            this.Caption = caption;
-            this.ToolTip = toolTip;
+            this.caption = caption;
+            this.toolTip = toolTip;
             this.Order = order;            
             this.ControlType = controlType;
             this.ControlValues = controlValues;
