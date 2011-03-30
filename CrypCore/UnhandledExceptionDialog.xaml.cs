91e7bd801ed357778a2873b4897c98390997831f
rech <rech@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2011-03-30 07:12:45 +0000

    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@3066 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypCore/UnhandledExceptionDialog.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Cryptool.Core
+{
+    /// <summary>
+    /// Interaction logic for UnhandledExceptionDialog.xaml
+    /// </summary>
+    public partial class UnhandledExceptionDialog : Window
+    {
+        public UnhandledExceptionDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Not implemented yet!");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        public static void ShowModalDialog(Exception e)
+        {
+            var unhandledExceptionDialog = new UnhandledExceptionDialog();
+            unhandledExceptionDialog.ExceptionNameLabel.Content = e.GetType().FullName;
+            unhandledExceptionDialog.ExceptionMessageLabel.Content = e.Message;
+            unhandledExceptionDialog.StackTraceBox.Text = e.StackTrace;
+            unhandledExceptionDialog.ShowDialog();
+        }
+    }
+}
