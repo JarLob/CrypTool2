d104b759043156f518574601cd94379d637d6194
rech <rech@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2011-03-30 07:14:17 +0000

    * integrated unhandled exception dialog into CrypStartup (refs #238) * new exes
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@3067 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypStartup/App.xaml.cs
+++ CrypStartup/App.xaml.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using Cryptool.Core;
+using Application = System.Windows.Application;
 
 namespace CrypStartup
 {
@@ -12,6 +17,28 @@ namespace CrypStartup
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            System.Windows.Forms.Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            System.Windows.Forms.Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Dispatcher.Invoke(DispatcherPriority.Normal, (SendOrPostCallback)delegate
+            {
+                UnhandledExceptionDialog.ShowModalDialog((Exception)e.ExceptionObject);
+            }, null);
+        }
+
+
+
+        private void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            UnhandledExceptionDialog.ShowModalDialog(e.Exception);
+        }
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             long x, y;
