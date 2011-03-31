a518ec757396dd76aaa1727e763b14c72f8ee58e
rech <rech@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2011-03-31 15:35:55 +0000

    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@3083 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypCore/Mailer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Cryptool.Core
+{
+    public static class Mailer
+    {
+        public static void SendMailToCoreDevs(string title, string text)
+        {
+            var client = new WebClient();
+            client.Headers["User-Agent"] = "CrypTool";
+            var stream = client.OpenWrite("http://www.cryptool.org/cgi/ct2devmail");
+
+            var postMessage = Encoding.ASCII.GetBytes(string.Format("title={0}&text={1}", Uri.EscapeDataString(title), Uri.EscapeDataString(text)));
+            stream.Write(postMessage, 0, postMessage.Length);
+            stream.Close();
+        }
+    }
+}
--- CrypCore/UnhandledExceptionDialog.xaml.cs
+++ CrypCore/UnhandledExceptionDialog.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,27 +19,80 @@ namespace Cryptool.Core
     /// </summary>
     public partial class UnhandledExceptionDialog : Window
     {
-        public UnhandledExceptionDialog()
+        private readonly Exception _e;
+        private readonly Version _version;
+        private readonly string _buildType;
+        private readonly string _productName;
+
+        public UnhandledExceptionDialog(Exception e, Version version, string buildType, string productName)
         {
+            _e = e;
+            _version = version;
+            _buildType = buildType;
+            _productName = productName;
             InitializeComponent();
+            ExceptionNameLabel.Content = e.GetType().FullName;
+            ExceptionMessageLabel.Content = e.Message;
+            StackTraceBox.Text = e.StackTrace;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs routedEventArgs)
         {
-            MessageBox.Show("Not implemented yet!");
+            var button = MessageBoxButton.YesNo;
+            var icon = MessageBoxImage.Question;
+            var res = MessageBox.Show("Do you want to report these error informations to the CrypTool 2.0 developers? This is fully anonymous!", "Report error", button, icon);
+            if (res == MessageBoxResult.Yes)
+            {
+                var sb = new StringBuilder();
+                sb.AppendLine(string.Format("Exception at {0} (UTC time):", DateTime.UtcNow));
+                sb.AppendLine(_e.ToString());
+                sb.AppendLine("");
+                sb.AppendLine("-");
+                sb.AppendLine("System infos:");
+                sb.AppendLine(GetSystemInfos());
+
+                try
+                {
+                    Mailer.SendMailToCoreDevs("Crash report!", sb.ToString());
+                    MessageBox.Show("The error has been reported. Thank you!", "Reporting done");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Error trying to report!", "Reporting failed");
+                }
+            }
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Button_Click_1(object sender, RoutedEventArgs routedEventArgs)
         {
             Close();
         }
 
-        public static void ShowModalDialog(Exception e)
+        private string GetSystemInfos()
         {
-            var unhandledExceptionDialog = new UnhandledExceptionDialog();
-            unhandledExceptionDialog.ExceptionNameLabel.Content = e.GetType().FullName;
-            unhandledExceptionDialog.ExceptionMessageLabel.Content = e.Message;
-            unhandledExceptionDialog.StackTraceBox.Text = e.StackTrace;
+            var pricipal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+            var hasAdministrativeRight = pricipal.IsInRole(WindowsBuiltInRole.Administrator);
+
+            var sb = new StringBuilder();
+            sb.AppendLine(string.Format("User Name: {0}", System.Environment.UserName));
+            sb.AppendLine(string.Format("Operating System: {0}", System.Environment.OSVersion.ToString()));
+            sb.AppendLine(string.Format("Plattform: {0}", Environment.OSVersion.Platform));
+            sb.AppendLine(string.Format("Machine Name: {0}", System.Environment.MachineName));
+            sb.AppendLine(string.Format("Processors: {0}", System.Environment.ProcessorCount));
+            sb.AppendLine(string.Format("Process Info: {0}", (System.Environment.Is64BitProcess ? "64 Bit" : "32 Bit")));
+            sb.AppendLine(string.Format("Administrative Rights: {0}", hasAdministrativeRight));
+            sb.AppendLine(string.Format("Current culture: {0}", CultureInfo.CurrentCulture.Name));
+            sb.AppendLine(string.Format("CrypTool version: {0}", _version));
+            sb.AppendLine(string.Format("Build type: {0}", _buildType));
+            sb.AppendLine(string.Format("Product name: {0}", _productName));
+            sb.AppendLine(string.Format("Common language runtime version: {0}", Environment.Version));
+
+            return sb.ToString();
+        }
+
+        public static void ShowModalDialog(Exception e, Version version, string buildType, string productName)
+        {
+            var unhandledExceptionDialog = new UnhandledExceptionDialog(e, version, buildType, productName);
             unhandledExceptionDialog.ShowDialog();
         }
     }
