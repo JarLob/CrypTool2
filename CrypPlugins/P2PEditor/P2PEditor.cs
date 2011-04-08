5de541f542eee67eaf405be411b7894e69f48e2c
rech <rech@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2011-04-08 13:58:46 +0000

    * fixed P2PEditor caption translation * fixed DevTestMethods project file (caused copying some project dlls to the wrong place, which made the P2PEditor translation error "unvisible" because of dll confusion)
    Please remove your CrypBuild directory.
    
    
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@3163 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/P2PEditor/P2PEditor.cs
+++ CrypPlugins/P2PEditor/P2PEditor.cs
@@ -31,7 +31,7 @@ namespace Cryptool.P2PEditor
     [TabColor("orange")]
     [EditorInfo("p2p")]
     [Author("Paul Lelgemann", "lelgemann@cryptool.org", "Uni Duisburg-Essen", "http://www.uni-due.de")]
-    [PluginInfo("Cryptool.P2PEditor.Resources.Attributes", false,"PluginCaption", "PluginTooltip", "PluginDescriptionURL", 
+    [PluginInfo("Cryptool.P2PEditor.Properties.Resources", false, "PluginCaption", "PluginTooltip", "PluginDescriptionURL", 
         "P2PEditor/images/icon.png")]
     public class P2PEditor : IEditor
     {
