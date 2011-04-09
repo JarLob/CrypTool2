52d40ab1e4d9837e71259642d4c3dea73bee7472
rech <rech@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2011-04-09 13:21:16 +0000

    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@3175 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPluginBase/ComponentInformations.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Cryptool.PluginBase.Control;
+using System.Linq;
+
+namespace Cryptool.PluginBase
+{
+    /// <summary>
+    /// This class is used to maintain global informations about all loaded components.
+    /// It is necessary, so that components get to know each other if necessary.
+    /// </summary>
+    public static class ComponentInformations
+    {
+        /// <summary>
+        /// This dictionary maps types IControlCost, IControlEnryption, IP2PControl and IControlCubeAttack
+        /// to a list of plugin, which uses them in slave mode.
+        /// </summary>
+        public static readonly Dictionary<Type, List<Type>> PluginsWithSpecificController = new Dictionary<Type, List<Type>>();
+
+        /// <summary>
+        /// This dictionary maps editor extensions to the corresponding editor type.
+        /// </summary>
+        public static Dictionary<string, Type> EditorExtension;
+
+        public static void AddPlugin(Type pluginType)
+        {
+            try
+            {
+                foreach (PropertyInfo pInfo in pluginType.GetProperties())
+                {
+                    var propertyType = pInfo.PropertyType;
+                    PropertyInfoAttribute[] attributes = (PropertyInfoAttribute[])pInfo.GetCustomAttributes(typeof(PropertyInfoAttribute), false);
+                    if (attributes.Length == 1 && attributes[0].Direction == Direction.ControlSlave)
+                    {
+                        if (propertyType.IsInterface && propertyType.GetInterfaces().Contains(typeof(IControl)))
+                        {
+                            if (PluginsWithSpecificController.ContainsKey(propertyType))
+                            {
+                                PluginsWithSpecificController[propertyType].Add(pluginType);
+                            }
+                            else
+                            {
+                                PluginsWithSpecificController.Add(propertyType, new List<Type>() { pluginType });
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+    }
+}
