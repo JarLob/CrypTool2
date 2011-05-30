b3400a893fbcd8a9f0391bbf6007a68898ca16aa
wander <wander@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2011-05-30 16:23:06 +0000

    The StatusGenerator command line utility reads the CrypPlugins directory and summarizes the following information in an HTML document:  * plugin included in developer solution?  * plugin included in nightly build? (works only for core developers)
    More information gathered automatically are welcome. This tool shall replace the outdated Wiki page https://www.cryptool.org/trac/CrypTool2/wiki/ListOfPlugins
    
    
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@3424 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ StatusGenerator/StatusGenerator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StatusGenerator
+{
+    public class StatusGenerator
+    {
+        public const string CrypPlugins = "CrypPlugins";
+
+        private IDictionary<string, string> publicSolution;
+        private IDictionary<string, string> coreSolution;
+
+        private StreamWriter streamWriter;
+
+        public void Generate(string programRoot, string outputFile)
+        {
+            streamWriter = new StreamWriter(outputFile);
+            streamWriter.WriteLine("<html><body><table>");
+            streamWriter.WriteLine("<tr><th>Plugin</th><th>Developer Solution</th><th>Nightly Build</th></tr>");
+
+            string pluginPath = Path.Combine(programRoot, CrypPlugins);
+
+            publicSolution = ReadSolution("../../CrypTool 2.0.sln");
+            coreSolution = ReadSolution("../../CoreDeveloper/CrypTool 2.0.sln");
+
+            foreach (DirectoryInfo dir in new DirectoryInfo(pluginPath).GetDirectories())
+            {
+                if (dir.Name.StartsWith("."))
+                    continue;
+
+                ProcessDirectory(dir);
+            }
+
+            streamWriter.WriteLine("</table></body></html>");
+            streamWriter.Close();
+        }
+
+        private void ProcessDirectory(DirectoryInfo dir)
+        {
+            string dirShortName = dir.Name;
+
+            bool isInDeveloperSolution = publicSolution.ContainsKey(dirShortName);
+            bool isInNightlyBuild = coreSolution.ContainsKey(dirShortName);
+
+            streamWriter.Write(string.Format("<tr><td>{0}</td>", dirShortName));
+            streamWriter.Write(isInDeveloperSolution ? "<td>true</td>" : "<td><font color=\"red\">false</td>");
+            streamWriter.Write(isInNightlyBuild ? "<td>true</td>" : "<td><font color=\"red\">false</td>");
+            streamWriter.WriteLine("</tr>");
+        }
+
+        private Regex slnRegex = new Regex("Project\\(\"{([A-Z0-9-]+)}\"\\) = \"(\\S+)\", \"(\\S+)\", \"{(\\w+)-(\\w+)-(\\w+)-(\\w+)-(\\w+)}\"");
+
+        private IDictionary<string, string> ReadSolution(string slnPath)
+        {
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+
+            StreamReader streamReader = new StreamReader(slnPath);
+            while(!streamReader.EndOfStream)
+            {
+                string line = streamReader.ReadLine();
+                Match match = slnRegex.Match(line);
+                if (match.Success)
+                {
+                    string pluginName = match.Groups[2].Value;
+                    string projectPath = match.Groups[3].Value;
+
+                    while (projectPath.StartsWith("..\\"))
+                        projectPath = projectPath.Replace("..\\", "");
+
+                    if (projectPath.StartsWith(CrypPlugins))
+                        dict[pluginName] = projectPath;
+                }
+            }
+
+            return dict;
+        }
+    }
+}
