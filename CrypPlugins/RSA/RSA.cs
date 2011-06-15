24a0f7a90bfcc090712e5d35aa8e606f0f479eb6
kopal <kopal@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2011-06-15 08:56:29 +0000

    rsa: converted old documentation to new one
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@3474 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/RSA/RSA.cs
+++ CrypPlugins/RSA/RSA.cs
@@ -31,7 +31,7 @@ using RSA.Properties;
 namespace Cryptool.Plugins.RSA
 {
     [Author("Dennis Nolte, Raoul Falk, Sven Rech, Nils Kopal", null, "Uni Duisburg-Essen", "http://www.uni-due.de")]
-    [PluginInfo("RSA.Properties.Resources", false, "PluginCaption", "PluginTooltip", "PluginDescriptionURL", "RSA/iconrsa.png", "RSA/Images/encrypt.png", "RSA/Images/decrypt.png")]
+    [PluginInfo("RSA.Properties.Resources", false, "PluginCaption", "PluginTooltip", "RSA/DetailedDescription/doc.xml", "RSA/iconrsa.png", "RSA/Images/encrypt.png", "RSA/Images/decrypt.png")]
     [EncryptionType(EncryptionType.Asymmetric)]
      //<summary>
      //This plugin does a RSA encryption/decryption on a Message M / Ciphertext C
