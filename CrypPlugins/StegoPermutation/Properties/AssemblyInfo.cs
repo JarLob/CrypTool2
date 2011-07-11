21263b6a63e64ddcde218a942474f6750a94c5fe
wander <wander@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2011-07-11 18:50:50 +0000

    renamed assembly to match project name
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@3588 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/StegoPermutation/Properties/AssemblyInfo.cs
+++ CrypPlugins/StegoPermutation/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StegoOrder")]
+[assembly: AssemblyTitle("StegoPermutation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StegoOrder")]
+[assembly: AssemblyProduct("StegoPermutation")]
 [assembly: AssemblyCopyright("Copyright © 2011 CrypTool 2 Team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
