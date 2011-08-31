fc6cef093ec704a92948652e7a19340625ac16c3
kopal <kopal@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2011-08-31 16:58:08 +0000

    created first version of an online help editing tool
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@3746 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypDocumentationEditor/CrypDocumentationEditor/Documentation.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Windows.Documents;
+using System.Windows;
+
+namespace CrypDocumentationEditor
+{
+    public class Documentation
+    {
+        private XmlDocument xml = null;
+        public const string DEFAULT_LANGUAGE = "en";
+
+        public Documentation()
+        {
+            try
+            {
+                Language = DEFAULT_LANGUAGE;
+                xml = new XmlDocument();
+
+                XmlDeclaration declaration = (XmlDeclaration)xml.CreateNode(XmlNodeType.XmlDeclaration, "xml", null);
+                declaration.Encoding = "utf-8";
+                XmlNode documentationNode = xml.CreateNode(XmlNodeType.Element, "documentation", null);
+                XmlNode introductionNode = xml.CreateNode(XmlNodeType.Element, "introduction", null);
+                XmlNode usageNode = xml.CreateNode(XmlNodeType.Element, "usage", null);
+                XmlNode presentationNode = xml.CreateNode(XmlNodeType.Element, "presentation", null);
+                XmlNode languageNode = xml.CreateNode(XmlNodeType.Element, "language", null);
+
+                XmlAttribute languageNodeAttribute = xml.CreateAttribute("culture");
+                languageNodeAttribute.Value = Language;                
+
+                XmlAttribute introductionlangAttribute = xml.CreateAttribute("lang");
+                introductionlangAttribute.Value = Language;
+                XmlAttribute usagelangAttribute = xml.CreateAttribute("lang");
+                usagelangAttribute.Value = Language;
+                XmlAttribute presentationlangAttribute = xml.CreateAttribute("lang");
+                presentationlangAttribute.Value = Language;
+
+                xml.AppendChild(declaration);
+                xml.AppendChild(documentationNode);
+                documentationNode.AppendChild(languageNode);
+                languageNode.Attributes.Append(languageNodeAttribute);
+                documentationNode.AppendChild(introductionNode);
+                introductionNode.Attributes.Append(introductionlangAttribute);
+                documentationNode.AppendChild(usageNode);
+                usageNode.Attributes.Append(usagelangAttribute);
+                documentationNode.AppendChild(presentationNode);
+                presentationNode.Attributes.Append(presentationlangAttribute);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public string Language
+        {
+            get;
+            set;
+        }
+
+        public void Load(string filename)
+        {            
+            xml.Load(filename);                    
+        }
+
+        public void Save(string filename)
+        {
+            xml.Save(filename);
+        }
+
+        public FlowDocument Introduction
+        {
+            get
+            {
+                XmlNode introduction = xml.SelectSingleNode("/documentation/introduction[@lang='"+ Language +"']");
+                string text = introduction.InnerXml.Trim();
+                FlowDocument document = new FlowDocument();
+                Paragraph para = new Paragraph();
+                para.Inlines.Add(new Run(text));
+                document.Blocks.Add(para);
+                return document;
+            }
+            set
+            {
+                XmlNode introduction = xml.SelectSingleNode("/documentation/introduction[@lang='" + Language + "']");
+                StringBuilder builder = new StringBuilder();
+                foreach (Paragraph p in value.Blocks)
+                {
+                    foreach (Run r in p.Inlines)
+                    {
+                        builder.Append(r.Text + Environment.NewLine);
+                    }
+                }
+                introduction.InnerXml = builder.ToString();
+            }
+        }
+
+        public FlowDocument Usage
+        {
+            get
+            {
+                XmlNode usage = xml.SelectSingleNode("/documentation/usage[@lang='" + Language + "']");
+                string text = usage.InnerXml.Trim();
+                FlowDocument document = new FlowDocument();
+                Paragraph para = new Paragraph();
+                para.Inlines.Add(new Run(text));
+                document.Blocks.Add(para);
+                return document;
+            }
+            set
+            {
+                XmlNode usage = xml.SelectSingleNode("/documentation/usage[@lang='" + Language + "']");
+                StringBuilder builder = new StringBuilder();
+                foreach (Paragraph p in value.Blocks)
+                {
+                    foreach (Run r in p.Inlines)
+                    {
+                        builder.Append(r.Text + Environment.NewLine);
+                    }
+                }
+                usage.InnerXml = builder.ToString();
+            }
+        }
+
+        public FlowDocument Presentation
+        {
+            get
+            {
+                XmlNode presentation = xml.SelectSingleNode("/documentation/presentation[@lang='" + Language + "']");
+                string text = presentation.InnerXml.Trim();
+                FlowDocument document = new FlowDocument();
+                Paragraph para = new Paragraph();
+                para.Inlines.Add(new Run(text));
+                document.Blocks.Add(para);
+                return document;
+            }
+            set
+            {
+                XmlNode presentation = xml.SelectSingleNode("/documentation/presentation[@lang='" + Language + "']");
+                StringBuilder builder = new StringBuilder();
+                foreach (Paragraph p in value.Blocks)
+                {
+                    foreach (Run r in p.Inlines)
+                    {
+                        builder.Append(r.Text + Environment.NewLine);
+                    }
+                }
+                presentation.InnerXml = builder.ToString();
+            }
+        }
+
+        public string[] GetLanguages()
+        {
+            XmlNodeList nodes = xml.SelectNodes("/documentation/language");
+            if (nodes.Count == 0)
+            {
+                return null;
+            }
+            string[] languages = new string[nodes.Count];
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                languages[i] = nodes[i].Attributes["culture"].Value;
+            }
+            return languages;
+        }
+
+        public void AddLanguage(string language)
+        {
+            XmlNode languageNode = xml.CreateNode(XmlNodeType.Element, "language", null);
+            XmlAttribute languageNodeAttribute = xml.CreateAttribute("culture");
+            languageNodeAttribute.Value = language;
+            languageNode.Attributes.Append(languageNodeAttribute);
+
+            XmlNode documentationNode = xml.SelectSingleNode("/documentation");
+
+            XmlNode introductionNode = xml.CreateNode(XmlNodeType.Element, "introduction", null);
+            XmlNode usageNode = xml.CreateNode(XmlNodeType.Element, "usage", null);
+            XmlNode presentationNode = xml.CreateNode(XmlNodeType.Element, "presentation", null);                        
+
+            XmlAttribute introductionlangAttribute = xml.CreateAttribute("lang");
+            introductionlangAttribute.Value = language;
+            XmlAttribute usagelangAttribute = xml.CreateAttribute("lang");
+            usagelangAttribute.Value = language;
+            XmlAttribute presentationlangAttribute = xml.CreateAttribute("lang");
+            presentationlangAttribute.Value = language;
+
+            documentationNode.AppendChild(languageNode);            
+            languageNode.Attributes.Append(languageNodeAttribute);
+            documentationNode.AppendChild(introductionNode);
+            introductionNode.Attributes.Append(introductionlangAttribute);
+            documentationNode.AppendChild(usageNode);
+            usageNode.Attributes.Append(usagelangAttribute);
+            documentationNode.AppendChild(presentationNode);
+            presentationNode.Attributes.Append(presentationlangAttribute);            
+        }
+    }
+
+
+
+}
---
+++ CrypDocumentationEditor/CrypDocumentationEditor/MainWindow.xaml.cs
@@ -0,0 +1,438 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.VisualBasic;
+
+namespace CrypDocumentationEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool hasChanges = false;
+        private Documentation docu;
+        private string[] languages;
+
+        public bool HasChanges { 
+            get{
+                return hasChanges;
+            }
+            set
+            {
+                if (value)
+                {
+                    this.Title = "CrypDocumentationEditor*";
+                }
+                else
+                {
+                    this.Title = "CrypDocumentationEditor";
+                }
+                hasChanges = value;
+            }
+        }
+
+        public MainWindow()
+        {            
+            InitializeComponent();            
+            NewButton_Click(null, null);            
+        }
+
+        private void GenerateLanguageSelector()
+        {
+            languages = docu.GetLanguages();            
+            this.DocuLanguage.ItemsSource = languages;
+            this.DocuLanguage.SelectedIndex = 0;
+        }
+
+        private void BoldButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Introduction.IsFocused)
+            {
+                Introduction.Selection.Start.InsertTextInRun("<b>");
+                Introduction.Selection.End.InsertTextInRun("</b>");
+                
+            }
+            if (Usage.IsFocused)
+            {
+                Usage.Selection.Start.InsertTextInRun("<b>");
+                Usage.Selection.End.InsertTextInRun("</b>");
+            }
+            if (Presentation.IsFocused)
+            {
+                Presentation.Selection.Start.InsertTextInRun("<b>");
+                Presentation.Selection.End.InsertTextInRun("</b>");
+            }
+        }
+
+        private void ItalicButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Introduction.IsFocused)
+            {
+                Introduction.Selection.Start.InsertTextInRun("<i>");
+                Introduction.Selection.End.InsertTextInRun("</i>");
+
+            }
+            if (Usage.IsFocused)
+            {
+                Usage.Selection.Start.InsertTextInRun("<i>");
+                Usage.Selection.End.InsertTextInRun("</i>");
+            }
+            if (Presentation.IsFocused)
+            {
+                Presentation.Selection.Start.InsertTextInRun("<i>");
+                Presentation.Selection.End.InsertTextInRun("</i>");
+            }
+        }
+
+        private void UnderlineButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Introduction.IsFocused)
+            {
+                Introduction.Selection.Start.InsertTextInRun("<u>");
+                Introduction.Selection.End.InsertTextInRun("</u>");
+
+            }
+            if (Usage.IsFocused)
+            {
+                Usage.Selection.Start.InsertTextInRun("<u>");
+                Usage.Selection.End.InsertTextInRun("</u>");
+            }
+            if (Presentation.IsFocused)
+            {
+                Presentation.Selection.Start.InsertTextInRun("<u>");
+                Presentation.Selection.End.InsertTextInRun("</u>");
+            }
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            double height = (e.NewSize.Height - 190) / 3;
+            Introduction.Height = height;
+            Usage.Height = height;
+            Presentation.Height = height;
+        }
+
+        private void NewButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (HasChanges)
+            {
+                string messageBoxText = "You have unsaved changes. Do you want to save changes? Click Yes to save and create a new documentation, No to create a new documentation without saving, or Cancel to not create a new documentation.";
+                string caption = "CrypDocumentationEditor";
+                MessageBoxResult result = MessageBox.Show(messageBoxText, caption, MessageBoxButton.YesNoCancel, MessageBoxImage.Warning);
+
+                switch (result)
+                {
+                    case MessageBoxResult.Yes:
+                        Microsoft.Win32.SaveFileDialog dlg2 = new Microsoft.Win32.SaveFileDialog();
+                        dlg2.FileName = "Documentation";
+                        dlg2.DefaultExt = ".xml";
+                        dlg2.Filter = "XML docu files (.xml)|*.xml";
+
+                        Nullable<bool> result2 = dlg2.ShowDialog();
+
+                        if (result2 == true)
+                        {
+                            string filename = dlg2.FileName;
+                            docu.Introduction = Introduction.Document;
+                            docu.Usage = Usage.Document;
+                            docu.Presentation = Presentation.Document;
+                            docu.Save(filename);
+                            HasChanges = false;
+                        }
+                        else
+                        {
+                            return;
+                        }
+                        break;
+                    case MessageBoxResult.No:
+                        // User pressed No button
+                        // ...
+                        break;
+                    case MessageBoxResult.Cancel:
+                        return;
+                }                                
+            }
+            
+            docu = new Documentation();            
+
+            FlowDocument document = new FlowDocument();
+            Paragraph para = new Paragraph();
+            para.Inlines.Add(new Run(""));
+            document.Blocks.Add(para);
+            Introduction.Document = document;
+
+            document = new FlowDocument();
+            para = new Paragraph();
+            para.Inlines.Add(new Run(""));
+            document.Blocks.Add(para);
+            Usage.Document = document;
+
+            document = new FlowDocument();
+            para = new Paragraph();
+            para.Inlines.Add(new Run(""));
+            document.Blocks.Add(para);
+            Presentation.Document = document;
+
+            GenerateLanguageSelector();
+
+            HasChanges = false;
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (HasChanges)
+            {
+                string messageBoxText = "You have unsaved changes. Do you want to save changes? Click Yes to save and open a documentation, No to open a documentation without saving, or Cancel to not open a documentation.";
+                string caption = "CrypDocumentationEditor";
+                MessageBoxResult result = MessageBox.Show(messageBoxText, caption, MessageBoxButton.YesNoCancel, MessageBoxImage.Warning);
+
+                switch (result)
+                {
+                    case MessageBoxResult.Yes:
+                        Microsoft.Win32.SaveFileDialog dlg2 = new Microsoft.Win32.SaveFileDialog();
+                        dlg2.FileName = "Documentation";
+                        dlg2.DefaultExt = ".xml";
+                        dlg2.Filter = "XML docu files (.xml)|*.xml";
+
+                        Nullable<bool> result2 = dlg2.ShowDialog();
+
+                        if (result2 == true)
+                        {
+                            string filename = dlg2.FileName;
+                            docu.Introduction = Introduction.Document;
+                            docu.Usage = Usage.Document;
+                            docu.Presentation = Presentation.Document;
+                            docu.Save(filename);
+                            HasChanges = false;
+                        }
+                        else
+                        {
+                            return;
+                        }
+                        break;
+                    case MessageBoxResult.No:
+                        // User pressed No button
+                        // ...
+                        break;
+                    case MessageBoxResult.Cancel:
+                        return;
+                }
+            }
+
+            Microsoft.Win32.OpenFileDialog dlg3 = new Microsoft.Win32.OpenFileDialog();
+            dlg3.FileName = "Documentation"; 
+            dlg3.DefaultExt = ".xml";
+            dlg3.Filter = "XML docu files (.xml)|*.xml";
+
+            Nullable<bool> result3 = dlg3.ShowDialog();
+
+            if (result3 == true)
+            {                                
+                string filename = dlg3.FileName;
+                docu = new Documentation();                                       
+                docu.Load(filename);
+
+                Introduction.Document = docu.Introduction;
+                Usage.Document = docu.Usage;
+                Presentation.Document = docu.Presentation;
+                
+            }
+
+            GenerateLanguageSelector();
+
+            HasChanges = false;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {         
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "Documentation";
+            dlg.DefaultExt = ".xml";
+            dlg.Filter = "XML docu files (.xml)|*.xml";
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {               
+                string filename = dlg.FileName;
+                docu.Introduction = Introduction.Document;
+                docu.Usage = Usage.Document;
+                docu.Presentation = Presentation.Document;
+                docu.Save(filename);
+                HasChanges = false;
+            }
+        }
+
+        private void Introduction_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            HasChanges = true;
+        }
+
+        private void Usage_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            HasChanges = true;
+        }
+
+        private void Presentation_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            HasChanges = true;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (HasChanges)
+            {
+                string messageBoxText = "You have unsaved changes. Do you want to save changes? Click Yes to save and close, No to close without saving, or Cancel to not close.";
+                string caption = "CrypDocumentationEditor";
+                MessageBoxResult result = MessageBox.Show(messageBoxText, caption, MessageBoxButton.YesNoCancel,  MessageBoxImage.Warning);
+
+                switch (result)
+                {
+                    case MessageBoxResult.Yes:
+                        Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+                        dlg.FileName = "Documentation";
+                        dlg.DefaultExt = ".xml";
+                        dlg.Filter = "XML docu files (.xml)|*.xml";
+
+                        Nullable<bool> result2 = dlg.ShowDialog();
+
+                        if (result2 == true)
+                        {
+                            string filename = dlg.FileName;
+                            docu.Introduction = Introduction.Document;
+                            docu.Usage = Usage.Document;
+                            docu.Presentation = Presentation.Document;
+                            docu.Save(filename);
+                            HasChanges = false;
+                        }
+                        else
+                        {
+                            return;
+                        }
+                        break;
+                    case MessageBoxResult.No:
+                        // User pressed No button
+                        // ...
+                        break;
+                    case MessageBoxResult.Cancel:
+                        e.Cancel = true;
+                        break;
+                }
+            }
+        }
+
+        private void AddLanguage_Click(object sender, RoutedEventArgs e)
+        {
+            string value = Interaction.InputBox("Please enter new language", "Enter new language", "", 100, 100);
+            if (value == "" || languages.Contains(value))
+            {
+                return;
+            }
+            this.docu.AddLanguage(value);
+            string[] newlanguages = new string[languages.Length + 1];
+            Array.Copy(languages, newlanguages, languages.Length);
+            newlanguages[newlanguages.Length - 1] = value;
+            languages = newlanguages;
+            this.DocuLanguage.ItemsSource = languages;
+            this.HasChanges = true;
+        }
+
+        private void AddPictureButton_Click(object sender, RoutedEventArgs e)
+        {
+            string uri = Interaction.InputBox("Please enter the uri to the picture", "Enter picture uri", "", 100, 100);
+            if (uri == "")
+            {
+                return;
+            }
+
+            if (Introduction.IsFocused)
+            {
+                Introduction.Selection.Start.InsertTextInRun("<img src=\"" + uri + "\"/>");
+            }
+            if (Usage.IsFocused)
+            {
+                Usage.Selection.Start.InsertTextInRun("<img src=\"" + uri + "\"/>");
+            }
+            if (Presentation.IsFocused)
+            {
+                Presentation.Selection.Start.InsertTextInRun("<img src=\"" + uri + "\"/>");
+            }
+        }
+
+        private void AddSectionButton_Click(object sender, RoutedEventArgs e)
+        {
+            string sectionname = Interaction.InputBox("Please enter the new section name", "Enter section name", "", 100, 100);
+            if (sectionname == "")
+            {
+                return;
+            }
+
+            if (Introduction.IsFocused)
+            {
+                Introduction.Selection.Start.InsertTextInRun("<section headline=\"" + sectionname + "\"/>");
+            }
+            if (Usage.IsFocused)
+            {
+                Usage.Selection.Start.InsertTextInRun("<section headline=\"" + sectionname + "\"/>");
+            }
+            if (Presentation.IsFocused)
+            {
+                Presentation.Selection.Start.InsertTextInRun("<section headline=\"" + sectionname + "\"/>");
+            }
+        }
+
+        private void DocuLanguage_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (DocuLanguage.SelectedValue == null)
+            {
+                return;
+            }
+            //Save old has changes state
+            bool hasChanges = HasChanges;
+
+            //save old language entries to xml
+            docu.Introduction = Introduction.Document;
+            docu.Usage = Usage.Document;
+            docu.Presentation = Presentation.Document;
+            
+            //change to new selected language
+            docu.Language = (string)DocuLanguage.SelectedValue;       
+     
+            //put new selected language entries from xml to view
+            Introduction.Document = docu.Introduction;
+            Usage.Document = docu.Usage;
+            Presentation.Document = docu.Presentation;
+
+            //restore old HasChanges state
+            HasChanges = hasChanges;
+        }
+
+        private void NewlineButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Introduction.IsFocused)
+            {
+                Introduction.Selection.Start.InsertTextInRun("<newline />");
+            }
+            if (Usage.IsFocused)
+            {
+                Usage.Selection.Start.InsertTextInRun("<newline />");
+            }
+            if (Presentation.IsFocused)
+            {
+                Presentation.Selection.Start.InsertTextInRun("<newline />");
+            }
+        }
+    }
+}
