b3895a1536ad16ce49e2d0498ee213b9e4426fba
wander <wander@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2011-11-10 14:35:54 +0000

    marked startable flag as obsolete, will be removed tomorrow or so
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@4124 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPluginBase/Attributes/PluginInfoAttribute.cs
+++ CrypPluginBase/Attributes/PluginInfoAttribute.cs
@@ -1,5 +1,5 @@
 ﻿/*
-   Copyright 2008 Martin Saternus, University of Duisburg-Essen
+   Copyright 2008-2011 CrypTool 2 Team <ct2contact@cryptool.org>
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -26,13 +26,13 @@ namespace Cryptool.PluginBase
         private readonly string caption;
         public string Caption
         {
-          get 
-          {
-            if (MultiLanguage && caption != null)
-              return PluginType.GetPluginStringResource(caption);
-            else
-              return caption; 
-          }
+            get
+            {
+                if (MultiLanguage && caption != null)
+                    return PluginType.GetPluginStringResource(caption);
+                else
+                    return caption;
+            }
         }
 
         public string EnglishCaption
@@ -51,13 +51,13 @@ namespace Cryptool.PluginBase
         public readonly string toolTip;
         public string ToolTip
         {
-          get
-          {
-            if (MultiLanguage && toolTip != null)
-              return PluginType.GetPluginStringResource(toolTip);
-            else
-              return toolTip;
-          }
+            get
+            {
+                if (MultiLanguage && toolTip != null)
+                    return PluginType.GetPluginStringResource(toolTip);
+                else
+                    return toolTip;
+            }
         }
 
         public readonly string descriptionUrl;
@@ -81,59 +81,64 @@ namespace Cryptool.PluginBase
 
         # region translation helpers
         public readonly string ResourceFile;
-        private Type pluginType;
 
         /// <summary>
         /// Gets or sets the type of the plugin. This value is set by extension method if ResourceFile exists. 
         /// It is used to access the plugins resources to translate the text elements.
         /// </summary>
         /// <value>The type of the plugin.</value>
-        public Type PluginType
-        {
-          get { return pluginType; }
-          set { pluginType = value; }
-        }
+        public Type PluginType { get; set; }
 
         private bool MultiLanguage
         {
-          get { return ResourceFile != null && PluginType != null; }
+            get { return ResourceFile != null && PluginType != null; }
         }
         # endregion translation helpers
 
         /// <summary>
         /// Initializes a new instance of the <see cref="PluginInfoAttribute"/> class.
         /// </summary>
-        /// <param name="startable">if set to <c>true</c> [startable].</param>
         /// <param name="caption">General name.</param>
         /// <param name="toolTip">The tool tip.</param>
         /// <param name="descriptionUrl">If FlowDocument for description is provided place the URI here. Set XAML file to "Resource" not "Embedded Resource".</param>
         /// <param name="icons">The icons. Set icon files to "Resource" not "Embedded Resource".</param>
-        public PluginInfoAttribute(bool startable, string caption, string toolTip, string descriptionUrl, params string[] icons)
-          {
-            this.Startable = startable;
+        public PluginInfoAttribute(string caption, string toolTip, string descriptionUrl, params string[] icons)
+        {
             this.caption = caption;
             this.toolTip = toolTip;
             this.descriptionUrl = descriptionUrl;
             this.Icons = icons;
         }
 
+        [Obsolete("startable flag will be removed soon")]
+        public PluginInfoAttribute(bool startable, string caption, string toolTip, string descriptionUrl, params string[] icons)
+            : this(caption, toolTip, descriptionUrl, icons)
+        {
+            this.Startable = startable;
+        }
+
         /// <summary>
         /// Temp. two constructors while not all plugins are switched to new multi language mode.
         /// </summary>
         /// <param name="resourceFile">The resource file.</param>
-        /// <param name="startable">if set to <c>true</c> [startable].</param>
         /// <param name="caption">The caption.</param>
         /// <param name="toolTip">The tool tip.</param>
         /// <param name="descriptionUrl">The description URL.</param>
         /// <param name="icons">The icons.</param>
-        public PluginInfoAttribute(string resourceFile, bool startable, string caption, string toolTip, string descriptionUrl, params string[] icons)
+        public PluginInfoAttribute(string resourceFile, string caption, string toolTip, string descriptionUrl, params string[] icons)
         {
-          this.ResourceFile = resourceFile;
-          this.Startable = startable;
-          this.caption = caption;
-          this.toolTip = toolTip;
-          this.descriptionUrl = descriptionUrl;
-          this.Icons = icons;
+            this.ResourceFile = resourceFile;
+            this.caption = caption;
+            this.toolTip = toolTip;
+            this.descriptionUrl = descriptionUrl;
+            this.Icons = icons;
+        }
+
+        [Obsolete("startable flag will be removed soon")]
+        public PluginInfoAttribute(string resourceFile, bool startable, string caption, string toolTip, string descriptionUrl, params string[] icons)
+            : this(resourceFile, caption, toolTip, descriptionUrl, icons)
+        {
+            this.Startable = startable;
         }
     }
 }
