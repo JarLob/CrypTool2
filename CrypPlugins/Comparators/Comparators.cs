04fda97c412159ee21585af49c0409694ef37816
kopal <kopal@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2012-02-21 23:21:33 +0000

    converted xaml description to new format
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@4527 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/Comparators/Comparators.cs
+++ CrypPlugins/Comparators/Comparators.cs
@@ -28,7 +28,7 @@ using System.Numerics;
 namespace Cryptool.Plugins.Comparators
 {
     [Author("Raoul Falk", "falk@cryptool.org", "Uni Duisburg-Essen", "http://www.uni-due.de")]
-    [PluginInfo("Comparators.Properties.Resources", "PluginCaption", "PluginTooltip", "PluginDescriptionURL", "Comparators/icons/icon_is.png", "Comparators/icons/icon_isnot.png", "Comparators/icons/icon_smaller.png", "Comparators/icons/icon_bigger.png", "Comparators/icons/icon_smallerIs.png", "Comparators/icons/icon_biggerIs.png")]
+    [PluginInfo("Comparators.Properties.Resources", "PluginCaption", "PluginTooltip", "Comparators/DetailedDescription/doc.xml", "Comparators/icons/icon_is.png", "Comparators/icons/icon_isnot.png", "Comparators/icons/icon_smaller.png", "Comparators/icons/icon_bigger.png", "Comparators/icons/icon_smallerIs.png", "Comparators/icons/icon_biggerIs.png")]
     [ComponentCategory(ComponentCategory.ToolsMisc)]
     class Comparators : ICrypComponent
     {
