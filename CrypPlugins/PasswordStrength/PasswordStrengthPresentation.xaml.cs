52e49865d6e4271a83d01de66f265bf56c4edd46
kopal <kopal@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2012-05-30 11:32:11 +0000

    - created new password strength plugin based on Mozilla test - added a template for password strength plugin
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@4835 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/PasswordStrength/PasswordStrengthPresentation.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace Cryptool.Plugins.Tools
+{
+    /// <summary>
+    /// Interaction logic for PasswordStrengthPresentation.xaml
+    /// </summary>
+    [PluginBase.Attributes.Localization("Cryptool.Plugins.Tools.Properties.Resources")]
+    public partial class PasswordStrengthPresentation : UserControl
+    {
+        public PasswordStrengthPresentation()
+        {
+            InitializeComponent();
+        }
+    }
+}
