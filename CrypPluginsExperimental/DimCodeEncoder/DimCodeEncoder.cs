683a9ddc0332b5f913d671b034f35a734c4fd77e
konze <konze@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2012-10-17 15:40:13 +0000

    add encoder framework for dimension codes to project
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@5018 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPluginsExperimental/DimCodeEncoder/DimCodeEncoder.cs
@@ -0,0 +1,160 @@
+﻿/*
+   Copyright 2011 CrypTool 2 Team <ct2contact@cryptool.org>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Controls;
+using Cryptool.PluginBase;
+using Cryptool.PluginBase.IO;
+using Cryptool.PluginBase.Miscellaneous;
+using DimCodeEncoder.DimCodes;
+
+namespace Cryptool.Plugins.DimCodeEncoder
+{
+    [Author("Anonymous", "coredevs@cryptool.org", "CrypTool 2 Team", "http://cryptool2.vs.uni-due.de")]
+    [PluginInfo("DimCodeEncoder", "Subtract one number from another", "DimCodeEncoder/userdoc.xml", new[] { "CrypWin/images/default.png" })]
+    [ComponentCategory(ComponentCategory.ToolsMisc)]
+    public class DimCodeEncoder : ICrypComponent
+    {
+        #region Private Variables
+
+        private Dictionary<DimCodeEncoderSettings.DimCodeType, IDimCode> codeTypeHandler = new Dictionary<DimCodeEncoderSettings.DimCodeType, IDimCode>();
+        private readonly DimCodeEncoderSettings settings = new DimCodeEncoderSettings();
+        
+        #endregion
+
+        public DimCodeEncoder()
+        {
+            //codeTypeHandler.Add(DimCodeEncoderSettings.DimCodeType.QRCode, new);
+        }
+
+        #region Data Properties
+
+        [PropertyInfo(Direction.InputData, "IncommingData", "IncommingDataTooltip")]
+        public ICryptoolStream IncommingData
+        {
+            get;
+            private set;
+        }
+
+
+        [PropertyInfo(Direction.OutputData, "PictureBytesOutput", "PictureBytesOutputTooltip")]
+        public byte[] PictureBytes
+        {
+            get;
+            private set;
+        }
+
+        #endregion
+
+        #region IPlugin Members
+
+        /// <summary>
+        /// Provide plugin-related parameters (per instance) or return null.
+        /// </summary>
+        public ISettings Settings
+        {
+            get { return settings; }
+        }
+
+        /// <summary>
+        /// Provide custom presentation to visualize the execution or return null.
+        /// </summary>
+        public UserControl Presentation
+        {
+            get { return null; }
+        }
+
+        /// <summary>
+        /// Called once when workflow execution starts.
+        /// </summary>
+        public void PreExecution()
+        {
+        }
+
+        /// <summary>
+        /// Called every time this plugin is run in the workflow execution.
+        /// </summary>
+        public void Execute()
+        {
+            // HOWTO: Use this to show the progress of a plugin algorithm execution in the editor.
+            ProgressChanged(0, 1);
+
+         
+            // HOWTO: Make sure the progress bar is at maximum when your Execute() finished successfully.
+            ProgressChanged(1, 1);
+        }
+
+        /// <summary>
+        /// Called once after workflow execution has stopped.
+        /// </summary>
+        public void PostExecution()
+        {
+        }
+
+        /// <summary>
+        /// Triggered time when user clicks stop button.
+        /// Shall abort long-running execution.
+        /// </summary>
+        public void Stop()
+        {
+        }
+
+        /// <summary>
+        /// Called once when plugin is loaded into editor workspace.
+        /// </summary>
+        public void Initialize()
+        {
+        }
+
+        /// <summary>
+        /// Called once when plugin is removed from editor workspace.
+        /// </summary>
+        public void Dispose()
+        {
+        }
+
+        #endregion
+
+        #region Event Handling
+
+        public event StatusChangedEventHandler OnPluginStatusChanged;
+
+        public event GuiLogNotificationEventHandler OnGuiLogNotificationOccured;
+
+        public event PluginProgressChangedEventHandler OnPluginProgressChanged;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void GuiLogMessage(string message, NotificationLevel logLevel)
+        {
+            EventsHelper.GuiLogMessage(OnGuiLogNotificationOccured, this, new GuiLogEventArgs(message, this, logLevel));
+        }
+
+        private void OnPropertyChanged(string name)
+        {
+            EventsHelper.PropertyChanged(PropertyChanged, this, new PropertyChangedEventArgs(name));
+        }
+
+        private void ProgressChanged(double value, double max)
+        {
+            EventsHelper.ProgressChanged(OnPluginProgressChanged, this, new PluginProgressEventArgs(value, max));
+        }
+
+        #endregion
+    }
+}
---
+++ CrypPluginsExperimental/DimCodeEncoder/DimCodeEncoderSettings.cs
@@ -0,0 +1,135 @@
+﻿/*
+   Copyright 2011 CrypTool 2 Team <ct2contact@cryptool.org>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using Cryptool.PluginBase;
+using Cryptool.PluginBase.Miscellaneous;
+
+namespace Cryptool.Plugins.DimCodeEncoder
+{
+    // HOWTO: rename class (click name, press F2)
+    public class DimCodeEncoderSettings : ISettings
+    {
+        #region Variables
+
+        private List<string> inputList = new List<string>();  
+        private Dictionary<DimCodeType, List<string>> inputVisibility = new Dictionary<DimCodeType,List<string>>();
+          
+        #region input Variables
+
+        private int integerInput;
+        private DimCodeType encodingType = DimCodeType.QRCode;
+
+        #endregion
+
+        public enum DimCodeType { QRCode, EAN8 };
+        
+        #endregion
+
+        public DimCodeEncoderSettings()
+        {
+            //init for each enum inputVisibilityLists 
+            foreach (DimCodeType name in Enum.GetValues(typeof(DimCodeType)))
+            {
+                inputVisibility[name] = new List<string>();
+            }
+
+            //add all inputs
+            inputList.Add("IntegerInput");
+            //add input for each codetype if it should be visible
+            inputVisibility[DimCodeType.QRCode].Add("IntegerInput");
+
+           
+        }
+
+
+
+        #region TaskPane Settings
+
+
+        // To add more codetypes you will have to add your codeType to the stringArray below and to the DimCodeType enum.
+        // if you want to have unique settings for your code type f.e. YourSettings:
+        // 1. add in constructor to inputlist : inputList.Add("YourSettings");
+        // 2. add your input to the inputVisibilitylist of your code type: inputVisibility[DimCodeType.QRCode].Add("YourSettings");
+        [TaskPane("EncodingCaption", "EncodingTooltip", null, 1, true, ControlType.ComboBox, new string[] { "QRCode", "EAN8" })]
+        public DimCodeType EncodingType
+        {
+            get { return this.encodingType; }
+            set
+            {
+                if (value != this.encodingType)
+                {
+                    this.encodingType = value;
+                    UpdateTaskPaneVisibility();
+                    OnPropertyChanged("EncodingType");
+
+                    
+                  //  UpdatePresentationTab(value);
+                }
+            }
+        }
+
+       
+        [TaskPane("IntegerInputCaption", "IntegerInputTooltip", "CodeTypeSettings", 1, false, ControlType.NumericUpDown, ValidationType.RangeInteger, 1, 65535)]
+        public int IntegerInput
+        {
+            get { return this.integerInput; }
+            set
+            {
+                if (value != this.integerInput)
+                {
+                    this.integerInput = value;
+                    OnPropertyChanged("IntegerInput");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Visualisation updates
+
+        internal void UpdateTaskPaneVisibility()
+        {
+            if (TaskPaneAttributeChanged == null)
+                return;
+
+            foreach (var tpac in inputList.Select(input => new TaskPaneAttribteContainer(input, (inputVisibility[EncodingType].Contains(input)) ? Visibility.Visible : Visibility.Collapsed)))
+            {
+                TaskPaneAttributeChanged(this, new TaskPaneAttributeChangedEventArgs(tpac));
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        public event TaskPaneAttributeChangedHandler TaskPaneAttributeChanged;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            EventsHelper.PropertyChanged(PropertyChanged, this, propertyName);
+        }
+
+        #endregion
+    }
+}
