291d5b5b969c398856438bb35ad0789c1a3db36a
kopal <kopal@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2012-12-17 20:24:30 +0000

    * removed unit test vectors of scytale since they are obtained from CT1 and the scytale of CT1 and CT2.0 are incompatible
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@5143 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- UnitTests/ScytaleTest.cs
+++ UnitTests/ScytaleTest.cs
@@ -40,12 +40,13 @@ namespace Tests.TemplateAndPluginTests
         // Source of the test vectors: http://en.wikipedia.org/wiki/Scytale, manually created test vector
         //
         TestVector[] testvectors = new TestVector[] {
-            new TestVector () { n=0, action=0, size=4, input="HELPMEIAMUNDERATTACK", output="HENTEIDTLAEAPMRCMUAK" },
+            /*new TestVector () { n=0, action=0, size=4, input="HELPMEIAMUNDERATTACK", output="HENTEIDTLAEAPMRCMUAK" },
             new TestVector () { n=1, action=1, size=4, input="HENTEIDTLAEAPMRCMUAK", output="HELPMEIAMUNDERATTACK" },
             new TestVector () { n=2, action=0, size=4, input="Help me I am under attack", output="H nteIdal ecpark m _m a_eut_" },
             new TestVector () { n=3, action=1, size=4, input="H nteIdal ecpark m _m a_eut_", output="Help me I am under attack" },
             new TestVector () { n=4, action=0, size=7, input="The quick brown fox jumps over the lazy dog.", output="Tcnuelghk mra.e fp z_ bosty_qrx h _uo oed_iwjv o_" },
-            new TestVector () { n=5, action=1, size=7, input="Tcnuelghk mra.e fp z_ bosty_qrx h _uo oed_iwjv o_", output="The quick brown fox jumps over the lazy dog." },
+            new TestVector () { n=5, action=1, size=7, input="Tcnuelghk mra.e fp z_ bosty_qrx h _uo oed_iwjv o_", output="The quick brown fox jumps over the lazy dog." },*/
+            //Scytale in CT1 and CT2.0 are incompatible, so such an unit test does not make any sense...
         };
 
     }
