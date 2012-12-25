b903a04d8551f489ccaf2348586653013ada55ba
matkovic <matkovic@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2012-12-25 23:33:21 +0000

    some changes
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@5159 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/CrypTutorials/CrypTutorialsPresentation.xaml.cs
+++ CrypPlugins/CrypTutorials/CrypTutorialsPresentation.xaml.cs
@@ -5,9 +5,11 @@ using System;
 using System.Windows.Data;
 using System.Windows.Controls;
 using System.ComponentModel;
+using System.Globalization;
 
 namespace Cryptool.CrypTutorials
 {
+
 
     public class RandomMaxConverter : IValueConverter
     {
@@ -105,4 +107,17 @@ namespace Cryptool.CrypTutorials
             }
         }
     }
+
+    public class IsNullConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value == null);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException("IsNullConverter can only be used OneWay.");
+        }
+    } 
 }
