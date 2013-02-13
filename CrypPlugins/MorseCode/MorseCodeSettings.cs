7db14299bac01da750bd1bc2ce58f2135f28d3f0
kopal <kopal@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2013-02-13 11:15:16 +0000

    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@5289 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/MorseCode/MorseCodeSettings.cs
@@ -0,0 +1,60 @@
+﻿/*
+   Copyright 2013 Nils Kopal <Nils.Kopal@Uni-Kassel.de>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System.ComponentModel;
+using Cryptool.PluginBase;
+using Cryptool.PluginBase.Miscellaneous;
+
+namespace Cryptool.Plugins.MorseCode
+{
+    public class MorseCodeSettings : ISettings
+    {
+        #region Private Variables
+
+        private int _action;
+
+        #endregion
+
+        #region TaskPane Settings
+
+        [TaskPane("ActionCaption", "ActionTooltip", null, 2, false, ControlType.ComboBox, new string[] { "Encode", "Decode" })]
+        public int Action
+        {
+            get { return _action; }
+            set
+            {
+                if (value != _action)
+                {
+                    _action = value;
+                    OnPropertyChanged("Action");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            EventsHelper.PropertyChanged(PropertyChanged, this, propertyName);
+        }
+
+        #endregion
+    }
+}
