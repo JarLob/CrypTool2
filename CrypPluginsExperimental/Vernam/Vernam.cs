ec319263bfa778d2ea1a295b528ee415988efc31
beuscher <beuscher@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2013-03-14 13:02:30 +0000

    Implemented (experimental) Plugin for Vernam-Cipher in Vernam.cs
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@5400 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPluginsExperimental/Vernam/Vernam.cs
@@ -0,0 +1,228 @@
+﻿/*
+   Copyright 2011 CrypTool 2 Team <ct2contact@cryptool.org>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+using System.ComponentModel;
+using System.Windows.Controls;
+using Cryptool.PluginBase;
+using Cryptool.PluginBase.Miscellaneous;
+using System.Text;
+
+namespace Cryptool.Plugins.Vernam
+{
+   
+    [Author("Benedict Beuscher", "benedict.beuscher@hotmail.com", "Uni Duisburg-Essen", "http://www.uni-due.de/")]
+
+    [PluginInfo("Vernam Cipher", "Combines any alphanumeric plaintext with a keytext to get a vernam-encrypted ciphertext", "Vernam/userdoc.xml", new[] { "Vernam/Images/Vernam.png" })]
+
+    [ComponentCategory(ComponentCategory.CiphersModernSymmetric)]
+    public class Vernam : ICrypComponent
+    {
+        #region Private Variables
+
+        private readonly VernamSettings settings = new VernamSettings();
+    
+        #endregion
+
+        #region Data Properties
+
+        private string inputString;
+        private string outputString;
+        private string keyString;
+
+
+        [PropertyInfo(Direction.InputData, "Key input", "KeyTooltip", false)]
+        public string KeyString
+        {
+            get { return this.keyString; }
+            set
+            {
+                if (value != keyString)
+                {
+                    this.keyString = value;
+                    OnPropertyChanged("newKeyString");
+                }
+            }
+        }
+
+        [PropertyInfo(Direction.InputData, "Text input", "Input a string to be encrypted by the Vernam Cipher",true)]
+        public string InputString
+        {
+            get { return this.inputString; }
+            set
+            {
+                if (value != InputString)
+                {
+                    this.inputString = value;
+                    OnPropertyChanged("newInputString");
+                }
+            }
+        }
+
+        
+
+        [PropertyInfo(Direction.OutputData, "Text output", "The string encrypted by the Vernam Cipher",false)]
+        public string OutputString
+        {
+            get { return this.outputString; }
+            set { this.outputString = value; }
+        }
+
+        #endregion
+
+        #region IPlugin Members
+
+        /// <summary>
+        /// Provide plugin-related parameters (per instance) or return null.
+        /// </summary>
+        public ISettings Settings
+        {
+            get { return settings; }
+        }
+
+        /// <summary>
+        /// Provide custom presentation to visualize the execution or return null.
+        /// </summary>
+        public UserControl Presentation
+        {
+            get { return null; }
+        }
+
+        /// <summary>
+        /// Called once when workflow execution starts.
+        /// </summary>
+        public void PreExecution()
+        {
+        }
+
+        /// <summary>
+        /// Called every time this plugin is run in the workflow execution.
+        /// </summary>
+        public void Execute()
+        {
+            ProgressChanged(0, 1);
+            StringBuilder newOutputString = new StringBuilder();
+            string alphabet = settings.alphabet;
+            
+            if (!string.IsNullOrEmpty(InputString))
+            {
+                for (int i = 0; i < InputString.Length; i++)
+                {
+                    char currentChar = InputString[i];
+                    char currentKeyChar = KeyString[i%KeyString.Length];
+                    int currentCharPosition = alphabet.IndexOf(currentChar);
+                    int currentKeyCharPosition = alphabet.IndexOf(currentKeyChar);
+                    int cipherCharPosition = 0;
+                    if (currentCharPosition >= 0)
+                    {
+
+                        if (settings.Action == VernamSettings.CipherMode.Encrypt)
+                        {
+                            cipherCharPosition = (currentCharPosition + currentKeyCharPosition) % alphabet.Length;
+                        }
+                        else if (settings.Action == VernamSettings.CipherMode.Decrypt)
+                        {
+                            cipherCharPosition = (currentCharPosition - currentKeyCharPosition + alphabet.Length) % alphabet.Length;
+                        }
+                        newOutputString.Append(alphabet[cipherCharPosition]);
+                    }
+                    else
+                    {
+                        if (settings.UnknownSymbolHandling == VernamSettings.UnknownSymbolHandlingMode.Ignore)
+                        {
+                            newOutputString.Append(currentKeyChar);
+                        }
+                        else if (settings.UnknownSymbolHandling == VernamSettings.UnknownSymbolHandlingMode.Replace)
+                        {
+                            newOutputString.Append("#");
+                        }
+                    }
+
+                    
+
+                }
+
+
+            }
+
+
+
+            ProgressChanged(1, 1);
+            OutputString = newOutputString.ToString();
+            OnPropertyChanged("OutputString");
+
+
+        }
+
+        
+
+        /// <summary>
+        /// Called once after workflow execution has stopped.
+        /// </summary>
+        public void PostExecution()
+        {
+        }
+
+        /// <summary>
+        /// Triggered time when user clicks stop button.
+        /// Shall abort long-running execution.
+        /// </summary>
+        public void Stop()
+        {
+        }
+
+        /// <summary>
+        /// Called once when plugin is loaded into editor workspace.
+        /// </summary>
+        public void Initialize()
+        {
+        }
+
+        /// <summary>
+        /// Called once when plugin is removed from editor workspace.
+        /// </summary>
+        public void Dispose()
+        {
+        }
+
+        #endregion
+
+        #region Event Handling
+
+        public event StatusChangedEventHandler OnPluginStatusChanged;
+
+        public event GuiLogNotificationEventHandler OnGuiLogNotificationOccured;
+
+        public event PluginProgressChangedEventHandler OnPluginProgressChanged;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void GuiLogMessage(string message, NotificationLevel logLevel)
+        {
+            EventsHelper.GuiLogMessage(OnGuiLogNotificationOccured, this, new GuiLogEventArgs(message, this, logLevel));
+        }
+
+        private void OnPropertyChanged(string name)
+        {
+            EventsHelper.PropertyChanged(PropertyChanged, this, new PropertyChangedEventArgs(name));
+        }
+
+        private void ProgressChanged(double value, double max)
+        {
+            EventsHelper.ProgressChanged(OnPluginProgressChanged, this, new PluginProgressEventArgs(value, max));
+        }
+
+        #endregion
+    }
+}
---
+++ CrypPluginsExperimental/Vernam/VernamSettings.cs
@@ -0,0 +1,124 @@
+﻿
+using System;
+using System.ComponentModel;
+using Cryptool.PluginBase;
+using Cryptool.PluginBase.Miscellaneous;
+
+namespace Cryptool.Plugins.Vernam
+{
+    
+    public class VernamSettings : ISettings
+    {
+      
+
+        public delegate void VernamLogMessage(string msg, NotificationLevel loglevel);
+        public enum CipherMode { Encrypt = 0, Decrypt = 1 };
+        public enum UnknownSymbolHandlingMode { Ignore = 0, Remove = 1, Replace = 2 };
+
+        public CipherMode selectedCipherMode = CipherMode.Encrypt;
+        public string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789";
+        private UnknownSymbolHandlingMode unknownSymbolHandling = UnknownSymbolHandlingMode.Ignore;
+        public event VernamLogMessage LogMessage;
+        
+
+        #region TaskPane Settings
+
+        /// <summary>
+        /// HOWTO: This is an example for a setting entity shown in the settings pane on the right of the CT2 main window.
+        /// This example setting uses a number field input, but there are many more input types available, see ControlType enumeration.
+        /// </summary>
+        [PropertySaveOrder(4)]
+        [TaskPane("CipherMode", "Change Mode to encryption or decryption", null, 1, true, ControlType.ComboBox, new string[] { "Encrypt", "Decrypt" })]
+        public CipherMode Action
+        {
+            get
+            {
+                return this.selectedCipherMode;
+            }
+            set
+            {
+                if (value != selectedCipherMode)
+                {
+                    this.selectedCipherMode = value;
+                    OnPropertyChanged("Action");
+                }
+            }
+        }
+
+        [PropertySaveOrder(5)]
+        [TaskPane("Unknown Symbol Handle", "Set what to do with unknown symbols", null, 4, true, ControlType.ComboBox, new string[] { "Ignore", "Remove", "Replace" })]
+        public UnknownSymbolHandlingMode UnknownSymbolHandling
+        {
+            get { return this.unknownSymbolHandling; }
+            set
+            {
+                if (value != unknownSymbolHandling)
+                {
+                    this.unknownSymbolHandling = value;
+                    OnPropertyChanged("UnknownSymbolHandling");
+                }
+            }
+        }
+
+        [PropertySaveOrder(6)]
+        [TaskPane("Input Alphabet", "fill out to specify alphabet", null, 6, true, ControlType.TextBox, "")]
+        public string AlphabetSymbols
+        {
+            get { return this.alphabet; }
+            set
+            {
+                string a = removeEqualChars(value);
+                if (a.Length == 0) 
+                {
+                    OnLogMessage("no alphabet. using: \"" + alphabet + "\" (" + alphabet.Length.ToString() + " Symbols)", NotificationLevel.Info);
+                }
+                else if (!alphabet.Equals(a))
+                {
+                    this.alphabet = a;
+                    OnLogMessage("new alphabet: \"" + alphabet + "\" (" + alphabet.Length.ToString() + " Symbols)", NotificationLevel.Info);
+                    OnPropertyChanged("AlphabetSymbols");
+                }
+            }
+        }
+
+        private string removeEqualChars(string value)
+        {
+            int length = value.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (value[i] == value[j])
+                    {
+                        OnLogMessage("Removing duplicate letter: \'" + value[j] + "\' from alphabet!", NotificationLevel.Warning);
+                        value = value.Remove(j, 1);
+                        j--;
+                        length--;
+                    }
+                }
+            }
+
+            return value;
+        }
+
+        private void OnLogMessage(string msg, NotificationLevel level)
+        {
+            if (LogMessage != null)
+                LogMessage(msg, level);
+        }
+
+        #endregion
+
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            EventsHelper.PropertyChanged(PropertyChanged, this, propertyName);
+        }
+
+        #endregion
+    }
+}
