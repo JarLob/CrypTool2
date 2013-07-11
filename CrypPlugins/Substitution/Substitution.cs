fea1e2b028c217324a56fa1656da2a7407dcd224
kopal <kopal@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2013-07-11 14:41:30 +0000

    added first version of presentation for the substitution
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@5566 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/Substitution/Substitution.cs
+++ CrypPlugins/Substitution/Substitution.cs
@@ -22,6 +22,11 @@ using Cryptool.PluginBase;
 using System.ComponentModel;
 using System.Windows.Controls;
 using Cryptool.PluginBase.Miscellaneous;
+using System.Windows.Threading;
+using System.Threading;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Windows;
 
 namespace Cryptool.Substitution
 {
@@ -31,6 +36,9 @@ namespace Cryptool.Substitution
     [ComponentCategory(ComponentCategory.CiphersClassic)]
     public class Substitution : ICrypComponent
     {
+
+        private SubstitutionPresentation _presentation = new SubstitutionPresentation();
+
         [PropertyInfo(Direction.InputData, "InputStringCaption", "InputStringTooltip", true)]
         public string InputString
         {
@@ -95,7 +103,7 @@ namespace Cryptool.Substitution
 
         public UserControl Presentation
         {
-            get { return null; }
+            get { return _presentation; }
         }
 
         public void Execute()
@@ -104,6 +112,7 @@ namespace Cryptool.Substitution
             ProgressChanged(0,1);
             var dict = GenerateSubstitutionDictionary(SourceAlphabet.Replace(Environment.NewLine, String.Empty), 
                                                       DestinationAlphabet.Replace(Environment.NewLine, String.Empty));
+            GeneratePresentationMapping(dict);
 
             if (((SubstitutionSettings) Settings).SymbolChoice == SymbolChoice.Random)
             {
@@ -115,6 +124,116 @@ namespace Cryptool.Substitution
             }
             OnPropertyChanged("OutputString");
             ProgressChanged(1, 1);
+        }
+
+        /// <summary>
+        /// Generates the graphical presentation
+        /// </summary>
+        /// <param name="dict"></param>
+        private void GeneratePresentationMapping(Dictionary<string, string> dict)
+        {
+            _presentation.Dispatcher.Invoke(DispatcherPriority.Background, (SendOrPostCallback)delegate
+            {
+                try
+                {
+                    _presentation.Stackpanel.Children.Clear();
+                    foreach (var entry in dict)
+                    {
+                        string[] froms = entry.Key.Split('|');
+                        string[] tos = entry.Value.Split('|');
+
+                        Grid grid = new Grid();
+                        grid.RowDefinitions.Add(new RowDefinition());
+                        grid.RowDefinitions.Add(new RowDefinition());
+                        grid.RowDefinitions.Add(new RowDefinition());
+                        grid.Width = Math.Max(55 * froms.Length, 55 * tos.Length);
+                        grid.Height = 155;
+
+                        for (int i = 0; i < Math.Max(froms.Length, tos.Length); i++)
+                        {
+                            grid.ColumnDefinitions.Add(new ColumnDefinition());
+                        }
+
+                        int fromColumnCounter = 0;
+                        foreach (string from in froms)
+                        {
+                            Rectangle fromRectangle = new Rectangle();
+                            SolidColorBrush fromsolidBrushColor = new SolidColorBrush();
+                            fromsolidBrushColor.Color = Color.FromArgb(255, 100, 255, 100);
+                            fromRectangle.Fill = fromsolidBrushColor;
+                            fromRectangle.StrokeThickness = 2;
+                            fromRectangle.Stroke = Brushes.Black;
+                            fromRectangle.Width = 50;
+                            fromRectangle.Height = 50;
+                            fromRectangle.SetValue(Grid.RowProperty, 0);
+                            fromRectangle.SetValue(Grid.ColumnProperty, fromColumnCounter);
+                            TextBlock fromText = new TextBlock();
+                            fromText.Text = from;
+                            fromText.FontSize = 12;
+                            fromText.VerticalAlignment = VerticalAlignment.Center;
+                            fromText.HorizontalAlignment = HorizontalAlignment.Center;
+                            fromText.SetValue(Grid.RowProperty, 0);
+                            fromText.SetValue(Grid.ColumnProperty, fromColumnCounter);
+                            grid.Children.Add(fromRectangle);
+                            grid.Children.Add(fromText);
+                            fromColumnCounter++;
+                        }
+
+                        int toColumnCounter = 0;
+                        foreach (string to in tos)
+                        {
+                            Rectangle toRectangle = new Rectangle();
+                            SolidColorBrush tosolidBrushColor = new SolidColorBrush();
+                            tosolidBrushColor.Color = Color.FromArgb(255, 255, 100, 100);
+                            toRectangle.Fill = tosolidBrushColor;
+                            toRectangle.StrokeThickness = 2;
+                            toRectangle.Stroke = Brushes.Black;
+                            toRectangle.Width = 50;
+                            toRectangle.Height = 50;
+                            toRectangle.SetValue(Grid.RowProperty, 2);
+                            toRectangle.SetValue(Grid.ColumnProperty, toColumnCounter);
+                            TextBlock toText = new TextBlock();
+                            toText.Text = to;
+                            toText.FontSize = 12;
+                            toText.VerticalAlignment = VerticalAlignment.Center;
+                            toText.HorizontalAlignment = HorizontalAlignment.Center;
+                            toText.SetValue(Grid.RowProperty, 2);
+                            toText.SetValue(Grid.ColumnProperty, toColumnCounter);
+                            grid.Children.Add(toRectangle);
+                            grid.Children.Add(toText);
+
+                            int fromcolumncounter = 0;
+                            foreach (string from in froms)
+                            {
+                                Line line = new Line();
+                                line.X1 = 25 + 55 * fromcolumncounter;
+                                line.Y1 = -5;
+                                line.X2 = 25 + 55 * toColumnCounter;
+                                line.Y2 = 55;
+                                SolidColorBrush lineSolidBrushColor = new SolidColorBrush();
+                                lineSolidBrushColor.Color = Colors.Black;
+                                line.Fill = lineSolidBrushColor;
+                                line.Stroke = lineSolidBrushColor;
+                                line.StrokeThickness = 2;
+                                line.HorizontalAlignment = HorizontalAlignment.Left;
+                                line.SetValue(Grid.RowProperty, 1);
+                                line.SetValue(Grid.ColumnProperty, 0);
+                                line.SetValue(Grid.ColumnSpanProperty, Math.Max(froms.Length, tos.Length));
+                                grid.Children.Add(line);
+                                fromcolumncounter++;
+                            }
+                            toColumnCounter++;
+                        }
+
+                        _presentation.Stackpanel.Children.Add(grid);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    GuiLogMessage(string.Format("Error creating substitution presentation: {0}", ex.Message), NotificationLevel.Error);
+                }
+            }, null);
+
         }
 
         public void Stop()
