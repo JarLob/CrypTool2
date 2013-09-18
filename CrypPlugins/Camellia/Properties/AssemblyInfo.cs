6c10407129df0f959eaeaab141279b9df5c7c1c7
kopal <kopal@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2013-09-18 15:39:27 +0000

    Camellia: * added first version of Camellia * added template for Camellia * added unit test for Camellia
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@5699 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/Camellia/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Camellia")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Camellia")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6bc71e0e-0d14-598f-cd66-37d28ad278bd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("2.0.0.0")]
