bf1db8ec4ebb9a4a54ccad3cf36d65a16ce22f83
kopal <kopal@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2014-01-20 11:52:54 +0000

    added updated version of NTDLR wrapper of Eric Schmeck
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@5842 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- LibSource/ExtendedNTL/src/NTL_Wrapper.cpp
+++ LibSource/ExtendedNTL/src/NTL_Wrapper.cpp
@@ -20,34 +20,40 @@ namespace NTL
 
 		array<BigInteger,2>^ LLLReduce (array<BigInteger,2>^ matrix, long dim, double delta)
 		{
+			return LLLReduce (matrix, dim, dim, delta);
+		}
+
+		array<BigInteger,2>^ LLLReduce (array<BigInteger,2>^ matrix, long n, long m, double delta)
+		{
 			mat_ZZ B, U;					
 
-			B.SetDims(dim, dim);
+			B.SetDims(n, m);
 
-			for (int i = 1; i <= dim; i++)
+			for (int i = 1; i <= n; i++)
 			{
-				for (int j = 1; j <= dim; j++) 
+				for (int j = 1; j <= m; j++) 
 				{		
-					B(i,j) = conv<ZZ>((char *) System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(matrix[i - 1,j - 1].ToString()).ToPointer());
+					B(i, j) = conv<ZZ>((char *) System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(matrix[i - 1, j - 1].ToString()).ToPointer());
 				}
 			}
 
 			LLL_FP(B, U, delta);
 
-			for (int i = 1; i <= dim; i++)
+			for (int i = 1; i <= n; i++)
 			{
-				for (int j = 1; j <= dim; j++) 
+				for (int j = 1; j <= m; j++) 
 				{	
-					matrix[i-1,j-1] = ConvertFromZZToBigInt(B(i,j));
+					matrix[i-1, j-1] = ConvertFromZZToBigInt(B(i, j));
 				}
 			}
 
-			array<BigInteger,2>^ transMatrix  = gcnew array<BigInteger,2>(dim, dim);
-			for (int i = 1; i <= dim; i++)
+			array<BigInteger,2>^ transMatrix  = gcnew array<BigInteger,2>(n, n);
+			
+			for (int i = 1; i <= n; i++)
 			{
-				for (int j = 1; j <= dim; j++) 
+				for (int j = 1; j <= n; j++) 
 				{	
-					transMatrix[i-1,j-1] = ConvertFromZZToBigInt(U(i,j));
+					transMatrix[i-1, j-1] = ConvertFromZZToBigInt(U(i, j));
 				}
 			}
 			return transMatrix;
@@ -65,22 +71,26 @@ namespace NTL
 				zz *= -1;
 			}
 
-			while(zz > 0){
+			while(zz > 0)
+			{
 				tmp = INTenc(to_int(zz % to_ZZ(10)));
 				if(tmp!=0)
 					result = System::String::Concat(tmp, result);
 
 				zz /= 10;
 			}
+
 			if (setMinus)
 				result = System::String::Concat("-", result);
 			if (result == "")
 				result = "0";
+
 			return BigInteger::Parse(result);
 		}
 
 
-		char INTenc(int v){
+		char INTenc(int v)
+		{
 			char* tab = "0123456789";
 			if (v>=0 && v<10)
 				return tab[v];
@@ -103,24 +113,8 @@ namespace NTL
 			}
 
 			ZZ det = determinant(B, 0);
-
-			String^ result ("");	
-			Char tmp;
-
-			if (det<0)			
-				det *= -1;			
-
-			while(det>0){
-				tmp = INTenc(to_int(det % to_ZZ(10)));
-				if(tmp!=0)
-					result = System::String::Concat(tmp, result);
-
-				det /= 10;
-			}
-
-			if (result == "")
-				result = "0";
-			return BigInteger::Parse(result);		
+			
+			return ConvertFromZZToBigInt(det);	
 		}
 
 		BigInteger ModInverse (BigInteger a, BigInteger mod)
@@ -132,20 +126,7 @@ namespace NTL
 
 			ZZ invModZZ = InvMod(aZZ, modZZ);
 
-			String^ result ("");	
-			Char tmp;						
-
-			while(invModZZ>0){
-				tmp = INTenc(to_int(invModZZ % to_ZZ(10)));
-				if(tmp!=0)
-					result = System::String::Concat(tmp, result);
-
-				invModZZ /= 10;
-			}
-
-			if (result == "")
-				result = "0";
-			return BigInteger::Parse(result);	
+			return ConvertFromZZToBigInt(invModZZ);	
 		}
 	};
 }
