51669e9c48c8c2495aab716faf12a5fc075cca3f
schmeck <schmeck@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2014-02-16 14:14:31 +0000

    - Unit test for Merkle-Hellman and RSA
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@5868 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- UnitTests/LatticeCryptoTest.cs
+++ UnitTests/LatticeCryptoTest.cs
@@ -10,7 +10,7 @@ namespace UnitTests
     public class LatticeCryptoTest
     {
         [TestMethod]
-        public void GGHMethod()
+        public void GGHTest()
         {
             //Private key, public key and error vector taken from the book:
             //Hoffstein, J.; Pipher, J. & Silverman, J. H. 
@@ -42,21 +42,85 @@ namespace UnitTests
 
             for (int i = 0; i < 1000; i++)
             {
-                string message = GenerateMessage(100);
+                string message = GenerateMessage(1, 100);
                 VectorND cipher = ggh.Encrypt(message);
                 string decryptedMessage = ggh.Decrypt(cipher);
-                Assert.AreEqual(decryptedMessage, message);
+                Assert.AreEqual(message, decryptedMessage);
             }
         }
 
-        public string GenerateMessage(int maxLength)
+        [TestMethod]
+        public void MerkleHellmanTest()
+        {
+            //Example taken from:
+            //http://en.wikipedia.org/wiki/Merkle%E2%80%93Hellman_knapsack_cryptosystem
+
+            VectorND privateKey = new VectorND(new BigInteger[] { 2, 7, 11, 21, 42, 89, 180, 354 });
+            MerkleHellmanModel merkleHellman = new MerkleHellmanModel(privateKey, 588, 881);
+
+            //Cryptosystem correct?
+            VectorND publicKey = new VectorND(new BigInteger[] { 295, 592, 301, 14, 28, 353, 120, 236 });
+            Assert.AreEqual(publicKey.ToString(), merkleHellman.publicKey.ToString());
+            BigInteger rI = 442;
+            Assert.AreEqual(rI, merkleHellman.rI);
+            
+            //Encryption and decryption correct?
+            string message = "a";
+            VectorND cipher = merkleHellman.Encrypt(message);
+            VectorND expectedCipher = new VectorND(new BigInteger[]{1129});
+            Assert.AreEqual(cipher.ToString(), expectedCipher.ToString());
+            string decryptedMessage = merkleHellman.Decrypt(cipher);
+            Assert.AreEqual(message, decryptedMessage);
+
+            //Cryptoanalysis correct?
+            string cryptanalysedMessage = merkleHellman.Cryptanalysis(cipher, null);
+            Assert.AreEqual(message, cryptanalysedMessage);
+
+            //More Testing
+            for (int i = 0; i < 1000; i++)
+            {
+                message = GenerateMessage(1, 100);
+                cipher = merkleHellman.Encrypt(message);
+                decryptedMessage = merkleHellman.Decrypt(cipher);
+                Assert.AreEqual(message, decryptedMessage);
+            }
+        }
+
+        [TestMethod]
+        public void RSATest()
+        {
+            //There is no need to test the generation of a RSA cryptosystem, because BouncyCastle is used
+            Random r = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int bitSize = r.Next(32, 1024);
+                RSAModel rsa = new RSAModel(bitSize);
+
+                //Encryption and decryption correct?
+                string message = GenerateMessage(3, rsa.GetBlockSize());
+                BigInteger cipher = rsa.Encrypt(message);
+                string decryptedMessage = rsa.Decrypt(cipher);
+                Assert.AreEqual(message, decryptedMessage);
+
+                //Cryptoanalysis correct?
+                int unknownStart = r.Next(message.Length);
+                int unknownLength = r.Next(1, message.Length - unknownStart);
+                string left = message.Substring(0, unknownStart);
+                string right = message.Substring(unknownStart + unknownLength);
+                string unknownMessageResult = rsa.StereotypedAttack(left, right, unknownLength, cipher, "4");
+                Assert.AreEqual(message.Substring(unknownStart, unknownLength), unknownMessageResult);
+            }
+        }
+
+        public string GenerateMessage(int min, int max)
         {
             Random r = new Random();
-            int length = r.Next(maxLength);
+            int length = r.Next(min, max);
             StringBuilder message = new StringBuilder(length);
 
-            for (int i = 0; i < maxLength; i++)
-                message.Append((char)r.Next(94, 127));
+            for (int i = 0; i < length; i++)
+                message.Append((char)r.Next(33, 127));
 
             return message.ToString();
         }
