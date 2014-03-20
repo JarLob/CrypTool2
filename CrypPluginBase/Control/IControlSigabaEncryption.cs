2a0f628b76bf56d312186e858a7eb037381adadd
weyers <weyers@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2014-03-20 18:00:48 +0000

    IControlSigabaEncryption added
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@5913 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPluginBase/Control/IControlSigabaEncryption.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cryptool.PluginBase.Control
+{
+    
+        public interface IControlSigabaEncryption : IControl, IDisposable
+        {
+            string Decrypt(string ciphertext);
+            void setInternalConfig();
+            void changeSettings(string setting, object value);
+
+            byte[] DecryptFast(byte[] ciphertext, int[] a, byte[] positions);
+
+            void setCipherRotors(int i, byte a);
+
+            void setControlRotors(byte i, byte b);
+
+            void setIndexRotors(byte i, byte c);
+
+            void setIndexMaze();
+
+            void setIndexMaze(int[] indexmaze);
+
+            void setBool(byte ix, byte i, bool rev);
+
+            void setPositionsControl(byte ix, byte i, byte position);
+
+            void setPositionsIndex(byte ix, byte i, byte position);
+
+            string preFormatInput(string text);
+            string postFormatOutput(string text);
+        }
+    
+}
