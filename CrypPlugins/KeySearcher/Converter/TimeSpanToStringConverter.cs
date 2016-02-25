40b9449f1ec002ff7b843051354dd774bba2bc4e
konze <konze@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2016-02-25 18:30:10 +0000

    KeySearcher: *Remaining Time: **removed hours, minutes and seconds if remaining time is longer that 1 year **removed minutes and seconds if remaining time is longer than a day
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@6682 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/KeySearcher/Converter/TimeSpanToStringConverter.cs
+++ CrypPlugins/KeySearcher/Converter/TimeSpanToStringConverter.cs
@@ -8,37 +8,29 @@ namespace KeySearcherConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value == null)
-            {
-                return "-";
-            }
+            if (value == null)  return "-";
 
             var timeSpan = (TimeSpan) value;
-
-            if (timeSpan == new TimeSpan(-1) || timeSpan.Ticks == 0)
-                return "~";
-
-            var seconds = timeSpan.Seconds;
-            if (seconds == 0 && timeSpan.Milliseconds > 0)
-                seconds = 1;
-
+            if (timeSpan == new TimeSpan(-1) || timeSpan.Ticks == 0) return "~";
+                
             if (timeSpan.Days > 365)
             {
-
                 var years = (int)(timeSpan.Days / 365.25f);
                 var ts = new TimeSpan((int)(365.25 * years), 0, 0, 0);
                 var timeWithoutYears = timeSpan.Subtract(ts);
-
-
-                return string.Format("{4} years, {0} days, {1:D2}:{2:D2}:{3:D2}",
-                     timeWithoutYears.Days, timeWithoutYears.Hours, timeWithoutYears.Minutes, timeWithoutYears.Seconds, years);
+                return string.Format("{0} years, {1} days",  years, timeWithoutYears.Days);
+            } 
+            
+            if (timeSpan.Days > 0)
+            {
+                return string.Format("{0:D2} days, {1:D2} hours", timeSpan.Days, timeSpan.Hours);
             }
 
-            if (timeSpan.Days > 0)
-                return string.Format("{0:D2} days, {1:D2}:{2:D2}:{3:D2}", timeSpan.Days, timeSpan.Hours,
-                                     timeSpan.Minutes, seconds);
-
-            
+            var seconds = timeSpan.Seconds;
+            if (seconds == 0 && timeSpan.Milliseconds > 0)
+            {
+                seconds = 1;
+            }
 
             return string.Format("{0:D2}:{1:D2}:{2:D2}", timeSpan.Hours, timeSpan.Minutes, seconds);
         }
