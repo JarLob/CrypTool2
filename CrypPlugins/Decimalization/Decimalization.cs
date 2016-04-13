9df6f8c8323bdc8f31fc6f24b431953c5de8b3fb
krauss <krauss@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2016-04-13 15:01:22 +0000

    Decimalization: code review, more compact code
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@6711 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/Decimalization/Decimalization.cs
+++ CrypPlugins/Decimalization/Decimalization.cs
@@ -37,6 +37,12 @@ namespace Cryptool.Plugins.Decimalization
         {
             private String sres;
             private int[] ires;
+            
+            public Result(int[] arr)
+            {
+                Ires = arr;
+                Sres = String.Join("", arr.Select(n => n.ToString()));
+            }
 
             public String Sres
             {
@@ -91,19 +97,11 @@ namespace Cryptool.Plugins.Decimalization
             get { return settings; }
         }
 
-        /// <summary>
-        /// HOWTO: You can provide a custom (tabbed) presentation to visualize your algorithm.
-        /// Return null if you don't provide one.
-        /// </summary>
         public UserControl Presentation
         {
             get { return null; }
         }
 
-        /// <summary>
-        /// HOWTO: You can provide custom (quickwatch) presentation to visualize your algorithm.
-        /// Return null if you don't provide one.
-        /// </summary>
         public UserControl QuickWatchPresentation
         {
             get { return null; }
@@ -113,42 +111,49 @@ namespace Cryptool.Plugins.Decimalization
         {
         }
 
-        /// <summary>
-        /// HOWTO: Enter the algorithm you'd like to implement in this method.
-        /// </summary>
         public void Execute()
         {
-            Result res = null;
-           
-
-            ProgressChanged(0, 1);
-
-            switch (settings.Mode)
+            try
             {
-                case 0:
-                    res = ProcessVisaMethod();
-                    break;
-                case 1:
-                    res = ProcessModuloMethod();
-                    break;
-                case 2:
-                    res = ProcessMultMethod();
-                    break;
-                case 3:
-                    res = ProcessIBMMethod();
-                    break;
-                default:
-                    GuiLogMessage("Unknown Decimalization Mode", NotificationLevel.Error);
-                    break;
+                Result res = null;
+
+                ProgressChanged(0, 1);
+
+                switch (settings.Mode)
+                {
+                    case 0:
+                        res = ProcessVisaMethod();
+                        break;
+                    case 1:
+                        res = ProcessModuloMethod();
+                        break;
+                    case 2:
+                        res = ProcessMultMethod();
+                        break;
+                    case 3:
+                        res = ProcessIBMMethod();
+                        break;
+                    default:
+                        GuiLogMessage("Unknown Decimalization Mode", NotificationLevel.Error);
+                        return;
+                }
+
+                DecimalNumberInt = res.Ires;
+                DecimalNumberStr = res.Sres;
+
+                OnPropertyChanged("DecimalNumberInt");
+                OnPropertyChanged("DecimalNumberStr");
+
+                ProgressChanged(1, 1);
             }
-
-            DecimalNumberInt = res.Ires;
-            DecimalNumberStr = res.Sres;
-            
-            OnPropertyChanged("DecimalNumberInt");
-            OnPropertyChanged("DecimalNumberStr");
-
-            ProgressChanged(1, 1);
+            catch (OverflowException e)
+            {
+                GuiLogMessage("Overflow Exception: Numbers are too big. Try again with smaller numbers.", NotificationLevel.Error);
+            }
+            catch (DivideByZeroException e)
+            {
+                GuiLogMessage("Divide By Zero Exception: Try again with other numbers.", NotificationLevel.Error);
+            }
         }
 
         public void PostExecution()
@@ -197,235 +202,76 @@ namespace Cryptool.Plugins.Decimalization
 
         private Result ProcessVisaMethod()
         {
-            Result res = new Result();
-            // Lists to save numbers
-            List<uint> listhex = new List<uint>();
-            List<uint> listhexgr9 = new List<uint>();
             List<int> listres = new List<int>();
-            // Arrays to iterate
-            uint[] hex;
-            uint[] hexgr9;
-            int[] resar = new int[settings.Quant];
-            // Helper variables
-            uint t = 0;
-            int j = 0;
+            List<int> listres9 = new List<int>();
 
-            // Create hexnumbers from binary data
-            for (int i = 0; i < BinaryNumber.Length; i++)
+            foreach(var b in BinaryNumber)
             {
-                t = BinaryNumber[i];
-                t = t >> 4;
-                listhex.Add(t);
-                t = BinaryNumber[i];
-                t = t << 28;
-                t = t >> 28;
-                listhex.Add(t);
+                int hi = b >> 4;
+                if (hi < 10) { listres.Add(hi); if (listres.Count >= settings.Quant) break; } else listres9.Add(hi - 10);
+                int lo = b % 0xf;
+                if (lo < 10) { listres.Add(lo); if (listres.Count >= settings.Quant) break; } else listres9.Add(lo - 10);
             }
 
-            // Copy list to array
-            hex = new uint[listhex.Count];
-            listhex.CopyTo(hex, 0);
-
-            // Fill result list with hexnumbers smaller 10 
-            for (int i = 0; i < settings.Quant; i++)
-            {
-                if ((j < (hex.Length)) && (hex[j] < 10))
-                {
-                    listres.Add((int)hex[j]);
-                    j++;
-                }
-                else if ((j < (hex.Length)) && (hex[j] > 9))
-                {
-                    while ((j < (hex.Length)) && (hex[j] > 9))
-                    {
-                        listhexgr9.Add(hex[j]);
-                        j++;
-                    }
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            // Fill rest of result with numbers greater 9 subtracted by 10
             if (listres.Count < settings.Quant)
-            {
-                hexgr9 = new uint[listhexgr9.Count];
-                listhexgr9.CopyTo(hexgr9, 0);
-                j = 0;
-                while (listres.Count < settings.Quant)
-                {
-                    if (j > hexgr9.Length - 1)
-                    {
-                         GuiLogMessage("Too less random input data for requested quantity of decimals.", NotificationLevel.Warning);
-                         break;
-                    }
-                    listres.Add((int)(hexgr9[j]-10));
-                    j++;
-                }
-            }
-            
-            listres.CopyTo(resar);
-            res.Ires = resar;
-            res.Sres = intArrayToString(resar);
+                listres.AddRange(listres9.Take(settings.Quant - listres.Count));
 
-            return res;
+            if (listres.Count < settings.Quant)
+                GuiLogMessage("Too few random input data for requested quantity of decimals.", NotificationLevel.Warning);
+
+            return new Result(listres.ToArray());
         }
 
         private Result ProcessModuloMethod()
         {
-            Result res = new Result();
-            byte[] binar = new byte[BinaryNumber.Length + 1];
-            BigInteger divisor = new BigInteger();
-            BigInteger dividend = new BigInteger();
-            BigInteger bigres = new BigInteger();
+            BigInteger dividend = byteArrayToBigInteger(BinaryNumber);
+            BigInteger divisor = BigInteger.Pow(10, settings.Quant);
+            BigInteger bigres = dividend % divisor;
 
-            try
-            {
-                divisor = new BigInteger(Math.Pow(10, settings.Quant));
-            }
-            catch (OverflowException e)
-            {
-                GuiLogMessage("Overflow Exception: Numbers to Big. Try again with smaller numbers", NotificationLevel.Error);
-            }
-
-            BinaryNumber.CopyTo(binar,0);
-            binar[binar.Length-1] = 0x00;
-            dividend = new BigInteger(binar);
-            // Execute Modulo Operation
-            bigres = BigInteger.Remainder(dividend, divisor);
-
-            // BigInteger to Int[]
-            res.Ires = bigIntegerToIntArray(bigres);
-            res.Sres = bigres.ToString();
-
-            return res;
+            return new Result(bigIntegerToIntArray(bigres));
         }
 
         private Result ProcessMultMethod()
         {
-            Result res = new Result();
-            BigInteger z, div = new BigInteger(), factor = new BigInteger(), bres;
-            byte[]  zbyte = new byte[BinaryNumber.Length+1];
+            BigInteger dividend = byteArrayToBigInteger(BinaryNumber) * BigInteger.Pow(10, settings.Quant);
+            BigInteger bigres = dividend >> (8 * BinaryNumber.Length);
 
-            // Calculate Result
-            BinaryNumber.CopyTo(zbyte, 0);
-            zbyte[zbyte.Length - 1] = 0x00;
-            z = new BigInteger(zbyte);
-
-            try
-            {
-                factor = new BigInteger(Math.Pow(10, settings.Quant));
-                div = new BigInteger(Math.Pow(2, 8 * BinaryNumber.Length));
-            }
-            catch (OverflowException e)
-            {
-                GuiLogMessage("Overflow Exception: Numbers to Big. Try again with smaller numbers.", NotificationLevel.Error);
-            }
-            bres = BigInteger.Multiply(z, factor);
-
-            try
-            {
-                bres = BigInteger.Divide(bres, div);
-            }
-            catch (DivideByZeroException e)
-            {
-                GuiLogMessage("Overflow Exception: Numbers to Big. Try again with smaller numbers.", NotificationLevel.Error);
-            }
-
-            // Write Result to Return Structure
-            res.Ires = bigIntegerToIntArray(bres);
-            res.Sres = bres.ToString();
-
-            return res;
+            return new Result(bigIntegerToIntArray(bigres));
         }
 
         private Result ProcessIBMMethod()
         {
-            Result res = new Result();
-            uint t = 0;
-            List<uint> listhex = new List<uint>();
-            uint[] arhex;
-            int[] arres = new int[settings.Quant];
-            int[] assocTable = { 0, 0, 0, 0, 0, 0 };
-            int j = 0;
+            int[] assocTable = { settings.IbmA, settings.IbmB, settings.IbmC, settings.IbmD, settings.IbmE, settings.IbmF };
 
-            // Build Associtation Table for HexNumbers > 9
-            assocTable[0] = settings.IbmA;
-            assocTable[1] = settings.IbmB;
-            assocTable[2] = settings.IbmC;
-            assocTable[3] = settings.IbmD;
-            assocTable[4] = settings.IbmE;
-            assocTable[5] = settings.IbmF;
+            List<int> listres = new List<int>();
 
-            // Create hexnumbers from binary data
-            for (int i = 0; i < BinaryNumber.Length; i++)
+            foreach (var b in BinaryNumber)
             {
-                t = BinaryNumber[i];
-                t = t >> 4;
-                listhex.Add(t);
-                t = BinaryNumber[i];
-                t = t << 28;
-                t = t >> 28;
-                listhex.Add(t);
+                int hi = b >> 4;
+                listres.Add(hi < 10 ? hi : assocTable[hi - 10]);
+                if (listres.Count >= settings.Quant) break;
+                int lo = b & 0xf;
+                listres.Add(lo < 10 ? lo : assocTable[lo - 10]);
+                if (listres.Count >= settings.Quant) break;
             }
 
-            arhex = new uint[listhex.Count];
-            listhex.CopyTo(arhex, 0);
+            if (listres.Count < settings.Quant)
+                GuiLogMessage("Too few random input data for requested quantity of decimals.", NotificationLevel.Warning);
 
-            // Fill Result Array
-            while ((j < arres.Length) && (j < arhex.Length))
-            {
-                if (arhex[j] < 10)
-                {
-                    arres[j] = (int)arhex[j];
-                }
-                else
-                {
-                    arres[j] = assocTable[arhex[j] - 10];
-                }
-                j++;
-            }
-
-            if (j < arres.Length)
-            {
-                GuiLogMessage("Too less random input data for requested quantity of decimals.", NotificationLevel.Warning);
-            }
-
-            res.Ires = arres;
-            res.Sres = intArrayToString(arres);
-
-            return res;
+            return new Result(listres.ToArray());
         }
 
         private int[] bigIntegerToIntArray(BigInteger nr)
         {
-            List<int> nrlist = new List<int>();
-            int[] nrar;
-            String s = nr.ToString();
-
-            for (int i = 0; i < s.Length; i++)
-            {
-                nrlist.Add(Convert.ToInt32(s[i])-48);
-            }
-            nrar = new int[nrlist.Count];
-            
-            nrlist.CopyTo(nrar, 0);
-
-            return nrar;
+            return nr.ToString().Select(c => (int)c - 48).ToArray();
         }
 
-        private String intArrayToString(int[] nrs)
+        private BigInteger byteArrayToBigInteger(byte[] bytes)
         {
-            String res = "";
-
-            for (int i = 0; i < nrs.Length; i++)
-            {
-                res += nrs[i].ToString();
-            }
-
-            return res;
+            byte[] bytesPlusZero = new byte[bytes.Length + 1];
+            bytes.CopyTo(bytesPlusZero, 0);
+            bytesPlusZero[bytesPlusZero.Length - 1] = 0x00;
+            return new BigInteger(bytesPlusZero);
         }
 
         #endregion
