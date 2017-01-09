3364a13da815bb53468b71293c24ef5952be96ee
deshpande <deshpande@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2017-01-09 15:41:11 +0000

    updated
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@6982 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- TREYFER/TREYFER.cs
+++ TREYFER/TREYFER.cs
@@ -15,7 +15,7 @@ namespace Cryptool.TREYFER
 {
     [Author("Aditya Deshpande", "adeshpan@mail.uni-mannheim.de", "Universität Mannheim", "https://www.uni-mannheim.de/1/")]
     [PluginInfo("Cryptool.TREYFER.Properties.Resources", "PluginCaption", "PluginTooltip", "TREYFER/userdoc.xml",
-        new[] { "TREYFER/Images/Treyfer.jpg", "TREYFER/Images/Treyfer.jpg", "TREYFER/Images/Treyfer.jpg" })]
+        new[] { "TREYFER/Images/Treyfer.jpg"})]
     [ComponentCategory(ComponentCategory.CiphersModernSymmetric)]
     public class TREYFER : ICrypComponent
     {
