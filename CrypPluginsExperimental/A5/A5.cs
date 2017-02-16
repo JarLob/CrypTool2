87441e99fcca7b6e8d2e6cd168c2a80210468dec
krauss <krauss@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2017-02-16 22:55:04 +0000

    A5: unused reference removed
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@7033 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPluginsExperimental/A5/A5.cs
+++ CrypPluginsExperimental/A5/A5.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-using System.Numerics;
 using Cryptool.PluginBase;
 using Cryptool.Plugins.A5;
 using Cryptool.PluginBase.IO;
