7cc6700da8bee801153d035aad92196c84305d5a
heuser <heuser@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2017-04-10 08:16:30 +0000

    Added String Similarity comparison extension
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@7095 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ LibSource/StringSimilarity/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PercentageSimilarity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source, target;
+            while (true)
+            {
+                Console.Write("Please enter source string: ");
+                source = Console.ReadLine();
+                Console.Write("Please enter target string: ");
+                target = Console.ReadLine();
+
+                Console.WriteLine("{0}%", source.ToLower().CalculateSimilarity(target.ToLower()) * 100);
+            }
+        }
+    }
+}
