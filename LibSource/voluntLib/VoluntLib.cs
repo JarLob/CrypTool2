6f0eae9b89870a56532329ab3bcdf9c9d81f2ba5
kopal <kopal@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2017-05-02 09:57:42 +0000

    changed multicast group to 224.0.224.1 since it is not registered at IANA
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@7142 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- LibSource/voluntLib/VoluntLib.cs
+++ LibSource/voluntLib/VoluntLib.cs
@@ -55,7 +55,8 @@ namespace voluntLib
         private const int DefaultWorkingListTimeout = 600000;
         private const int DefaultMaximumBackoffTime = 1000;
         private const int DefaultPort = 13337;
-        private const string DefaultMulticastGroup = "224.0.7.1";
+        private const string DefaultMulticastGroup = "224.0.224.1"; //currently not registered at IANA 
+        // see https://www.iana.org/assignments/multicast-addresses/multicast-addresses.xhtml#multicast-addresses-1
         private readonly string defaultLocalStoragePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),  "VoluntLib" + Path.DirectorySeparatorChar + "VoluntLibStore.xml");
 
         #region Configuration Properties
