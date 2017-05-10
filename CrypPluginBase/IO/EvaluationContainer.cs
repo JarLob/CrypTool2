de904f53500d3bcd211bedd6619901258d9793d4
heuser <heuser@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2017-05-10 10:13:20 +0000

    Changed copyright
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@7147 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPluginBase/IO/EvaluationContainer.cs
+++ CrypPluginBase/IO/EvaluationContainer.cs
@@ -1,5 +1,5 @@
 ﻿/*
-   Copyright 2009-2010 Matthäus Wander, University of Duisburg-Essen
+   Copyright 2017 Bastian Heuser, University of Kassel
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
