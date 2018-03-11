0bd79e37f20a0288722a4b991122fd19cb74657d
krauss <krauss@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2018-03-11 20:15:09 +0000

    PrimeTest: some small fixes - generate list of small primes (instead of hardcoded list) - more efficient direct lookup of small primes added
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@7305 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPluginBase/Miscellaneous/BigIntegerHelper.cs
+++ CrypPluginBase/Miscellaneous/BigIntegerHelper.cs
@@ -26,12 +26,31 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Numerics;
 using System.Security.Cryptography;
-using System.Globalization;
 
 namespace Cryptool.PluginBase.Miscellaneous
 {
     public static class BigIntegerHelper
     {
+        private static BigInteger[] smallPrimes = new BigInteger[] {};
+        private static HashSet<BigInteger> smallPrimesSet;
+        private static int smallPrimesLimit = 2000;
+
+        static BigIntegerHelper()
+        {
+            var primes = new List<int> { 2 };
+
+            for (int n = 3; n < smallPrimesLimit; n += 2)
+            {
+                int i, w = (int)Math.Sqrt(n);
+                for (i = 0; primes[i] <= w; i++)
+                    if (n % primes[i] == 0) break;
+                if (primes[i] > w) primes.Add(n);
+            }
+
+            smallPrimes = primes.Select(p => (BigInteger)p).ToArray();
+            smallPrimesSet = new HashSet<BigInteger>(smallPrimes);
+        }
+
         #region internal stuff of expression parser
 
         delegate BigInteger FunctionDelegate(BigInteger[] args);
@@ -656,33 +675,6 @@ namespace Cryptool.PluginBase.Miscellaneous
             return RandomIntLimit( ((BigInteger)1) << bits );
         }
 
-        #region primesBelow2000
-        // primes smaller than 2000 to test the generated prime number (taken from BigInteger class written by Chew Keong TAN)
-
-        private static readonly int[] primesBelow2000 = {
-            2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
-            101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199,
-	        211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293,
-	        307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397,
-	        401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499,
-	        503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599,
-	        601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691,
-	        701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,
-	        809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887,
-	        907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997,
-	        1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097,
-	        1103, 1109, 1117, 1123, 1129, 1151, 1153, 1163, 1171, 1181, 1187, 1193,
-	        1201, 1213, 1217, 1223, 1229, 1231, 1237, 1249, 1259, 1277, 1279, 1283, 1289, 1291, 1297,
-	        1301, 1303, 1307, 1319, 1321, 1327, 1361, 1367, 1373, 1381, 1399,
-	        1409, 1423, 1427, 1429, 1433, 1439, 1447, 1451, 1453, 1459, 1471, 1481, 1483, 1487, 1489, 1493, 1499,
-	        1511, 1523, 1531, 1543, 1549, 1553, 1559, 1567, 1571, 1579, 1583, 1597,
-	        1601, 1607, 1609, 1613, 1619, 1621, 1627, 1637, 1657, 1663, 1667, 1669, 1693, 1697, 1699,
-	        1709, 1721, 1723, 1733, 1741, 1747, 1753, 1759, 1777, 1783, 1787, 1789,
-	        1801, 1811, 1823, 1831, 1847, 1861, 1867, 1871, 1873, 1877, 1879, 1889,
-	        1901, 1907, 1913, 1931, 1933, 1949, 1951, 1973, 1979, 1987, 1993, 1997, 1999 };
-
-        #endregion
-
         public static BigInteger NextProbablePrime( this BigInteger n )
         {
             if (n < 0) throw new ArithmeticException("NextProbablePrime cannot be called on value < 0");
@@ -702,7 +694,6 @@ namespace Cryptool.PluginBase.Miscellaneous
                 if (IsProbablePrime(n)) return n;
                 n += 4;
             }
-
         }
 
         public static BigInteger PreviousProbablePrime(this BigInteger n)
@@ -724,20 +715,16 @@ namespace Cryptool.PluginBase.Miscellaneous
                 if (IsProbablePrime(n)) return n;
                 n -= 4;
             }
-
         }
-
-        /*
-         * This code is heavily inspired by the code from the BigInteger class written by Chew Keong TAN
-         */
+        
         public static bool IsProbablePrime(this BigInteger n)
         {
             n = BigInteger.Abs(n);
 
             // test small numbers
-
-            if (n < 4) return n > 1;
-            if (primesBelow2000.Where(p => p < n).Any(p => n % p == 0)) return false;
+            
+            if (n < smallPrimesLimit) return smallPrimesSet.Contains(n);
+            if (smallPrimes.Any(p => n % p == 0)) return false;
 
             // perform Miller-Rabin Test
 
@@ -745,14 +732,14 @@ namespace Cryptool.PluginBase.Miscellaneous
             // (see https://de.wikipedia.org/wiki/Miller-Rabin-Test)
 
             int[] tests =
-                n < 1373653 ? new int[] { 2, 3 } :
-                n < 9080191 ? new int[] { 31, 73 } :
-                n < 4759123141 ? new int[] { 2, 7, 61 } :
-                n < 2152302898747 ? new int[] { 2, 3, 5, 7, 11 } :
-                n < 3474749660383 ? new int[] { 2, 3, 5, 7, 11, 13 } :
-                n < 341550071728321 ? new int[] { 2, 3, 5, 7, 11, 13, 17 } :
-                n < 3825123056546413051 ? new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23 } :
-                /*n < 318665857834031151167461*/ new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37 };
+            n < 1373653 ? new int[] { 2, 3 } :
+            n < 9080191 ? new int[] { 31, 73 } :
+            n < 4759123141 ? new int[] { 2, 7, 61 } :
+            n < 2152302898747 ? new int[] { 2, 3, 5, 7, 11 } :
+            n < 3474749660383 ? new int[] { 2, 3, 5, 7, 11, 13 } :
+            n < 341550071728321 ? new int[] { 2, 3, 5, 7, 11, 13, 17 } :
+            n < 3825123056546413051 ? new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23 } :
+            /*n < 318665857834031151167461*/ new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37 };
 
             return tests.All(a => IsStrongPseudoprime(n, a));
         }
