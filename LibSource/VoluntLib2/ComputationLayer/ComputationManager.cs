1961a65ea1fbce6380a239d960d8f9fc3997da4b
kopal <kopal@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2018-05-18 07:57:28 +0000

    * VoluntLib2: ** started implementing ComputationManager
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@7424 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- LibSource/VoluntLib2/ComputationLayer/ComputationManager.cs
+++ LibSource/VoluntLib2/ComputationLayer/ComputationManager.cs
@@ -14,16 +14,30 @@
    limitations under the License.
 */
 using System;
+using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using VoluntLib2.ManagementLayer;
+using VoluntLib2.Tools;
 
 namespace VoluntLib2.ComputationLayer
 {
     internal class ComputationManager
     {
+        private const int MAX_TERMINATION_WAIT_TIME = 5000; //5 s
+        private const int WORKER_THREAD_SLEEPTIME = 1; // ms
+        
+        private Logger Logger = Logger.GetLogger();
+        private bool Running = false;
+        private Thread WorkerThread;
+
+        internal ConcurrentDictionary<BigInteger, JobAssignment> JobAssignments = new ConcurrentDictionary<BigInteger, JobAssignment>();
+        internal ConcurrentQueue<Operation> Operations = new ConcurrentQueue<Operation>();
 
         public ComputationManager(VoluntLib voluntLib, JobManager jobManager)
         {
@@ -32,22 +46,139 @@ namespace VoluntLib2.ComputationLayer
 
         public void Start()
         {
+            Running = true;
+            WorkerThread = new Thread(ComputationManagerWork);
+            WorkerThread.IsBackground = true;
+            WorkerThread.Start();
+        }
 
+        private void ComputationManagerWork(object obj)
+        {
+            Logger.LogText("WorkerThread started", this, Logtype.Info);
+            while (Running)
+            {
+                try
+                {
+                    Operation operation;
+                    if (Operations.TryDequeue(out operation) == true)
+                    {
+                        // before we execute an operation, we check if it is finished
+                        if (operation.IsFinished == false)
+                        {
+                            //operations that are not finished are enqueued again
+                            Operations.Enqueue(operation);
+                        }
+                        else
+                        {
+                            Logger.LogText(String.Format("Operation {0}-{1} has finished. Removed it.", operation.GetType().FullName, operation.GetHashCode()), this, Logtype.Debug);
+                            //we dont execute this operation since it is finished
+                            continue;
+                        }
+                        try
+                        {
+                            operation.Execute();
+                        }
+                        catch (Exception ex)
+                        {
+                            Logger.LogText(String.Format("Exception during execution of operation {0}-{1}: {2}", operation.GetType().FullName, operation.GetHashCode(), ex.Message), this, Logtype.Error);
+                            Logger.LogException(ex, this, Logtype.Error);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogText(String.Format("Exception during handling of operation: {2}", ex.Message), this, Logtype.Error);
+                    Logger.LogException(ex, this, Logtype.Error);
+                }
+                try
+                {
+                    Thread.Sleep(WORKER_THREAD_SLEEPTIME);
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogText(String.Format("Exception during sleep of thread: {0}", ex.Message), this, Logtype.Error);
+                    Logger.LogException(ex, this, Logtype.Error);
+                }
+            }
+            Logger.LogText("WorkerThread terminated", this, Logtype.Info);
         }
 
         public void Stop()
         {
-
+            if (!Running)
+            {
+                return;
+            }
+            Logger.LogText("Stop method was called...", this, Logtype.Info);
+            Running = false;
+            DateTime start = DateTime.Now;
+            while ((WorkerThread.IsAlive) && DateTime.Now < start.AddMilliseconds(MAX_TERMINATION_WAIT_TIME))
+            {
+                Thread.Sleep(100);
+            }
+            if (WorkerThread.IsAlive)
+            {
+                Logger.LogText("WorkerThread did not end within 5 seconds", this, Logtype.Info);
+                try
+                {
+                    WorkerThread.Abort();
+                    Logger.LogText("Aborted WorkerThread", this, Logtype.Info);
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogText(String.Format("Exception during abortion of WorkerThread: {0}", ex.Message), this, Logtype.Error);
+                    Logger.LogException(ex, this, Logtype.Error);
+                }
+            }
         }
 
-        internal bool JoinJob(System.Numerics.BigInteger jobID, ACalculationTemplate template, int amountOfWorker)
-        {            
+        internal bool JoinJob(BigInteger jobId, ACalculationTemplate calculationTemplate, int amountOfWorkers)
+        {
+            if (!JobAssignments.ContainsKey(jobId))
+            {
+                try
+                {
+                    if (!JobAssignments.TryAdd(jobId, new JobAssignment() { JobId = jobId, CalculationTemplate = calculationTemplate, AmountOfWorkers = amountOfWorkers }))
+                    {
+                        Logger.LogText(String.Format("Could not add job with jobid {0} to internal JobAssignments dictionary!", BitConverter.ToString(jobId.ToByteArray())), this, Logtype.Warning);
+                    }
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogText(String.Format("Could not add job with jobid {0} to internal JobAssignments dictionary!", BitConverter.ToString(jobId.ToByteArray())), this, Logtype.Warning);
+                    Logger.LogException(ex, this, Logtype.Error);
+                }                
+            }           
             return false;
         }
 
-        internal void StopJob(System.Numerics.BigInteger jobID)
+        internal void StopJob(BigInteger jobId)
         {
-           
-        }
+            if (JobAssignments.ContainsKey(jobId))
+            {
+                try
+                {
+                    JobAssignment jobassignment;
+                    if (!JobAssignments.TryRemove(jobId, out jobassignment))
+                    {
+                        Logger.LogText(String.Format("Could not remove job with jobid {0} from internal JobAssignments dictionary!", BitConverter.ToString(jobId.ToByteArray())), this, Logtype.Warning);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogText(String.Format("Could not remove job with jobid {0} from internal JobAssignments dictionary!", BitConverter.ToString(jobId.ToByteArray())), this, Logtype.Warning);
+                    Logger.LogException(ex, this, Logtype.Error);
+                }
+            }
+        }        
+    }
+
+    internal class JobAssignment
+    {
+        public BigInteger JobId { get; set; }
+        public ACalculationTemplate CalculationTemplate { get; set; }
+        public int AmountOfWorkers { get; set; }
+        public ArrayList Tasks = ArrayList.Synchronized(new ArrayList());
     }
 }
---
+++ LibSource/VoluntLib2/ComputationLayer/Operations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoluntLib2.ComputationLayer
+{
+    /// <summary>
+    /// Abstract super class of all operations:
+    /// Operations are state machines that send messages and work on received message using the ConnectionManager
+    /// If an operation's IsFinished equals true, it can be deleted by ConnectionManager
+    /// </summary>
+    internal abstract class Operation
+    {
+        /// <summary>
+        /// Needed by each operation
+        /// </summary>
+        public ComputationManager ConnectionManager { get; set; }
+
+        /// <summary>
+        /// Tells the worker thread if this operation is finished. If it is, it can be deleted
+        /// </summary>
+        public abstract bool IsFinished { get; }
+
+        /// <summary>
+        /// Called by the worker thread (cooperative multitasking)
+        /// </summary>
+        public abstract void Execute();
+    }
+}
