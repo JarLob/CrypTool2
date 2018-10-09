700a15b57e865841922cb5b1a73dad9d7cc80970
kopal <kopal@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2018-10-09 10:20:03 +0000

    CrypToolStoreDeveloperClient * UserManagement button is now invisibile when the user is not admin
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@7709 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- LibSource/CrypToolStoreLib/CrypToolStoreDeveloperClient/MainWindow.xaml.cs
+++ LibSource/CrypToolStoreLib/CrypToolStoreDeveloperClient/MainWindow.xaml.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*
+   Copyright 2018 Nils Kopal <Nils.Kopal<AT>Uni-Kassel.de>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -32,25 +47,31 @@ namespace CrypToolStoreDeveloperClient
         public string Password;
         public string Username;
         public bool IsLoggedIn;
+        public bool IsAdmin;
         public UiState UiState;
 
         public MainWindow()
-        {           
-            InitializeComponent();           
+        {
+            InitializeComponent();
 
             Username = null;
             Password = null;
             IsLoggedIn = false;
+            IsAdmin = false;
+           
+            ResizeMode = ResizeMode.CanMinimize;
 
             LoginView.MainWindow = this;
             HeaderView.MainWindow = this;
             MainMenuView.MainWindow = this;
 
-            ResizeMode = ResizeMode.CanMinimize;
-
             ChangeScreen(UiState.LoginScreen);            
         }
 
+        /// <summary>
+        /// Changes the current screen that CrypToolStoreDeveloperClient displays
+        /// </summary>
+        /// <param name="uiState"></param>
         public void ChangeScreen(UiState uiState)
         {
             UiState = uiState;
--- LibSource/CrypToolStoreLib/CrypToolStoreDeveloperClient/Views/LoginView.xaml.cs
+++ LibSource/CrypToolStoreLib/CrypToolStoreDeveloperClient/Views/LoginView.xaml.cs
@@ -1,4 +1,19 @@
-﻿using CrypToolStoreLib.Client;
+﻿/*
+   Copyright 2018 Nils Kopal <Nils.Kopal<AT>Uni-Kassel.de>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+using CrypToolStoreLib.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -67,6 +82,11 @@ namespace CrypToolStoreDeveloperClient.Views
                         MainWindow.Username = Username.Text;
                         MainWindow.Password = Password.Password;
                         MainWindow.HeaderView.Username = Username.Text;
+                        if (client.IsAdmin)
+                        {
+                            MainWindow.IsAdmin = true;
+                        }
+
                         client.Disconnect();
                         ((MainWindow)((Grid)this.Parent).Parent).ChangeScreen(UiState.MainMenu);
                     }
