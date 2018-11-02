42b60a835ffb5edef4f38c360cfe580bba452d02
kopal <kopal@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2018-11-02 12:03:36 +0000

    * CrypToolStore: ** adding missing file
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@7816 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ LibSource/CrypToolStoreLib/CrypToolStoreLib/Tools/ICrypStoreSerializable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrypToolStoreLib.Tools
+{
+    /// <summary>
+    /// Interface for serializable CrypToolStoreObjects
+    /// </summary>
+    public interface ICrypToolStoreSerializable
+    {
+        byte[] Serialize();
+        void Deserialize(byte[] bytes);
+    }
+}
