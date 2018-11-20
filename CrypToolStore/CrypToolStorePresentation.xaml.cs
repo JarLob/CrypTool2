a67574847ba1a55dde4d8c5e2ea37f94b02869a5
kopal <kopal@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2018-11-20 14:29:31 +0000

    * CrypToolStore ** Created first version of CrypToolStore user interface in CrypTool 2
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@7865 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypToolStore/CrypToolStorePresentation.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cryptool.CrypToolStore
+{
+    /// <summary>
+    /// Interaktionslogik für CrypToolStorePresentation.xaml
+    /// </summary>
+    [Cryptool.PluginBase.Attributes.Localization("CrypTool.CrypToolStore.Properties.Resources")]
+    public partial class CrypToolStorePresentation : UserControl
+    {
+        public CrypToolStorePresentation()
+        {
+            InitializeComponent();
+        }
+    }
+}
