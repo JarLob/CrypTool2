ac4e62fae30f58617a850dd356cfc84c8b214d55
kopal <kopal@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2019-05-06 19:17:40 +0000

    Startcenter: * fixed path to CrypToolBook * added exception handling (ignoring)
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@8106 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- Startcenter/Buttons.xaml.cs
+++ Startcenter/Buttons.xaml.cs
@@ -1,13 +1,11 @@
-﻿ 
-using System.IO;
+﻿ using System.IO;
 using System.Windows;
 using System.Windows.Controls; 
-//using Cryptool.CrypTutorials;  
 using Cryptool.PluginBase;
 using Cryptool.PluginBase.IO;
 using OnlineDocumentationGenerator.Generators.HtmlGenerator;
 using System.Windows.Documents;
-using System.Windows.Media;
+using System;
 
 namespace Startcenter
 {
@@ -22,57 +20,94 @@ namespace Startcenter
         public Buttons()
         {
             InitializeComponent();
-
-                //DistCompButton.Visibility = Visibility.Collapsed;
-                //DistCompLabel.Visibility = Visibility.Collapsed; 
         }
 
         private void WizardButton_Click(object sender, RoutedEventArgs e)
         {
-            OnOpenEditor(typeof(Wizard.Wizard), null);
+            try
+            {
+                OnOpenEditor(typeof(Wizard.Wizard), null);
+            }
+            catch (Exception)
+            {
+                //do nothing
+            }
         }
-
-        /*private void CrypTutorialsButton_Click(object sender, RoutedEventArgs e)
-        {
-            OnOpenEditor(typeof(CrypTutorials), null);
-        }*/
             
         private void HelpButton_Click(object sender, RoutedEventArgs e)
         {
-            OnlineHelp.InvokeShowDocPage(null);
+            try
+            {
+                OnlineHelp.InvokeShowDocPage(null);
+            }
+            catch (Exception)
+            {
+                //do nothing
+            }
         }
 
         private void WorkspaceButton_Click(object sender, RoutedEventArgs e)
         {
-            OnOpenEditor(typeof(WorkspaceManager.WorkspaceManagerClass), null);
-        }
-
-        private void ConnectButton_Click(object sender, RoutedEventArgs e)
-        { 
+            try
+            {
+                OnOpenEditor(typeof(WorkspaceManager.WorkspaceManagerClass), null);
+            }
+            catch (Exception)
+            {
+                //do nothing
+            }
         }
 
         private void WebpageButton_Click(object sender, RoutedEventArgs e)
         {
-            System.Diagnostics.Process.Start("https://www.cryptool.org/cryptool2");
+            try
+            {
+                System.Diagnostics.Process.Start("https://www.cryptool.org/cryptool2");
+            }
+            catch (Exception)
+            {
+                //do nothing
+            }
         }
 
         private void FacebookButton_Click(object sender, RoutedEventArgs e)
         {
-            System.Diagnostics.Process.Start("http://www.facebook.de/cryptool2");
+            try
+            {
+                System.Diagnostics.Process.Start("http://www.facebook.de/cryptool2");
+            }
+            catch (Exception)
+            {
+                //do nothing
+            }
         }
 
         private void BookButton_Click(object sender, RoutedEventArgs e)
         {
-            System.Diagnostics.Process.Start(Path.Combine(DirectoryHelper.DirectoryCrypPlugins, Properties.Resources.CTBookFilename));
+            try
+            {
+                System.Diagnostics.Process.Start(Path.Combine(DirectoryHelper.BaseDirectory, Properties.Resources.CTBookFilename));
+            }
+            catch (Exception)
+            {
+                //do nothing
+            }
         }
 
         private void CrypToolStoreButton_Click(object sender, RoutedEventArgs e)
-        {            
-            OnOpenEditor(typeof(Cryptool.CrypToolStore.CrypToolStoreEditor), new TabInfo()
+        {
+            try
             {
-                Title = Properties.Resources.CrypToolStore,
-                Tooltip = new Span(new Run(Properties.Resources.CrypToolStore))
-            });
+                OnOpenEditor(typeof(Cryptool.CrypToolStore.CrypToolStoreEditor), new TabInfo()
+                {
+                    Title = Properties.Resources.CrypToolStore,
+                    Tooltip = new Span(new Run(Properties.Resources.CrypToolStore))
+                });
+            }
+            catch (Exception)
+            {
+                //do nothing
+            }
         }
     }
 }
