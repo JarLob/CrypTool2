c2c6b26683df68aac104fb18501984bc8046b4a2
bender <bender@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2019-06-06 09:52:43 +0000

    ToyCiphers: Added missing implementation
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@8153 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPluginsExperimental/ToyCiphers/Ciphers/Cipher4/Cipher4Configuration.cs
@@ -0,0 +1,26 @@
+﻿/*
+   Copyright 2019 Christian Bender christian1.bender@student.uni-siegen.de
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+namespace ToyCiphers.Ciphers.Cipher4
+{
+    public static class Cipher4Configuration
+    {
+        public static readonly int BITWIDTHCIPHERFOUR = 4;
+        public static readonly int SBOXNUM = 1;
+        public static readonly int KEYNUM = 4;
+        public static readonly int[] SBOX = { 6, 4, 12, 5, 0, 7, 2, 14, 1, 15, 3, 13, 8, 10, 9, 11 };
+    }
+}
