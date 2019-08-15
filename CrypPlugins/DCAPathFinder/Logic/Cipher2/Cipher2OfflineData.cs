900bfcd305407106ee20d83a9e561ca0eb377c98
bender <bender@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2019-08-15 20:19:27 +0000

    Added missing class (DCA PathFinder)
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@8307 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/DCAPathFinder/Logic/Cipher2/Cipher2OfflineData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCAPathFinder.Logic.Cipher2
+{
+    public class Cipher2OfflineData
+    {
+        public DifferentialAttackRoundConfiguration configuration;
+        public List<Characteristic>[] allCharateristics;
+    }
+}
