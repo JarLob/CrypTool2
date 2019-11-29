3e27cd7c4734819ab5bd7bb2d149b0c64b1b494d
kopal <kopal@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2019-11-29 21:28:54 +0000

    - removed error added in last checkin
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@8482 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypWin/App.xaml.cs
+++ CrypWin/App.xaml.cs
@@ -36,7 +36,6 @@ namespace Cryptool.CrypWin
             System.Windows.Forms.Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
             AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
             this.DispatcherUnhandledException += App_DispatcherUnhandledException;
-            error!!!
         }
 
         private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
