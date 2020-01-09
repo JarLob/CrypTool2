de4f6434a18660beff3353ae2601258c22e33bc8
bender <bender@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2020-01-09 14:23:49 +0000

    Initial commit of Speck
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@8538 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/Speck/Speck.cs
@@ -0,0 +1,167 @@
+﻿/*
+   Copyright 2020 Christian Bender christian1.bender@student.uni-siegen.de
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System.ComponentModel;
+using System.Windows.Controls;
+using Cryptool.PluginBase;
+using Cryptool.PluginBase.Miscellaneous;
+
+namespace Cryptool.Plugins.Speck
+{
+    [Author("Christian Bender", "christian1.bender@student.uni-siegen.de", null, "http://www.uni-siegen.de")]
+    [PluginInfo("Speck.Properties.Resources", "PluginCaption", "PluginTooltip", "Speck/userdoc.xml", new[] { "Speck/Images/IC_Speck.png" })]
+    [ComponentCategory(ComponentCategory.CiphersModernSymmetric)]
+
+    public class Speck : ICrypComponent
+    {
+        #region Private Variables
+
+        // HOWTO: You need to adapt the settings class as well, see the corresponding file.
+        private readonly SpeckSettings settings = new SpeckSettings();
+
+        #endregion
+
+        #region Data Properties
+
+        /// <summary>
+        /// HOWTO: Input interface to read the input data. 
+        /// You can add more input properties of other type if needed.
+        /// </summary>
+        [PropertyInfo(Direction.InputData, "Input name", "Input tooltip description")]
+        public int SomeInput
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// HOWTO: Output interface to write the output data.
+        /// You can add more output properties ot other type if needed.
+        /// </summary>
+        [PropertyInfo(Direction.OutputData, "Output name", "Output tooltip description")]
+        public int SomeOutput
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region IPlugin Members
+
+        /// <summary>
+        /// Provide plugin-related parameters (per instance) or return null.
+        /// </summary>
+        public ISettings Settings
+        {
+            get { return settings; }
+        }
+
+        /// <summary>
+        /// Provide custom presentation to visualize the execution or return null.
+        /// </summary>
+        public UserControl Presentation
+        {
+            get { return null; }
+        }
+
+        /// <summary>
+        /// Called once when workflow execution starts.
+        /// </summary>
+        public void PreExecution()
+        {
+        }
+
+        /// <summary>
+        /// Called every time this plugin is run in the workflow execution.
+        /// </summary>
+        public void Execute()
+        {
+            // HOWTO: Use this to show the progress of a plugin algorithm execution in the editor.
+            ProgressChanged(0, 1);
+
+            // HOWTO: After you have changed an output property, make sure you announce the name of the changed property to the CT2 core.
+            SomeOutput = SomeInput - settings.SomeParameter;
+            OnPropertyChanged("SomeOutput");
+
+            // HOWTO: You can pass error, warning, info or debug messages to the CT2 main window.
+            if (settings.SomeParameter < 0)
+            {
+                GuiLogMessage("SomeParameter is negative", NotificationLevel.Debug);
+            }
+            // HOWTO: Make sure the progress bar is at maximum when your Execute() finished successfully.
+            ProgressChanged(1, 1);
+        }
+
+        /// <summary>
+        /// Called once after workflow execution has stopped.
+        /// </summary>
+        public void PostExecution()
+        {
+        }
+
+        /// <summary>
+        /// Triggered time when user clicks stop button.
+        /// Shall abort long-running execution.
+        /// </summary>
+        public void Stop()
+        {
+        }
+
+        /// <summary>
+        /// Called once when plugin is loaded into editor workspace.
+        /// </summary>
+        public void Initialize()
+        {
+        }
+
+        /// <summary>
+        /// Called once when plugin is removed from editor workspace.
+        /// </summary>
+        public void Dispose()
+        {
+        }
+
+        #endregion
+
+        #region Event Handling
+
+        public event StatusChangedEventHandler OnPluginStatusChanged;
+
+        public event GuiLogNotificationEventHandler OnGuiLogNotificationOccured;
+
+        public event PluginProgressChangedEventHandler OnPluginProgressChanged;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void GuiLogMessage(string message, NotificationLevel logLevel)
+        {
+            EventsHelper.GuiLogMessage(OnGuiLogNotificationOccured, this, new GuiLogEventArgs(message, this, logLevel));
+        }
+
+        private void OnPropertyChanged(string name)
+        {
+            EventsHelper.PropertyChanged(PropertyChanged, this, new PropertyChangedEventArgs(name));
+        }
+
+        private void ProgressChanged(double value, double max)
+        {
+            EventsHelper.ProgressChanged(OnPluginProgressChanged, this, new PluginProgressEventArgs(value, max));
+        }
+
+        #endregion
+    }
+}
