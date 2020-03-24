141880378808c41f4d4c48fd9fd1c9ed8561c575
kopal <kopal@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2020-03-24 14:56:06 +0000

    Alphabets: * added messagebox before deletion of alphabet * some minor layout fixes
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@8749 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

--- CrypPlugins/Alphabets/Properties/Resources.Designer.cs
+++ CrypPlugins/Alphabets/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Cryptool.Alphabets.Properties {
     // -Klasse über ein Tool wie ResGen oder Visual Studio automatisch generiert.
     // Um einen Member hinzuzufügen oder zu entfernen, bearbeiten Sie die .ResX-Datei und führen dann ResGen
     // mit der /str-Option erneut aus, oder Sie erstellen Ihr VS-Projekt neu.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -156,6 +156,24 @@ namespace Cryptool.Alphabets.Properties {
         public static string Delete {
             get {
                 return ResourceManager.GetString("Delete", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Sucht eine lokalisierte Zeichenfolge, die Do you really want to delete the alphabet \&quot;{0}\&quot;? ähnelt.
+        /// </summary>
+        public static string DeleteAlphabetMessageBoxText {
+            get {
+                return ResourceManager.GetString("DeleteAlphabetMessageBoxText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Sucht eine lokalisierte Zeichenfolge, die Delete alphabet? ähnelt.
+        /// </summary>
+        public static string DeleteAlphabetMessageBoxTitle {
+            get {
+                return ResourceManager.GetString("DeleteAlphabetMessageBoxTitle", resourceCulture);
             }
         }
         
