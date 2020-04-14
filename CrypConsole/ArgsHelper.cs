1748f7a6d1050720426dbfff9bdfd4d10c1a3a4b
kopal <kopal@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2020-04-14 15:08:23 +0000

    CrypConsole: * created first version of standalone-tool CrypConsole, which allow the execution of a cwm-file in the Windows console
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@8797 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypConsole/ArgsHelper.cs
@@ -0,0 +1,161 @@
+﻿/*
+   Copyright 2020 Nils Kopal <kopal<AT>cryptool.org>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptool.CrypConsole
+{
+    public class ArgsHelper
+    {        
+        /// <summary>
+        /// Returns the filename of the first cwm entry in args
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static string GetCWMFileName(string[] args)
+        {
+            var query = from str in args
+                        where (str.Length >= 5 && str.ToLower().Substring(0, 5).Equals("-cwm=")) 
+                           || (str.Length >= 6 && str.ToLower().Substring(0, 6).Equals("--cwm="))
+                        select str;
+
+            if (query.Count() > 0)
+            {
+                var filename = query.First().Split('=')[1];
+                if (filename.StartsWith("\""))
+                {
+                    filename = filename.Substring(1, filename.Length - 1);
+                }
+                if (filename.EndsWith("\""))
+                {
+                    filename = filename.Substring(0, filename.Length - 1);
+                }
+                return filename;
+            }
+            return null;
+        }
+
+        public static bool CheckVerbose(string[] args)
+        {
+            var query = from str in args
+                        where (str.Length >= 8 && str.ToLower().Substring(0, 8).Equals("-verbose"))
+                           || (str.Length >= 9 && str.ToLower().Substring(0, 9).Equals("--verbose"))
+                        select str;
+
+            if (query.Count() > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static List<string> GetInputParameters(string[] args)
+        {
+            var query = from str in args
+                        where (str.Length >= 7 && str.ToLower().Substring(0, 7).Equals("-input=")) 
+                            || (str.Length >= 8 && str.ToLower().Substring(0, 8).Equals("--input="))
+                        select str;
+
+            List<string> parameters = new List<string>();
+            foreach(var param in query)
+            {
+                var p = param.Split('=')[1];
+                if (p.StartsWith("\""))
+                {
+                    p = p.Substring(1, p.Length - 1);
+                }
+                if (p.EndsWith("\""))
+                {
+                    p = p.Substring(0, p.Length - 1);
+                }
+                parameters.Add(p);
+            }
+            return parameters;
+        }
+
+
+        /// <summary>
+        /// This method checks the args, if the user wants to see the help
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static bool CheckShowHelp(string[] args)
+        {
+            var query = from str in args
+                        where str.ToLower().Equals("--help") || str.ToLower().Equals("-help")
+                        select str;
+
+            //we show help, if requested or no parameters were given
+            if (args.Length == 0 || query.Count() > 0)
+            {
+                ShowHelp();
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        ///  Shows the help
+        /// </summary>
+        public static void ShowHelp()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("-= CrypConsole -- a CrypTool 2 Console for executing CrypTool 2 workspaces in the Windows console =- ");
+            Console.WriteLine("(C) 2020 by Nils Kopal, kopal@cryptool.org");
+            Console.WriteLine("Usage:");
+            Console.WriteLine("CrypConsole.exe -cwm=path/to/cwm/file -input=<input param definition> -output=<output param definition>");
+            Console.WriteLine("");
+            Console.WriteLine(" -help                               -> shows this help page");
+            Console.WriteLine(" -verbose                            -> writes logs etc to the console; for debugging");
+            Console.WriteLine(" -cwm=path/to/cwm/file               -> specifies a path to a cwm file that should be executed");
+            Console.WriteLine(" -input=<input param definition>     -> specifies an input parameter");
+            Console.WriteLine(" -output==<output param definition>  -> specifies an output parameter");
+        }
+    }
+
+    public enum ParameterType
+    {
+        Number,
+        Text,
+        Hex,
+        File
+    }
+
+    public class Parameter
+    {
+        public ParameterType ParameterType
+        {
+            get; 
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Value
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ CrypConsole/Main.xaml.cs
@@ -0,0 +1,211 @@
+﻿/*
+   Copyright 2020 Nils Kopal <kopal<AT>cryptool.org>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+using System;
+using System.Reflection;
+using WorkspaceManager.Execution;
+using WorkspaceManager.Model;
+using System.IO;
+using Path = System.IO.Path;
+using System.Windows;
+using System.Threading;
+using Cryptool.PluginBase;
+
+namespace Cryptool.CrypConsole
+{  
+    public partial class Main : Window
+    {
+        private static string[] subfolders = new string[]
+        {
+            "",
+            "CrypPlugins",
+            "Lib",
+        };
+
+        private bool _verbose = false;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Initialized(object sender, EventArgs e)
+        {
+            Start(CrypConsole.Args);
+        }
+
+        public void Start(string[] args)
+        {
+            if (ArgsHelper.CheckShowHelp(args))
+            {
+                Environment.Exit(0);
+            }
+
+            string cwm_file = ArgsHelper.GetCWMFileName(args);
+            if (cwm_file == null)
+            {
+                Console.WriteLine("Please specify a cwm file using -cwm=filename");
+                Environment.Exit(-1);
+            }
+            if (!File.Exists(cwm_file))
+            {
+                Console.WriteLine("Specified cwm file \"{0}\" does not exist", cwm_file);
+                Environment.Exit(-2);
+            }
+
+            _verbose = ArgsHelper.CheckVerbose(args);
+
+            try
+            {
+                var parameters = ArgsHelper.GetInputParameters(args);
+                foreach (var param in parameters)
+                {
+                    Console.WriteLine("Input param: " + param);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exception occured while parsing parameters: {0}", ex.Message);
+                Environment.Exit(-3);
+            }
+
+            try
+            {
+                UpdateAppDomain();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exception occured while parsing parameters: {0}", ex.Message);
+                Environment.Exit(-4);
+            }
+
+            WorkspaceModel model = null;
+            try
+            {
+                ModelPersistance modelPersistance = new ModelPersistance();
+                model = modelPersistance.loadModel(cwm_file, true);
+
+                foreach (var pluginModel in model.GetAllPluginModels())
+                {
+                    pluginModel.Plugin.OnGuiLogNotificationOccured += OnGuiLogNotificationOccured;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occured while loading model from cwm file: {0}", ex.Message);
+                Environment.Exit(-5);
+            }
+
+            ExecutionEngine engine = null;
+            try
+            {
+
+                engine = new ExecutionEngine(null);
+                engine.OnGuiLogNotificationOccured += OnGuiLogNotificationOccured;
+                engine.Execute(model, false);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exception occured while executing model: {0}", ex.Message);
+                Environment.Exit(-6);
+            }
+
+            Thread t = new Thread(() =>
+            {
+                while (engine.IsRunning())
+                {
+                    foreach (var p in model.GetAllPluginModels())
+                    {
+                        if (p.GetName().Equals("Ciphertext"))
+                        {
+                            foreach (var input in p.GetInputConnectors())
+                            {
+                                if (input.PropertyName.Equals("Input"))
+                                {
+                                    if (input.LastData != null)
+                                    {
+                                        Console.WriteLine("Output data: " + input.LastData);
+                                        engine.Stop();                                        
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                Environment.Exit(0);
+            });
+            t.Start();
+        }        
+
+        /// <summary>
+        /// Logs guilog to console based on error level and verbosity
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void OnGuiLogNotificationOccured(PluginBase.IPlugin sender, PluginBase.GuiLogEventArgs args)
+        {
+            if (_verbose || args.NotificationLevel == NotificationLevel.Error)
+            {
+                Console.WriteLine("GuiLog:{0}:{1}:{2}:{3}", DateTime.Now, args.NotificationLevel, (sender != null ? sender.GetType().Name : "null"), args.Message);
+            }
+        }
+
+        /// <summary>
+        /// Updates app domain with user defined assembly resolber routine
+        /// </summary>
+        private void UpdateAppDomain()
+        {
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.AssemblyResolve += new ResolveEventHandler(LoadAssembly);
+        }
+
+        /// <summary>
+        /// Loads assemblies defined by subfolders definition
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        private Assembly LoadAssembly(object sender, ResolveEventArgs args)
+        {
+            string folderPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            foreach (var subfolder in subfolders)
+            {
+                string assemblyPath = Path.Combine(folderPath, (Path.Combine(subfolder, new AssemblyName(args.Name).Name + ".dll")));
+
+                if (File.Exists(assemblyPath))
+                {
+                    Assembly assembly = Assembly.LoadFrom(assemblyPath);
+                    if (_verbose)
+                    {
+                        Console.WriteLine("Loaded assembly: " + assemblyPath);
+                    }
+                    return assembly;
+                }
+                assemblyPath = Path.Combine(folderPath, (Path.Combine(subfolder, new AssemblyName(args.Name).Name + ".exe")));
+
+                if (File.Exists(assemblyPath))
+                {
+                    Assembly assembly = Assembly.LoadFrom(assemblyPath);
+                    if (_verbose)
+                    {
+                        Console.WriteLine("Loaded assembly: " + assemblyPath);
+                    }
+                    return assembly;
+                }
+            }
+            return null;
+        }
+    }       
+}
