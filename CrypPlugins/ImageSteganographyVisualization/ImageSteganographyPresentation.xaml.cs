77652a19e62a7fc6492629dbb75b1e29f83e1afd
kopal <kopal@9b0d660e-8ab2-dd11-ad6d-000c29d71a56>
2020-10-23 11:16:14 +0000

    TextSteganography, ImageSteganographyPresentation: * added new steganography components and templates
    git-svn-id: https://svn.cryptool.org/CrypTool2/trunk@8926 9b0d660e-8ab2-dd11-ad6d-000c29d71a56

---
+++ CrypPlugins/ImageSteganographyVisualization/ImageSteganographyPresentation.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ImageSteganographyVisualization
+{
+    [Cryptool.PluginBase.Attributes.Localization("ImageSteganographyVisualization.Properties.Resources")]
+
+    public partial class ImageSteganographyPresentation : UserControl
+    {
+        public ModeType mode;
+        public LSBPresentation lsb;
+        public BPCSPresentation bpcs;
+        public ImageSteganographyVisualization imageStegVis;
+
+        public ImageSteganographyPresentation(ImageSteganographyVisualization imageStegVis)
+        {
+            InitializeComponent();
+            this.imageStegVis = imageStegVis;
+            this.mode = imageStegVis.GetSettings().GetMode();
+            Prompt.Text = Properties.Resources.ShowPresentationPrompt;
+        }
+
+        public void DisplayHidingProcessPresentation(ModeType mode)
+        {
+            this.mode = mode;
+            this.MinHeight = 350;
+            this.MinWidth = 550;
+            this.MainFrame.Children.Remove(MainPanel);
+
+            if (mode == ModeType.BPCS)
+            {
+                this.MainFrame.Children.Add(bpcs);
+            }
+            else
+            {
+                this.MainFrame.Children.Add(lsb);
+            }
+        }
+
+        public void HidePresentation()
+        {
+            if (mode == ModeType.LSB)
+            {
+                this.MainFrame.Children.Remove(lsb);
+            }
+            else if (mode == ModeType.BPCS)
+            {
+                this.MainFrame.Children.Remove(bpcs);
+            }
+            this.MinHeight = 100;
+            this.MinWidth = 150;
+            this.MainFrame.Children.Clear();
+            this.MainFrame.Children.Add(MainPanel);
+            Prompt.Text = Properties.Resources.ShowPresentationPrompt;
+        }
+
+        public void DisplayExtractionPresentation(int messageLength, BitArray redBitMask, BitArray greenBitMask, BitArray blueBitMask)
+        {
+            this.MinHeight = 100;
+            this.MinWidth = 150;
+            MainPanel.Visibility = Visibility.Hidden;
+
+            ExtractedHeader.Visibility = Visibility.Visible;
+            MessageLengthTB.Text = string.Format(Properties.Resources.MessageLengthExtractLabel + "{0} bits = {1} characters.", messageLength * 8, messageLength);
+            RedBitMask.Text = Properties.Resources.RedBitMaskLabel + ": " + BitArrayToString(redBitMask);
+            GreenBitMask.Text = Properties.Resources.GreenBitMaskLabel + ": " + BitArrayToString(greenBitMask);
+            BlueBitMask.Text = Properties.Resources.BlueBitMaskLabel + ": " + BitArrayToString(blueBitMask);
+        }
+
+        public void DisplayNoPresentation()
+        {
+            this.MinHeight = 100;
+            this.MinWidth = 150;
+            MainPanel.Visibility = Visibility.Visible;
+            ExtractedHeader.Visibility = Visibility.Hidden;
+
+            Prompt.Text = Properties.Resources.NoPresentationPrompt;
+
+        }
+
+        private string BitArrayToString(BitArray bits)
+        {
+            string bitString = "";
+            for (int i = 7; i >= 0; i--)
+            {
+                if (bits[i])
+                {
+                    bitString += "1";
+                }
+                else
+                {
+                    bitString += "0";
+                }
+            }
+            return bitString;
+        }
+    }
+
+}
---
+++ CrypPlugins/TextSteganography/TextSteganographyPresentation.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace TextSteganography
+{
+    [Cryptool.PluginBase.Attributes.Localization("TextSteganography.Properties.Resources")]
+    public partial class TextSteganographyPresentation : UserControl
+    {
+        private TextSteganography textSteg; 
+
+        public TextSteganographyPresentation(TextSteganography textSteg)
+        {
+            InitializeComponent();
+            this.textSteg = textSteg;
+        }
+
+        public void ShowZeroWidthSpaceEncoding()
+        {
+            StegoTextBlock.Text = "";
+            if(textSteg.GetAction() == ActionType.Hide)
+            {
+                BitArray messagebits = new BitArray(Encoding.UTF8.GetBytes(textSteg.InputSecretMessage));
+                StegoTextBlock.Inlines.Add(textSteg.CoverText.Substring(0, textSteg.offset));
+                for (int i = 0; i < messagebits.Length; i++)
+                {
+                    if (messagebits[i])
+                    {
+                        StegoTextBlock.Inlines.Add(new Run(" ") { Background = Brushes.Aquamarine });
+                    }
+                    else
+                    {
+                        StegoTextBlock.Inlines.Add(new Run(" ") { Background = Brushes.LightYellow });
+                    }
+                }
+                StegoTextBlock.Inlines.Add(textSteg.CoverText.Substring(textSteg.offset));
+            } else
+            {
+                for(int i = 0; i < textSteg.CoverText.Length; i++)
+                {
+                    if(textSteg.CoverText[i] == '\u200b')
+                    {
+                        StegoTextBlock.Inlines.Add(new Run(" ") { Background = Brushes.Aquamarine });
+                    } else if(textSteg.CoverText[i] == '\u200c')
+                    {
+                        StegoTextBlock.Inlines.Add(new Run(" ") { Background = Brushes.LightYellow });
+                    } else
+                    {
+                        StegoTextBlock.Inlines.Add((textSteg.CoverText[i]).ToString());
+                    }
+                }
+            }
+            
+            CBShowBits.IsChecked = false;
+        }
+
+        public void ShowBitsCheckbox()
+        {
+            CBPanel.Visibility = Visibility.Visible; 
+        }
+
+        public void HideBitsCheckBox()
+        {
+            CBPanel.Visibility = Visibility.Hidden;
+        }
+
+        public void ClearPres()
+        {
+            StegoTextBlock.Text = ""; 
+        }
+
+        public void HideMessageBits(object sender, RoutedEventArgs e)
+        {
+            ShowZeroWidthSpaceEncoding();
+        }
+
+        public void ShowMessageBits(object sender, RoutedEventArgs e)
+        {
+            StegoTextBlock.Text = "";
+            
+            if(textSteg.GetAction() == ActionType.Hide)
+            {
+                BitArray messagebits = new BitArray(Encoding.UTF8.GetBytes(textSteg.InputSecretMessage));
+                StegoTextBlock.Inlines.Add(textSteg.CoverText.Substring(0, textSteg.offset));
+                for (int i = 0; i < messagebits.Length; i++)
+                {
+                    if (messagebits[i])
+                    {
+                        StegoTextBlock.Inlines.Add(new Run("1") { Background = Brushes.Aquamarine });
+                    }
+                    else
+                    {
+                        StegoTextBlock.Inlines.Add(new Run("0") { Background = Brushes.LightYellow });
+                    }
+                }
+                StegoTextBlock.Inlines.Add(textSteg.CoverText.Substring(textSteg.offset));
+            } else
+            {
+                for (int i = 0; i < textSteg.CoverText.Length; i++)
+                {
+                    if (textSteg.CoverText[i] == '\u200b')
+                    {
+                        StegoTextBlock.Inlines.Add(new Run("1") { Background = Brushes.Aquamarine });
+                    }
+                    else if (textSteg.CoverText[i] == '\u200c')
+                    {
+                        StegoTextBlock.Inlines.Add(new Run("0") { Background = Brushes.LightYellow });
+                    }
+                    else
+                    {
+                        StegoTextBlock.Inlines.Add((textSteg.CoverText[i]).ToString());
+                    }
+                }
+            }
+            
+        }
+
+        public void ShowCapitalLetterEncoding(string stegoText)
+        {
+            StegoTextBlock.Text = "";
+            for (int i = 0; i < stegoText.Length; i++)
+            {
+                char c = stegoText[i];
+                if (Char.IsUpper(stegoText[i]))
+                {
+                    StegoTextBlock.Inlines.Add(new Run(Char.ToString(stegoText[i])) { FontWeight = FontWeights.Bold, Background = Brushes.Aquamarine });
+                }
+                else if (stegoText[i] == '\n')
+                {
+                    StegoTextBlock.Inlines.Add(new Run(Char.ToString(stegoText[i])) { Background = Brushes.Aquamarine });
+                }
+                else
+                {
+                    StegoTextBlock.Inlines.Add(new Run(Char.ToString(stegoText[i])) { });
+                }
+            }
+        }
+
+        public void ShowLettersMarkingEncoding(string stegoText)
+        {
+            StegoTextBlock.Text = "";
+            for (int i = 0; i < stegoText.Length - 1; i++)
+            {
+                char c = stegoText[i];
+                char d = stegoText[i + 1];
+
+                if (d == '\u0323')
+                {
+                    string s = "";
+                    s += c;
+                    s += d;
+
+                    StegoTextBlock.Inlines.Add(new Run(s) { FontWeight = FontWeights.Bold, Background = Brushes.Aquamarine });
+                    i++;
+                }
+                else
+                {
+                    StegoTextBlock.Inlines.Add(new Run(Char.ToString(c)));
+                }
+            }
+        }
+    }
+}
